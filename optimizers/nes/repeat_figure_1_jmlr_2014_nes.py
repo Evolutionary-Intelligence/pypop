71cdf16badcd05a0ebba8fe3be15edb58718ac06
Evolutionary-Intelligence <duanqq077@163.com>
2021-03-09 20:18:06 +0800

    Fix a bug for optimizers/nes/repeat_figure_1_jmlr_2014_nes.py

--- optimizers/nes/repeat_figure_1_jmlr_2014_nes.py
+++ optimizers/nes/repeat_figure_1_jmlr_2014_nes.py
@@ -22,16 +22,16 @@ class CanonicalSearchGradientAlgorithm(object):
         self.n_individuals = n_individuals  # number of individuals (population size, lambda)
         self.seed_rng = seed_rng  # seed of random number generator (rng)
 
-    def initialize(self, mu=1.0, sigma=np.array([[1.0]])):
+    def initialize(self, mu=1.0, sigma=1.0):
         z = np.empty((self.n_individuals, self.ndim_problem))  # population
         f_z = np.empty((self.n_individuals,))  # fitness of population
         g_mu = np.empty((self.n_individuals, self.ndim_problem))  # log-derivatives of mean of Gaussian distribution
         g_sigma = np.empty((self.n_individuals, self.ndim_problem))  # log-derivatives of std of Gaussian distribution
-        history = {"mu": [mu], "sigma": [float(sigma)]}
+        history = {"mu": [mu], "sigma": [sigma]}
         return z, f_z, g_mu, g_sigma, mu, sigma, history
 
     def iterate(self, z, f_z, g_mu, g_sigma, mu, sigma):
-        inv_sigma = np.linalg.inv(sigma)
+        inv_sigma = np.linalg.inv(np.array([[sigma]]))
         for k in range(self.n_individuals):
             # draw sample
             z[k] = mu + sigma * default_rng(self.seed_rng).standard_normal((self.ndim_problem,))
@@ -47,22 +47,24 @@ class CanonicalSearchGradientAlgorithm(object):
         z, f_z, g_mu, g_sigma, mu, sigma, history = self.initialize()
         for _ in range(int(self.max_generations)):
             g_mu, g_sigma = self.iterate(z, f_z, g_mu, g_sigma, mu, sigma)
-            mu -= self.eta * np.mean(g_mu, 0)
-            sigma -= self.eta * np.mean(g_sigma, 0)
+            mu -= float(self.eta * np.mean(g_mu, 0))
+            sigma -= float(self.eta * np.mean(g_sigma, 0))
             history["mu"].append(mu)
-            history["sigma"].append(float(sigma))
+            history["sigma"].append(sigma)
         return history
 
 
 if __name__ == "__main__":
     def sphere(x):
         return np.power(x, 2)
-    for _ in range(1):
-        sg = CanonicalSearchGradientAlgorithm(sphere)
+    n_trials = 3
+    stat_mean, stat_std = np.empty((n_trials, 1001)), np.empty((n_trials, 1001))
+    for t in range(n_trials):
+        sg = CanonicalSearchGradientAlgorithm(sphere, seed_rng=t)
         history_mean_std = sg.optimize()
-        plt.plot(history_mean_std["mu"], "black")
-        plt.plot(history_mean_std["sigma"], "red")
-        print(np.min(history_mean_std["mu"]), np.min(history_mean_std["sigma"]))
-        print(np.max(history_mean_std["mu"]), np.max(history_mean_std["sigma"]))
+        stat_mean[t] = np.array(history_mean_std["mu"])
+        stat_std[t] = np.array(history_mean_std["sigma"])
+    plt.plot(np.median(stat_mean, 0), "black")
+    plt.plot(np.median(stat_std, 0), "red")
     plt.yscale("log")
     plt.show()
