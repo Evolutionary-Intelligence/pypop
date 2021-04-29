83f0e92db4bf2dab66b7a8934f5c33e887ae016d
Evolutionary-Intelligence <duanqq077@163.com>
2021-04-29 23:09:58 +0800

    Test base function rastrigin

--- benchmarks/test_base_functions.py
+++ benchmarks/test_base_functions.py
@@ -122,6 +122,14 @@ class TestBaseFunctions(unittest.TestCase):
                 self.assertTrue(sample.compare(func, ndim, get_y_ackley(ndim - 2), atol=0.001))
             self.assertTrue(sample.check_origin(func))
 
+    def test_rastrigin(self):
+        sample = TestCases()
+        rastrigin_object = Rastrigin()
+        for func in [rastrigin, rastrigin_object]:
+            for ndim in range(2, 8):
+                self.assertTrue(sample.compare(func, ndim, get_y_rastrigin(ndim - 2)))
+            self.assertTrue(sample.check_origin(func))
+
 
 if __name__ == '__main__':
     unittest.main()
