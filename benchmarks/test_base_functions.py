459ddbb6d9b7ad87b453c458d6e44f9a0e747c89
Evolutionary-Intelligence <duanqq077@163.com>
2021-05-14 21:34:11 +0800

    Test base function schwefel222

--- benchmarks/test_base_functions.py
+++ benchmarks/test_base_functions.py
@@ -87,6 +87,13 @@ class TestBaseFunctions(unittest.TestCase):
                 self.assertTrue(sample.compare(func, ndim, get_y_step(ndim - 1)))
             self.assertTrue(sample.check_origin(func))
 
+    def test_schwefel222(self):
+        sample = TestCases()
+        for func in [schwefel222, Schwefel222()]:
+            for ndim in range(1, 8):
+                self.assertTrue(sample.compare(func, ndim, get_y_schwefel222(ndim - 1)))
+            self.assertTrue(sample.check_origin(func))
+
     def test_rosenbrock(self):
         sample = TestCases()
         for func in [rosenbrock, Rosenbrock()]:
