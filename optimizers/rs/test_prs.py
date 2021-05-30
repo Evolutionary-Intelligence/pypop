ae5a567316e1840c62f82d99bac84ee785c26fbb
Evolutionary-Intelligence <duanqq077@163.com>
2021-05-30 22:18:44 +0800

    Update optimizers/rs/test_prs.py

--- optimizers/rs/test_prs.py
+++ optimizers/rs/test_prs.py
@@ -1,44 +1,32 @@
 import unittest
 import numpy as np
+import time
 
-from benchmarks.base_functions import ellipsoid
+from benchmarks.base_functions import ellipsoid, rosenbrock, rastrigin
 from optimizers.rs.prs import PRS
 
 
 class TestPRS(unittest.TestCase):
     def test_run(self):
+        start_run = time.time()
         ndim_problem = 1000
-        problem = {'fitness_function': ellipsoid,
-                   'ndim_problem': ndim_problem,
-                   'lower_boundary': np.zeros((ndim_problem,)),
-                   'upper_boundary': 2 * np.ones((ndim_problem,))}
-        options = {'max_function_evaluations': 1e6,
-                   'fitness_threshold': 1e-10,
-                   'seed_rng': 0,
-                   'sampling_distribution': 0,
-                   'x': np.ones((ndim_problem,)),
-                   'record_options': {'record_fitness': True,
-                                      'frequency_record_fitness': 2000}}
-
-        print('*' * 7 + ' Run 1 ' + '*' * 7)
-        prs = PRS(problem, options)
-        print('sampling_distribution: ' + str(prs.sampling_distribution))
-        results = prs.optimize()
-        print(results)
-
-        print('*' * 7 + ' Run 2 ' + '*' * 7)
-        del options['x']
-        prs = PRS(problem, options)
-        print('sampling_distribution: ' + str(prs.sampling_distribution))
-        results = prs.optimize()
-        print(results)
-
-        print('*' * 7 + ' Run 3 ' + '*' * 7)
-        del options['sampling_distribution']
-        prs = PRS(problem, options)
-        print('sampling_distribution: ' + str(prs.sampling_distribution))
-        results = prs.optimize()
-        print(results)
+        for f in [ellipsoid, rosenbrock, rastrigin]:
+            print('*' * 7 + ' ' + f.__name__ + ' ' + '*' * 7)
+            problem = {'fitness_function': f,
+                       'ndim_problem': ndim_problem,
+                       'lower_boundary': -5 * np.zeros((ndim_problem,)),
+                       'upper_boundary': 5 * np.ones((ndim_problem,))}
+            options = {'max_function_evaluations': 2e6,
+                       'fitness_threshold': 1e-10,
+                       'seed_rng': 0,
+                       'x': 4 * np.ones((ndim_problem,)),
+                       'verbose_options': {'frequency_verbose': 200000},
+                       'record_options': {'record_fitness': True,
+                                          'frequency_record_fitness': 200000}}
+            prs = PRS(problem, options)
+            results = prs.optimize()
+            print(results)
+            print('*** Runtime: {:7.5e}'.format(time.time() - start_run))
 
 
 if __name__ == '__main__':
