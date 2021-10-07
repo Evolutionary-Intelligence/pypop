6763b7a5d9f32e7f82b1df4564df582f4f1035c8
Evolutionary-Intelligence <duanqq077@163.com>
2021-10-07 21:45:23 +0800

    Simplify optimizer class NES

--- optimizers/nes/nes.py
+++ optimizers/nes/nes.py
@@ -10,17 +10,17 @@ class NES(Optimizer):
     ---------
     Wierstra, D., Schaul, T., Glasmachers, T., Sun, Y., Peters, J. and Schmidhuber, J., 2014.
     Natural evolution strategies.
-    Journal of Machine Learning Research, 15(1), pp.949-980.
+    Journal of Machine Learning Research, 15(27), pp.949-980.
     https://jmlr.org/papers/v15/wierstra14a.html
     """
     def __init__(self, problem, options):
-        problem['_is_maximization'] = True
+        problem['_is_maximization'] = True  # mandatory setting for NES
         Optimizer.__init__(self, problem, options)
-        if self.n_individuals is None:  # population size
+        if self.n_individuals is None:  # population size (λ: lambda)
             self.n_individuals = 4 + int(np.floor(3 * np.log(self.ndim_problem)))
-        self.eta_mu = options.get('eta_mu')  # learning rate of mean of Gaussian search distribution
-        self.eta_sigma = options.get('eta_sigma')  # learning rate of std of Gaussian search distribution
-        self.mu = options.get('mu')
+        self.mu = options.get('mu')  # mean of Gaussian search distribution (μ)
+        self.eta_mu = options.get('eta_mu')  # learning rate of mean (η_μ)
+        self.eta_sigma = options.get('eta_sigma')  # learning rate of std (η_σ)
         self.n_generations = options.get('n_generations', 0)
 
     def initialize(self):
@@ -46,13 +46,17 @@ class NES(Optimizer):
     def _update_distribution(self):
         raise NotImplementedError
 
-    def _print_verbose_info(self, y=None):
-        if self.verbose_options['verbose']:
-            if not self.n_generations % self.verbose_options['frequency_verbose']:
-                info = '  * Generation {:d}: best_so_far_y {:7.5e}, min(y) {:7.5e} & Evaluations {:d}'
-                print(info.format(self.n_generations, -self.best_so_far_y, np.min(y), self.n_function_evaluations))
+    def _print_verbose_info(self, y):
+        if self.verbose and (not self.n_generations % self.verbose_frequency):
+            info = '  * Generation {:d}: best_so_far_y {:7.5e}, min(y) {:7.5e} & Evaluations {:d}'
+            print(info.format(self.n_generations, -self.best_so_far_y, np.min(y), self.n_function_evaluations))
 
     def _collect_results(self):
         results = Optimizer._collect_results(self)
         results['n_generations'] = self.n_generations
         return results
+
+    def _fitness_shaping(self):
+        base = np.log(self.n_individuals / 2 + 1)
+        utilities = np.array([max(0, base - np.log(k)) for k in (np.arange(self.n_individuals) + 1)])
+        return utilities / np.sum(utilities) - (1 / self.n_individuals)
