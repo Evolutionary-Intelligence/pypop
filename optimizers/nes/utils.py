d0506bc18b67fe94d34f24720f1fd032e63c62a8
Evolutionary-Intelligence <duanqq077@163.com>
2021-10-07 20:24:39 +0800

    Delete optimizers/nes/utils.py

--- optimizers/nes/utils.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-
-
-def fitness_shaping(y):
-    n_individuals = y.size
-    tmp = np.log(n_individuals / 2 + 1)
-    tmp = [max(0, tmp - np.log(k)) for k in range(1, n_individuals + 1)]
-    return tmp / np.sum(tmp) - (1 / n_individuals)
