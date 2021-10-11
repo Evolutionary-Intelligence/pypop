fb2d4f9c801707ee501c58bb94106e8be81261ba
Evolutionary-Intelligence <duanqq077@163.com>
2021-10-12 04:38:18 +0800

    Fix serious bug for SNES

--- optimizers/nes/test_snes.py
+++ optimizers/nes/test_snes.py
@@ -20,7 +20,7 @@ class TestSNES(unittest.TestCase):
                        'fitness_threshold': 1e-10,
                        'seed_rng': 0,
                        'x': 4 * np.ones((ndim_problem,)),
-                       'verbose_frequency': 200000,
+                       'verbose_frequency': 200,
                        'record_fitness': True,
                        'record_fitness_frequency': 200000}
             solver = Solver(problem, options)
