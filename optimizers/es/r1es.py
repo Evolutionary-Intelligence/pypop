12bb0a9aa5caf14cf23a5dcd7fe8b1cb8b9c71db
Evolutionary-Intelligence <duanqq077@163.com>
2021-10-19 08:58:00 +0800

    Add optimizer class R1ES

---
+++ optimizers/es/r1es.py
@@ -0,0 +1,84 @@
+import time
+import numpy as np
+
+from optimizers.es.es import ES
+
+
+class R1ES(ES):
+    """Rank One Evolution Strategy (R1ES).
+
+    Reference
+    ---------
+    Li, Z. and Zhang, Q., 2018.
+    A simple yet efficient evolution strategy for large-scale black-box optimization.
+    IEEE Transactions on Evolutionary Computation, 22(5), pp.637-646.
+    https://ieeexplore.ieee.org/abstract/document/8080257
+    """
+    def __init__(self, problem, options):
+        ES.__init__(self, problem, options)
+        self.c_cov = 1 / (3 * np.sqrt(self.ndim_problem) + 5)  # for Line 5 in Algorithm 1
+        self.c = 2 / (self.ndim_problem + 7)  # for Line 12 in Algorithm 1
+        self.c_s = 0.3  # for Line 15 in Algorithm 1
+        self.q_star = 0.3  # for Line 15 in Algorithm 1
+        self.d_sigma = 1  # for Line 16 in Algorithm 1
+        self._p_1 = 1 - self.c  # for Line 12 in Algorithm 1
+        self._p_2 = np.sqrt(self.c * (2 - self.c) * self.mu_eff)  # for Line 12 in Algorithm 1
+        self._rr = np.arange(self.n_parents * 2) + 1  # for rule-based success rule
+
+    def initialize(self, args=None):
+        x = np.empty((self.n_individuals, self.ndim_problem))  # population
+        mu = self._initialize_mu()  # mean of Gaussian search distribution
+        p = np.zeros((self.ndim_problem,))  # principal search direction
+        s = 0  # cumulative rank rate
+        y = np.tile(self._evaluate_fitness(mu, args), (self.n_individuals,))  # fitness
+        return x, mu, p, s, y
+
+    def iterate(self, x=None, mu=None, p=None, s=None, y=None, args=None):
+        for k in range(self.n_individuals):  # for Line 3 in Algorithm 1
+            z = self.rng.standard_normal((self.ndim_problem,))  # for Line 4 in Algorithm 1
+            r = self.rng.standard_normal()  # for Line 4 in Algorithm 1
+            # for Line 5 in Algorithm 1
+            x[k] = mu + self.sigma * (np.sqrt(1 - self.c_cov) * z + np.sqrt(self.c_cov) * r * p)
+            y[k] = self._evaluate_fitness(x[k], args)
+        return x, y
+
+    def _update_distribution(self, x=None, mu=None, p=None, s=None, y=None, y_bak=None):
+        order = np.argsort(y)
+        y = np.sort(y)
+        mu_w = np.zeros((self.ndim_problem,))
+        for k in range(self.n_parents):
+            mu_w += self.w[k] * x[order[k]]
+        p = self._p_1 * p + self._p_2 * (mu_w - mu) / self.sigma  # for Line 12 in Algorithm 1
+        mu = mu_w  # for Line 11 in Algorithm 1
+        # for rank-based adaptation of mutation strength
+        f = np.hstack((y_bak[:self.n_parents], y[:self.n_parents]))
+        r = np.argsort(f)
+        rr = self._rr[r < self.n_parents] - self._rr[r >= self.n_parents]
+        q = np.dot(self.w, rr) / self.n_parents  # for Line 14 in Algorithm 1
+        s = (1 - self.c_s) * s + self.c_s * (q - self.q_star)  # for Line 15 in Algorithm 1
+        self.sigma *= np.exp(s / self.d_sigma)  # for Line 16 in Algorithm 1
+        return mu, p, s
+
+    def optimize(self, fitness_function=None, args=None):  # for all generations (iterations)
+        self.start_time = time.time()
+        fitness = []  # store all fitness generated during evolution
+        if fitness_function is not None:
+            self.fitness_function = fitness_function
+        x, mu, p, s, y = self.initialize(args)
+        fitness.append(y[0])
+        while True:
+            y_bak = np.copy(y)
+            x, y = self.iterate(x, mu, p, s, y, args)  # sample and evaluate offspring population
+            if self.record_fitness:
+                fitness.extend(y.tolist())
+            if self._check_terminations():
+                break
+            mu, p, s = self._update_distribution(x, mu, p, s, y, y_bak)
+            self.n_generations += 1
+            self._print_verbose_info(y)
+        if self.record_fitness:
+            self._compress_fitness(fitness[:self.n_function_evaluations])
+        results = self._collect_results()
+        results['mu'] = mu
+        results['p'] = p
+        return results
---
+++ optimizers/es/test_r1es.py
@@ -0,0 +1,33 @@
+import unittest
+import time
+import numpy as np
+
+from benchmarks.base_functions import ellipsoid, rosenbrock, rastrigin
+from optimizers.es.r1es import R1ES as Solver
+
+
+class TestR1ES(unittest.TestCase):
+    def test_optimize(self):
+        start_run = time.time()
+        ndim_problem = 1000
+        for f in [ellipsoid, rosenbrock, rastrigin]:
+            print('*' * 7 + ' ' + f.__name__ + ' ' + '*' * 7)
+            problem = {'fitness_function': f,
+                       'ndim_problem': ndim_problem,
+                       'lower_boundary': -5 * np.zeros((ndim_problem,)),
+                       'upper_boundary': 5 * np.ones((ndim_problem,))}
+            options = {'max_function_evaluations': 2e6,
+                       'fitness_threshold': 1e-10,
+                       'seed_rng': 0,
+                       'x': 4 * np.ones((ndim_problem,)),
+                       'verbose_frequency': 200,
+                       'record_fitness': True,
+                       'record_fitness_frequency': 200000}
+            solver = Solver(problem, options)
+            results = solver.optimize()
+            print(results)
+            print('*** Runtime: {:7.5e}'.format(time.time() - start_run))
+
+
+if __name__ == '__main__':
+    unittest.main()
