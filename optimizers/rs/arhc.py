2fe30c78571b42cfdcfc7567b074abdae15834e7
Evolutionary-Intelligence <duanqq077@163.com>
2021-12-01 01:26:26 +0800

    Standardize optimizer class ARHC

--- optimizers/rs/arhc.py
+++ optimizers/rs/arhc.py
@@ -16,17 +16,17 @@ class ARHC(RHC):
     def __init__(self, problem, options):
         RHC.__init__(self, problem, options)
         self.temperature = options.get('temperature', 1.0)
-        self.parent_x = np.copy(self.best_so_far_x)
-        self.parent_y = np.copy(self.best_so_far_y)
+        self._parent_x = np.copy(self.best_so_far_x)
+        self._parent_y = np.copy(self.best_so_far_y)
 
     def iterate(self):  # draw sample via mutating the parent individual
         noise = self.rng_optimization.standard_normal(size=(self.ndim_problem,))
-        return self.parent_x + self.global_std * noise
+        return self._parent_x + self.global_std * noise
 
     def _evaluate_fitness(self, x, args=None):
         y = RHC._evaluate_fitness(self, x, args)
         # update parent solution and fitness
-        diff = y - self.parent_y
+        diff = y - self._parent_y
         if (diff < 0) or (self.rng_optimization.random() < np.exp(-diff / self.temperature)):
-            self.parent_x, self.parent_y = np.copy(x), y
+            self._parent_x, self._parent_y = np.copy(x), y
         return y
