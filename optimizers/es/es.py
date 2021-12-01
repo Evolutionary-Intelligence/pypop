cbf459521a8dd0732e6f394f23979021924779f0
Evolutionary-Intelligence <duanqq077@163.com>
2021-12-01 22:02:49 +0800

    Standardize optimizer class ES

--- optimizers/es/es.py
+++ optimizers/es/es.py
@@ -8,6 +8,11 @@ class ES(Optimizer):
 
     Reference
     ---------
+    Hansen, N., Arnold, D.V. and Auger, A., 2015.
+    Evolution strategies.
+    In Springer Handbook of Computational Intelligence (pp. 871-898). Springer, Berlin, Heidelberg.
+    https://link.springer.com/chapter/10.1007%2F978-3-662-43505-2_44
+
     http://www.scholarpedia.org/article/Evolution_strategies
 
     Beyer, H.G. and Schwefel, H.P., 2002.
@@ -25,20 +30,18 @@ class ES(Optimizer):
         Optimizer.__init__(self, problem, options)
         if self.n_individuals is None:  # offspring population size (λ: lambda)
             self.n_individuals = 4 + int(np.floor(3 * np.log(self.ndim_problem)))
-        # to avoid notation confusion in μ, use `n_parents` and `mu` to represent
-        #   parent population size and mean of Gaussian search distribution, respectively
         if self.n_parents is None:  # parent population size (μ: mu)
             self.n_parents = int(self.n_individuals / 2)
             w_base, w = np.log((self.n_individuals + 1) / 2), np.log(np.arange(self.n_parents) + 1)
             self.w = (w_base - w) / (self.n_parents * w_base - np.sum(w))
             self.mu_eff = 1 / np.sum(np.power(self.w, 2))  # μ_eff
-        self.mu = options.get('mu')  # mean of Gaussian search distribution (μ)
-        if self.mu is None:  # 'mu' has priority over 'x'
-            self.mu = options.get('x')
-        self.sigma = options.get('sigma', 0.1)  # global step-size (σ)
-        self.eta_mu = options.get('eta_mu')  # learning rate of mean (η_μ)
-        self.eta_sigma = options.get('eta_sigma')  # learning rate of std (η_σ)
-        self.n_generations = options.get('n_generations', 0)
+        self.mean = options.get('mean')  # mean of Gaussian search distribution
+        if self.mean is None:  # 'mean' has priority over 'x'
+            self.mean = options.get('x')
+        self.sigma = options.get('sigma')  # global step-size (σ)
+        self.eta_mean = options.get('eta_mean')  # learning rate of mean
+        self.eta_sigma = options.get('eta_sigma')  # learning rate of std
+        self._n_generations = 0
 
     def initialize(self):
         raise NotImplementedError
@@ -46,23 +49,23 @@ class ES(Optimizer):
     def iterate(self):
         raise NotImplementedError
 
-    def _initialize_mu(self):
-        if self.mu is None:
+    def _initialize_mean(self):
+        if self.mean is None:
             rng = self.rng_initialization
-            mu = rng.uniform(self.initial_lower_boundary, self.initial_upper_boundary)
+            mean = rng.uniform(self.initial_lower_boundary, self.initial_upper_boundary)
         else:
-            mu = np.copy(self.mu)
-        return mu
+            mean = np.copy(self.mean)
+        return mean
 
     def _print_verbose_info(self, y):
-        if self.verbose and (not self.n_generations % self.verbose_frequency):
+        if self.verbose and (not self._n_generations % self.verbose_frequency):
             best_so_far_y = -self.best_so_far_y if self._is_maximization else self.best_so_far_y
             info = '  * Generation {:d}: best_so_far_y {:7.5e}, min(y) {:7.5e} & Evaluations {:d}'
-            print(info.format(self.n_generations, best_so_far_y, np.min(y), self.n_function_evaluations))
+            print(info.format(self._n_generations, best_so_far_y, np.min(y), self.n_function_evaluations))
 
     def _collect_results(self, fitness):
         results = Optimizer._collect_results(self, fitness)
-        results['mu'] = self.mu
+        results['mean'] = self.mean
         results['sigma'] = self.sigma
-        results['n_generations'] = self.n_generations
+        results['_n_generations'] = self._n_generations
         return results
