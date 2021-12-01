df44f4c12623925f04e173e92b58386d28a2bad0
Evolutionary-Intelligence <duanqq077@163.com>
2021-12-01 22:05:13 +0800

    Standardize optimizer class SAES

--- optimizers/es/saes.py
+++ optimizers/es/saes.py
@@ -24,35 +24,36 @@ class SAES(ES):
 
     def initialize(self):
         x = np.empty((self.n_individuals, self.ndim_problem))  # offspring population
-        mu = self._initialize_mu()  # mean of Gaussian search distribution
-        sigmas = np.ones((self.n_individuals,))  # step-sizes for each offspring
+        mean = self._initialize_mean()  # mean of Gaussian search distribution
+        sigmas = np.ones((self.n_individuals,))  # step-sizes for all offspring
         y = np.empty((self.n_individuals,))  # fitness (no evaluation)
-        return x, mu, sigmas, y
+        return x, mean, sigmas, y
 
-    def iterate(self, x=None, mu=None, sigmas=None, y=None, args=None):
+    def iterate(self, x=None, mean=None, sigmas=None, y=None, args=None):
         for k in range(self.n_individuals):  # sample population
             if self._check_terminations():
                 return x, sigmas, y
             sigmas[k] = self.sigma * np.exp(self.eta_sigma * self.rng_optimization.standard_normal())
-            x[k] = mu + sigmas[k] * self.rng_optimization.standard_normal((self.ndim_problem,))
+            x[k] = mean + sigmas[k] * self.rng_optimization.standard_normal((self.ndim_problem,))
             y[k] = self._evaluate_fitness(x[k], args)
         return x, sigmas, y
 
     def optimize(self, fitness_function=None, args=None):  # for all generations (iterations)
         ES.optimize(self, fitness_function)
         fitness = []  # store all fitness generated during evolution
-        x, mu, sigmas, y = self.initialize()
+        x, mean, sigmas, y = self.initialize()
         while True:
-            x, sigmas, y = self.iterate(x, mu, sigmas, y, args)  # sample and evaluate offspring population
+            # sample and evaluate offspring population
+            x, sigmas, y = self.iterate(x, mean, sigmas, y, args)
             if self.record_fitness:
                 fitness.extend(y.tolist())
             if self._check_terminations():
                 break
             order = np.argsort(y)[:self.n_parents]
-            mu = np.mean(x[order], axis=0)
+            mean = np.mean(x[order], axis=0)
             self.sigma = np.mean(sigmas[order])
-            self.n_generations += 1
+            self._n_generations += 1
             self._print_verbose_info(y)
         results = self._collect_results(fitness)
-        results['mu'] = mu
+        results['mean'] = mean
         return results
--- optimizers/es/test_saes.py
+++ optimizers/es/test_saes.py
@@ -19,7 +19,8 @@ class TestSAES(unittest.TestCase):
             options = {'max_function_evaluations': 2e6,
                        'fitness_threshold': 1e-10,
                        'seed_rng': 0,
-                       'x': 4 * np.ones((ndim_problem,)),
+                       'x': 4 * np.ones((ndim_problem,)),  # mean
+                       'sigma': 0.1,
                        'verbose_frequency': 200,
                        'record_fitness': True,
                        'record_fitness_frequency': 200000}
