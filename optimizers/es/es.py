678c69ea6587afc81a671836786396b635fb7310
Evolutionary-Intelligence <duanqq077@163.com>
2021-12-02 01:33:58 +0800

    Standardize optimizer class ES

--- optimizers/es/es.py
+++ optimizers/es/es.py
@@ -32,9 +32,10 @@ class ES(Optimizer):
             self.n_individuals = 4 + int(np.floor(3 * np.log(self.ndim_problem)))
         if self.n_parents is None:  # parent population size (μ: mu)
             self.n_parents = int(self.n_individuals / 2)
-            w_base, w = np.log((self.n_individuals + 1) / 2), np.log(np.arange(self.n_parents) + 1)
-            self.w = (w_base - w) / (self.n_parents * w_base - np.sum(w))
-            self.mu_eff = 1 / np.sum(np.power(self.w, 2))  # μ_eff
+            if self.n_parents > 0:
+                w_base, w = np.log((self.n_individuals + 1) / 2), np.log(np.arange(self.n_parents) + 1)
+                self.w = (w_base - w) / (self.n_parents * w_base - np.sum(w))
+                self.mu_eff = 1 / np.sum(np.power(self.w, 2))  # μ_eff
         self.mean = options.get('mean')  # mean of Gaussian search distribution
         if self.mean is None:  # 'mean' has priority over 'x'
             self.mean = options.get('x')
