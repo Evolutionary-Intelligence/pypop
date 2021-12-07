e38ccb59cea3850f07b1c5373e47c7ea777e3e29
Evolutionary-Intelligence <duanqq077@163.com>
2021-12-07 23:30:00 +0800

    Standardize optimizer class R1ES

--- optimizers/es/test_r1es.py
+++ optimizers/es/test_r1es.py
@@ -20,6 +20,7 @@ class TestR1ES(unittest.TestCase):
                        'fitness_threshold': 1e-10,
                        'seed_rng': 0,
                        'x': 4 * np.ones((ndim_problem,)),
+                       'sigma': 0.1,
                        'verbose_frequency': 200,
                        'record_fitness': True,
                        'record_fitness_frequency': 200000}
