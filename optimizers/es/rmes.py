76de4e8e50452ec65b0685356cc4623a9dff5abf
Evolutionary-Intelligence <duanqq077@163.com>
2021-12-08 21:10:28 +0800

    Standardize optimizer class RMES

--- optimizers/es/rmes.py
+++ optimizers/es/rmes.py
@@ -14,55 +14,54 @@ class RMES(R1ES):
         self._b = np.sqrt(self.c_cov)  # for Line 4 in Algorithm 3
 
     def initialize(self, args=None):
-        x, mu, p, s, y = R1ES.initialize(self, args)
+        x, mean, p, s, y = R1ES.initialize(self, args)
         mp = np.zeros((self.n_evolution_paths, self.ndim_problem))  # multiple evolution paths
         t_hat = np.zeros((self.n_evolution_paths,))
-        return x, mu, p, s, mp, t_hat, y
+        return x, mean, p, s, mp, t_hat, y
 
-    def iterate(self, x=None, mu=None, p=None, s=None, mp=None, t_hat=None, y=None, args=None):
+    def iterate(self, x=None, mean=None, p=None, s=None, mp=None, t_hat=None, y=None, args=None):
         for k in range(self.n_individuals):
+            if self._check_terminations():
+                return x, y
             z = self.rng.standard_normal((self.ndim_problem,))
             sum_p = np.zeros((self.ndim_problem,))
             for i in np.arange(self.n_evolution_paths) + 1:
                 r = self.rng.standard_normal()
                 sum_p += np.power(self._a, self.n_evolution_paths - i) * r * mp[i - 1]
-            x[k] = mu + self.sigma * (self._a_m * z + self._b * sum_p)
+            x[k] = mean + self.sigma * (self._a_m * z + self._b * sum_p)
             y[k] = self._evaluate_fitness(x[k], args)
         return x, y
 
-    def _update_distribution(self, x=None, mu=None, p=None, s=None, mp=None, t_hat=None, y=None, y_bak=None):
-        mu, p, s = R1ES._update_distribution(self, x, mu, p, s, y, y_bak)
+    def _update_distribution(self, x=None, mean=None, p=None, s=None, mp=None, t_hat=None, y=None, y_bak=None):
+        mean, p, s = R1ES._update_distribution(self, x, mean, p, s, y, y_bak)
         # update multiple evolution paths
         t_min = np.min(np.diff(t_hat))
-        if (t_min > self.generation_gap) or (self.n_generations < self.n_evolution_paths):
+        if (t_min > self.generation_gap) or (self._n_generations < self.n_evolution_paths):
             for i in range(self.n_evolution_paths - 1):
-                mp[i] = mp[i + 1]
-                t_hat[i] = t_hat[i + 1]
+                mp[i], t_hat[i] = mp[i + 1], t_hat[i + 1]
         else:
             i_apostrophe = np.argmin(np.diff(t_hat))
             for i in range(i_apostrophe, self.n_evolution_paths - 1):
-                mp[i] = mp[i + 1]
-                t_hat[i] = t_hat[i + 1]
-        mp[-1] = p
-        t_hat[-1] = self.n_generations
-        return mu, p, s, mp, t_hat
+                mp[i], t_hat[i] = mp[i + 1], t_hat[i + 1]
+        mp[-1], t_hat[-1] = p, self._n_generations
+        return mean, p, s, mp, t_hat
 
     def optimize(self, fitness_function=None, args=None):  # for all generations (iterations)
         ES.optimize(self, fitness_function)
         fitness = []  # store all fitness generated during evolution
-        x, mu, p, s, mp, t_hat, y = self.initialize(args)
+        x, mean, p, s, mp, t_hat, y = self.initialize(args)
         fitness.append(y[0])
         while True:
-            y_bak = np.copy(y)
-            x, y = self.iterate(x, mu, p, s, mp, t_hat, y, args)  # sample and evaluate offspring population
+            y_bak = np.sort(y)
+            x, y = self.iterate(x, mean, p, s, mp, t_hat, y, args)  # sample and evaluate offspring population
             if self.record_fitness:
                 fitness.extend(y.tolist())
             if self._check_terminations():
                 break
-            mu, p, s, mp, t_hat = self._update_distribution(x, mu, p, s, mp, t_hat, y, y_bak)
-            self.n_generations += 1
+            mean, p, s, mp, t_hat = self._update_distribution(x, mean, p, s, mp, t_hat, y, y_bak)
+            self._n_generations += 1
             self._print_verbose_info(y)
         results = self._collect_results(fitness)
-        results['mu'] = mu
+        results['mean'] = mean
         results['p'] = p
         return results
