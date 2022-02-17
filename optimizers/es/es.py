40bfa8c629e46ee7cdb3de3281c415ea0e29f5c6
Evolutionary-Intelligence <78018333+Evolutionary-Intelligence@users.noreply.github.com>
2022-02-17 23:52:36 +0800

    Update optimizer class ES

--- optimizers/es/es.py
+++ optimizers/es/es.py
@@ -22,7 +22,7 @@ class ES(Optimizer):
 
     Rechenberg, I., 1989.
     Evolution strategy: Nature’s way of optimization.
-    In Optimization: Methods and applications, possibilities and limitations (pp. 106-126).
+    In Optimization: Methods and Applications, Possibilities and Limitations (pp. 106-126).
     Springer, Berlin, Heidelberg.
     https://link.springer.com/chapter/10.1007/978-3-642-83814-9_6
 
