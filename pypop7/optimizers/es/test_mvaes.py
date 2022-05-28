cbb5b30cb64a9767881a78c16f5c8082e60e67e6
Evolutionary-Intelligence <duanqq077@163.com>
2022-05-28 14:19:43 +0800

    Update optimizer class MVAES

--- pypop7/optimizers/es/test_mvaes.py
+++ pypop7/optimizers/es/test_mvaes.py
@@ -29,7 +29,8 @@ class TestMVAES(unittest.TestCase):
                        'sigma': 0.1,
                        'verbose_frequency': 200,
                        'record_fitness': True,
-                       'record_fitness_frequency': 200000}
+                       'record_fitness_frequency': 200000,
+                       'n_parents': 1}
             solver = Solver(problem, options)
             results = solver.optimize()
             print(results)
