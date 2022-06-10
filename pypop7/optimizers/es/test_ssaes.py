49459c4fb3821cd5b2cf63290e1ee812a9571811
Evolutionary-Intelligence <duanqq077@163.com>
2022-06-10 12:51:46 +0800

    Add optimizer class SSAES

--- pypop7/optimizers/es/test_ssaes.py
+++ pypop7/optimizers/es/test_ssaes.py
@@ -7,8 +7,8 @@ import unittest
 import time
 import numpy as np
 
-from benchmarks.base_functions import ellipsoid, rosenbrock, rastrigin
-from optimizers.es.ssaes import SSAES as Solver
+from pypop7.benchmarks.base_functions import ellipsoid, rosenbrock, rastrigin
+from pypop7.optimizers.es.ssaes import SSAES as Solver
 
 
 class TestSSAES(unittest.TestCase):
