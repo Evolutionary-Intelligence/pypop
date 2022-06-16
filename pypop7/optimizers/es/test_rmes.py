c9185f24d4fa18b0722fd79ae9746d5749ba038e
Evolutionary-Intelligence <duanqq077@163.com>
2022-06-16 15:52:34 +0800

    Add pypop7/optimizers/es/test_rmes.py

--- pypop7/optimizers/es/test_rmes.py
+++ pypop7/optimizers/es/test_rmes.py
@@ -7,8 +7,8 @@ import unittest
 import time
 import numpy as np
 
-from benchmarks.base_functions import ellipsoid, rosenbrock, rastrigin
-from optimizers.es.rmes import RMES as Solver
+from pypop7.benchmarks.base_functions import ellipsoid, rosenbrock, rastrigin
+from pypop7.optimizers.es.rmes import RMES as Solver
 
 
 class TestRMES(unittest.TestCase):
