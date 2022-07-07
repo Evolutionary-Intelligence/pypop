1698bec2023504731df909eae14cfe7bbb866d9a
Evolutionary-Intelligence <duanqq077@163.com>
2022-07-07 19:06:55 +0800

    Add pypop7/benchmarks/test_base_functions.py

--- pypop7/benchmarks/test_base_functions.py
+++ pypop7/benchmarks/test_base_functions.py
@@ -1,27 +1,24 @@
 import unittest
 
-import numpy as np
-
-from benchmarks.base_functions import _squeeze_and_check
-from benchmarks.base_functions import *
+from pypop7.benchmarks.base_functions import *
 from test_cases import *
 
 
 class TestBaseFunctions(unittest.TestCase):
     def test_squeeze_and_check(self):
-        self.assertEqual(_squeeze_and_check(0), np.array([0]))
-        self.assertEqual(_squeeze_and_check(np.array(0)), np.array([0]))
+        self.assertEqual(squeeze_and_check(0), np.array([0]))
+        self.assertEqual(squeeze_and_check(np.array(0)), np.array([0]))
         x1 = np.array([0.7])
-        self.assertEqual(_squeeze_and_check(x1), x1)
+        self.assertEqual(squeeze_and_check(x1), x1)
         x2 = np.array([0.0, 1.0])
-        self.assertTrue(np.allclose(_squeeze_and_check(x2), x2))
+        self.assertTrue(np.allclose(squeeze_and_check(x2), x2))
         x3 = np.arange(6).reshape(2, 3)
         with self.assertRaisesRegex(TypeError, 'The number+'):
-            _squeeze_and_check(x3)
+            squeeze_and_check(x3)
         with self.assertRaisesRegex(TypeError, 'The size should > 1+'):
-            _squeeze_and_check(x1, True)
+            squeeze_and_check(x1, True)
         with self.assertRaisesRegex(TypeError, 'the size should != 0.'):
-            _squeeze_and_check([])
+            squeeze_and_check([])
 
     def test_sphere(self):
         sample = TestCases()
