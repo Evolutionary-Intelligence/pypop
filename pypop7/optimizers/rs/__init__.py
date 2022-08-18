0adb3fac98356a2e2367ecaa5414ee372ef5f48c
Evolutionary-Intelligence <duanqq077@163.com>
2022-08-18 18:02:11 +0800

    Add pypop7/optimizers/rs/__init__.py

---
+++ pypop7/optimizers/rs/__init__.py
@@ -0,0 +1,8 @@
+from pypop7.optimizers.rs.rs import RS
+from pypop7.optimizers.rs.prs import PRS
+from pypop7.optimizers.rs.rhc import RHC
+from pypop7.optimizers.rs.arhc import ARHC
+from pypop7.optimizers.rs.srs import SRS
+
+
+__all__ = [RS, PRS, RHC, ARHC, SRS]
