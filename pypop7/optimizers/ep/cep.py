7abd3a50072f7b59634b95fcdf6f1041244947ba
Evolutionary-Intelligence <duanqq077@163.com>
2022-08-21 16:08:42 +0800

    Add pypop7/optimizers/ep/cep.py

--- pypop7/optimizers/ep/cep.py
+++ pypop7/optimizers/ep/cep.py
@@ -1,73 +1,77 @@
 import numpy as np
-import random
 
 from pypop7.optimizers.ep.ep import EP
 
 
 class CEP(EP):
-    """Classical Evolutionary Programming(CEP)
-        Reference
-        -----------
-        X. Yao, Y, Liu, G. Lin
-        Evolutionary Programming Made Faster
-        IEEE TRANSACTIONS ON EVOLUTIONARY COMPUTATION, VOL. 3, NO. 2, JULY 1999
-        https://ieeexplore.ieee.org/abstract/document/771163
+    """Classical Evolutionary Programming with self-adaptive mutation (CEP).
+
+    References
+    ----------
+    Yao, X., Liu, Y. and Lin, G., 1999.
+    Evolutionary programming made faster.
+    IEEE Transactions on Evolutionary Computation, 3(2), pp.82-102.
+    https://ieeexplore.ieee.org/abstract/document/771163
     """
     def __init__(self, problem, options):
         EP.__init__(self, problem, options)
-        self.t = 1.0 / np.sqrt(2 * np.sqrt(self.ndim_problem))
-        self.t1 = 1.0 / np.sqrt(2 * self.ndim_problem)
-        self.q = options.get('q', 10)
-        self.init_n = options.get('init_n', 3)
+        self.sigma = options.get('sigma')  # initial global step-size
+        self.q = options.get('q', 10)  # number of opponents for pairwise comparisons
+        # two learning rate factors of individual step-size
+        self.tau = options.get('tau', 1.0 / np.sqrt(2.0*np.sqrt(self.ndim_problem)))
+        self.tau_apostrophe = options.get('tau_apostrophe', 1.0 / np.sqrt(2.0*self.ndim_problem))
 
-    def initialize(self, is_restart=False):
-        x = np.empty((self.n_individuals, self.ndim_problem))
+    def initialize(self, args=None):
+        x = self.rng_initialization.uniform(self.initial_lower_boundary, self.initial_upper_boundary,
+                                            size=(self.n_individuals, self.ndim_problem))
+        sigmas = self.sigma*np.ones((self.n_individuals, self.ndim_problem))  # eta (η)
         y = np.empty((self.n_individuals,))
-        n = self.init_n * np.ones((self.n_individuals, self.ndim_problem))
         for i in range(self.n_individuals):
-            x[i] = self._initialize_mean()
-            y[i] = self._evaluate_fitness(x[i])
-        return x, y, n
+            if self._check_terminations():
+                break
+            y[i] = self._evaluate_fitness(x[i], args)
+        offspring_x = np.empty((self.n_individuals, self.ndim_problem))
+        offspring_sigmas = np.empty((self.n_individuals, self.ndim_problem))  # eta (η)
+        offspring_y = np.empty((self.n_individuals,))
+        return x, sigmas, y, offspring_x, offspring_sigmas, offspring_y
 
-    def iterate(self, x, y, n):
-        new_x = np.empty((self.n_individuals, self.ndim_problem))
-        new_n = np.empty((self.n_individuals, self.ndim_problem))
-        new_y = np.empty((self.n_individuals,))
-        rand1 = self.rng_optimization.standard_normal(self.ndim_problem)
+    def iterate(self, x=None, sigmas=None, y=None,
+                offspring_x=None, offspring_sigmas=None, offspring_y=None):
         for i in range(self.n_individuals):
+            if self._check_terminations():
+                return x, sigmas, y, offspring_x, offspring_sigmas, offspring_y
             for j in range(self.ndim_problem):
-                new_x[i][j] = x[i][j] + n[i][j] * rand1[j]
-                new_n[i][j] = n[i][j] * np.exp(self.t1 * self.rng_optimization.standard_normal(1) + self.t * rand1[j])
-            new_x[i] = np.clip(new_x[i], self.lower_boundary, self.upper_boundary)
-            new_y[i] = self._evaluate_fitness(new_x[i])
-        new_x = np.vstack((new_x, x))
-        new_y = np.hstack((new_y, y))
-        new_n = np.vstack((new_x, x))
-        win = np.zeros((2 * self.n_individuals,))
-        list1 = list(range(0, 2 * self.n_individuals))
-        for i in range(2 * self.n_individuals):
-            list2 = random.sample(list1, self.q)
-            for j in list2:
-                if new_y[i] >= new_y[j]:
-                    win[i] += 1
-        order = np.argsort(win)
+                n_j = self.rng_optimization.standard_normal()
+                offspring_x[i][j] = x[i][j] + sigmas[i][j]*n_j
+                offspring_sigmas[i][j] = sigmas[i][j]*np.exp(
+                    self.tau_apostrophe*self.rng_optimization.standard_normal() + self.tau*n_j)
+            offspring_y[i] = self._evaluate_fitness(offspring_x[i])
+        new_x = np.vstack((offspring_x, x))
+        new_sigmas = np.vstack((offspring_sigmas, sigmas))
+        new_y = np.hstack((offspring_y, y))
+        n_win = np.zeros((2*self.n_individuals,))  # number of win
+        for i in range(2*self.n_individuals):
+            for j in self.rng_optimization.choice(2*self.n_individuals, self.q):
+                if new_y[i] <= new_y[j]:
+                    n_win[i] += 1
+        order = np.argsort(n_win)[::-1]  # in decreasing order for minimization
         for i in range(self.n_individuals):
             x[i] = new_x[order[i]]
+            sigmas[i] = new_sigmas[order[i]]
             y[i] = new_y[order[i]]
-            n[i] = new_n[order[i]]
-        return x, y, n
+        return x, sigmas, y, offspring_x, offspring_sigmas, offspring_y
 
-    def optimize(self, fitness_function=None):
+    def optimize(self, fitness_function=None, args=None):
         fitness = EP.optimize(self, fitness_function)
-        x, y, n = self.initialize()
+        x, sigmas, y, offspring_x, offspring_sigmas, offspring_y = self.initialize()
         fitness.extend(y)
         while True:
-            x, y, n = self.iterate(x, y, n)
+            x, sigmas, y, offspring_x, offspring_sigmas, offspring_y = self.iterate(
+                x, sigmas, y, offspring_x, offspring_sigmas, offspring_y)
             if self.record_fitness:
                 fitness.extend(y)
             if self._check_terminations():
                 break
             self._n_generations += 1
             self._print_verbose_info(y)
-        results = self._collect_results(fitness)
-        return results
+        return self._collect_results(fitness)
--- pypop7/optimizers/ep/test_cep.py
+++ pypop7/optimizers/ep/test_cep.py
@@ -1,30 +1,34 @@
+"""This code only tests whether the corresponding optimizer could run or not.
+  In other words, it *cannot* validate the correctness regarding its working procedure.
+  For the correctness validation of programming, refer to the following link:
+    https://github.com/Evolutionary-Intelligence/pypop/wiki/Correctness-Validation-of-Optimizers.md
+"""
 import unittest
 import time
+
 import numpy as np
 
-from pypop7.benchmarks.base_functions import sphere, schwefel12, schwefel221, step
+from pypop7.benchmarks.base_functions import ellipsoid, rosenbrock, rastrigin
 from pypop7.optimizers.ep.cep import CEP as Solver
 
 
-class TestSECEM(unittest.TestCase):
+class TestCEP(unittest.TestCase):
     def test_optimize(self):
         start_run = time.time()
-        ndim_problem = 30
-        for f in [sphere, schwefel12, schwefel221, step]:
+        ndim_problem = 1000
+        for f in [ellipsoid, rosenbrock, rastrigin]:
             print('*' * 7 + ' ' + f.__name__ + ' ' + '*' * 7)
             problem = {'fitness_function': f,
                        'ndim_problem': ndim_problem,
-                       'lower_boundary': -100 * np.ones((ndim_problem,)),
-                       'upper_boundary': 100 * np.ones((ndim_problem,))}
+                       'lower_boundary': -5 * np.ones((ndim_problem,)),
+                       'upper_boundary': 5 * np.ones((ndim_problem,))}
             options = {'max_function_evaluations': 2e6,
                        'fitness_threshold': 1e-10,
                        'seed_rng': 0,
-                       'n_individuals': 100,
-                       'q': 10,  # tournament size
-                       'init_n': 3.0,
-                       'verbose_frequency': 20,
+                       'sigma': 0.1,
+                       'verbose_frequency': 200,
                        'record_fitness': True,
-                       'record_fitness_frequency': 1}
+                       'record_fitness_frequency': 200000}
             solver = Solver(problem, options)
             results = solver.optimize()
             print(results)
