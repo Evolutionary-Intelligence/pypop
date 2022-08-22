564d7e5d964da2f98937cc44abcd35e824efdfb8
Evolutionary-Intelligence <duanqq077@163.com>
2022-08-22 18:34:39 +0800

    Add docs/sphinx/source/eda/eda.rst

---
+++ docs/sphinx/source/eda/eda.rst
@@ -0,0 +1,4 @@
+Estimation of Distribution Algorithms (EDA)
+===========================================
+
+.. autoclass:: pypop7.optimizers.eda.eda.EDA
--- pypop7/optimizers/eda/eda.py
+++ pypop7/optimizers/eda/eda.py
@@ -4,20 +4,78 @@ from pypop7.optimizers.core.optimizer import Optimizer
 
 
 class EDA(Optimizer):
-    """Estimation of distribution algorithms(EDA)
-    Reference
-    --------------
-    P. A. N. Bosman, D. Thierens
-    Expanding from Discrete to Continuous Estimation of Distribution Algorithms: The IDEA
-    PPSN 2000: 767-776
+    """Estimation of Distribution Algorithms (EDA).
+
+    This is the **base** (abstract) class for all EDA classes. Please use any of its concrete subclasses to
+    optimize the black-box problem at hand.
+
+    .. note:: Its three methods (`initialize`, `iterate`, `optimize`) should be implemented by all its
+       subclasses.
+
+    Parameters
+    ----------
+    problem : dict
+              problem arguments with the following common settings (`keys`):
+                * 'fitness_function' - objective function to be **minimized** (`func`),
+                * 'ndim_problem'     - number of dimensionality (`int`),
+                * 'upper_boundary'   - upper boundary of search range (`array_like`),
+                * 'lower_boundary'   - lower boundary of search range (`array_like`).
+    options : dict
+              optimizer options with the following common settings (`keys`):
+                * 'max_function_evaluations' - maximum of function evaluations (`int`, default: `np.Inf`),
+                * 'max_runtime'              - maximal runtime (`float`, default: `np.Inf`),
+                * 'seed_rng'                 - seed for random number generation needed to be *explicitly* set (`int`),
+                * 'record_fitness'           - flag to record fitness list to output results (`bool`, default: `False`),
+                * 'record_fitness_frequency' - function evaluations frequency of recording (`int`, default: `1000`),
+
+                  * if `record_fitness` is set to `False`, it will be ignored,
+                  * if `record_fitness` is set to `True` and it is set to 1, all fitness generated during optimization
+                    will be saved into output results.
+
+                * 'verbose'                  - flag to print verbose info during optimization (`bool`, default: `True`),
+                * 'verbose_frequency'        - frequency of printing verbose info (`int`, default: `10`);
+              and with two particular settings (`keys`):
+                * 'n_individuals' - number of offspring, offspring population size (`int`, default: `200`),
+                * 'n_parents'     - number of parents, parental population size (`int`, default:
+                  `int(self.n_individuals / 2)`).
+
+    Attributes
+    ----------
+    n_individuals : `int`
+                    number of offspring, offspring population size.
+    n_parents     : `int`
+                    number of parents, parental population size.
+
+    Methods
+    -------
+
+    References
+    ----------
+    Larrañaga, P. and Lozano, J.A. eds., 2001.
+    Estimation of distribution algorithms: A new tool for evolutionary computation.
+    Springer Science & Business Media.
+    https://link.springer.com/book/10.1007/978-1-4615-1539-5
+
+    Berny, A., 2000, September.
+    Selection and reinforcement learning for combinatorial optimization.
+    In International Conference on Parallel Problem Solving from Nature (pp. 601-610).
+    Springer, Berlin, Heidelberg.
+    https://link.springer.com/chapter/10.1007/3-540-45356-3_59
+
+    Bosman, P.A. and Thierens, D., 2000, September.
+    Expanding from discrete to continuous estimation of distribution algorithms: The IDEA.
+    In International Conference on Parallel Problem Solving from Nature (pp. 767-776).
+    Springer, Berlin, Heidelberg.
+    https://link.springer.com/chapter/10.1007/3-540-45356-3_75
     """
     def __init__(self, problem, options):
         Optimizer.__init__(self, problem, options)
-        self._n_generations = 0
-        if self.n_individuals is None:  # number of offspring, offspring population size (λ: lambda)
-            self.n_individuals = 4 + int(3 * np.log(self.ndim_problem))  # for small population setting
-        if self.n_parents is None:  # number of parents, parental population size (μ: mu)
+        if self.n_individuals is None:  # number of offspring, offspring population size
+            self.n_individuals = 200
+        if self.n_parents is None:  # number of parents, parental population size
             self.n_parents = int(self.n_individuals / 2)
+        self.n_restart = 0
+        self._n_generations = 0
 
     def initialize(self):
         raise NotImplementedError
@@ -27,19 +85,12 @@ class EDA(Optimizer):
 
     def _print_verbose_info(self, y):
         if self.verbose and (not self._n_generations % self.verbose_frequency):
-            best_so_far_y = -self.best_so_far_y if self._is_maximization else self.best_so_far_y
             info = '  * Generation {:d}: best_so_far_y {:7.5e}, min(y) {:7.5e} & Evaluations {:d}'
-            print(info.format(self._n_generations, best_so_far_y, np.min(y), self.n_function_evaluations))
+            print(info.format(self._n_generations, self.best_so_far_y,
+                              np.min(y), self.n_function_evaluations))
 
-    def _initialize_x(self, is_restart=False):
-        if is_restart or (self.x is None):
-            x = self.rng_initialization.uniform(self.initial_lower_boundary,
-                                                self.initial_upper_boundary)
-        else:
-            x = np.copy(self.x)
-        return x
-
-    def _collect_results(self, fitness, mean=None):
+    def _collect_results(self, fitness):
         results = Optimizer._collect_results(self, fitness)
         results['_n_generations'] = self._n_generations
+        results['n_restart'] = self.n_restart
         return results
