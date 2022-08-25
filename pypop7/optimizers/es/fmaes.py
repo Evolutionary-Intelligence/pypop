9e3f847e9510c16d64e2470963fc0d704164a5f6
Evolutionary-Intelligence <duanqq077@163.com>
2022-08-25 16:25:41 +0800

    Update pypop7/optimizers/es/fmaes.py

--- pypop7/optimizers/es/fmaes.py
+++ pypop7/optimizers/es/fmaes.py
@@ -2,10 +2,88 @@ from pypop7.optimizers.es.maes import MAES
 
 
 class FMAES(MAES):
-    """Fast Matrix Adaptation Evolution Strategy (FMAES, Fast-(μ/μ_w,λ)-MA-ES).
+    """Fast Matrix Adaptation Evolution Strategy (FMAES).
 
-    Reference
-    ---------
+    .. note:: `FMAES` is an efficient implementation of `MAES` with *quadractic* time complexity.
+
+       It is **highly recommended** to first attempt other more advanced ES variants for large-scale black-box
+       optimization.
+
+    Parameters
+    ----------
+    problem : dict
+              problem arguments with the following common settings (`keys`):
+                * 'fitness_function' - objective function to be **minimized** (`func`),
+                * 'ndim_problem'     - number of dimensionality (`int`),
+                * 'upper_boundary'   - upper boundary of search range (`array_like`),
+                * 'lower_boundary'   - lower boundary of search range (`array_like`).
+    options : dict
+              optimizer options with the following common settings (`keys`):
+                * 'max_function_evaluations' - maximum of function evaluations (`int`, default: `np.Inf`),
+                * 'max_runtime'              - maximal runtime (`float`, default: `np.Inf`),
+                * 'seed_rng'                 - seed for random number generation needed to be *explicitly* set (`int`),
+                * 'record_fitness'           - flag to record fitness list to output results (`bool`, default: `False`),
+                * 'record_fitness_frequency' - function evaluations frequency of recording (`int`, default: `1000`),
+
+                  * if `record_fitness` is set to `False`, it will be ignored,
+                  * if `record_fitness` is set to `True` and it is set to 1, all fitness generated during optimization
+                    will be saved into output results.
+
+                * 'verbose'                  - flag to print verbose info during optimization (`bool`, default: `True`),
+                * 'verbose_frequency'        - frequency of printing verbose info (`int`, default: `10`);
+              and with four particular settings (`keys`):
+                * 'mean'          - initial (starting) point, mean of Gaussian search distribution (`array_like`),
+
+                  * if not given, it will draw a random sample from the uniform distribution whose search range is
+                    bounded by `problem['lower_boundary']` and `problem['upper_boundary']`).
+
+                * 'sigma'         - initial global step-size (σ), mutation strength (`float`),
+                * 'n_individuals' - number of offspring (λ: lambda), offspring population size (`int`, default:
+                  `4 + int(3*np.log(self.ndim_problem))`),
+                * 'n_parents'     - number of parents (μ: mu), parental population size (`int`, default:
+                  `int(self.n_individuals / 2)`).
+
+    For its correctness checking of coding, refer to `this code-based repeatability report
+    <https://tinyurl.com/37ews6h4>`_ for more details.
+
+    Examples
+    --------
+    Use the ES optimizer `FMAES` to minimize the well-known test function
+    `Rosenbrock <http://en.wikipedia.org/wiki/Rosenbrock_function>`_:
+
+    .. code-block:: python
+       :linenos:
+
+       >>> import numpy
+       >>> from pypop7.benchmarks.base_functions import rosenbrock  # function to be minimized
+       >>> from pypop7.optimizers.es.fmaes import FMAES
+       >>> problem = {'fitness_function': rosenbrock,  # define problem arguments
+       ...            'ndim_problem': 2,
+       ...            'lower_boundary': -5 * numpy.ones((2,)),
+       ...            'upper_boundary': 5 * numpy.ones((2,))}
+       >>> options = {'max_function_evaluations': 5000,  # set optimizer options
+       ...            'seed_rng': 2022,
+       ...            'mean': 3 * numpy.ones((2,)),
+       ...            'sigma': 0.1}
+       >>> fmaes = FMAES(problem, options)  # initialize the optimizer class
+       >>> results = fmaes.optimize()  # run the optimization process
+       >>> # return the number of function evaluations and best-so-far fitness
+       >>> print(f"FMAES: {results['n_function_evaluations']}, {results['best_so_far_y']}")
+       FMAES: 5000, 1.211565542464775e-18
+
+    Attributes
+    ----------
+    n_individuals   : `int`
+                      number of offspring (λ: lambda), offspring population size.
+    n_parents       : `int`
+                      number of parents (μ: mu), parental population size.
+    mean            : `array_like`
+                      initial (starting) point, mean of Gaussian search distribution.
+    sigma           : `float`
+                      initial global step-size (σ), mutation strength (`float`).
+
+    References
+    ----------
     Beyer, H.G., 2020, July.
     Design principles for matrix adaptation evolution strategies.
     In Proceedings of Annual Conference on Genetic and Evolutionary Computation Companion (pp. 682-700).
