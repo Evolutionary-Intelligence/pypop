4e0245aa8bdef43aecf77a098a4f14944330ea87
Evolutionary-Intelligence <duanqq077@163.com>
2022-09-05 23:39:53 +0800

    Add pypop7/optimizers/de/des.py

--- pypop7/optimizers/de/_repeat_des.py
+++ pypop7/optimizers/de/_repeat_des.py
@@ -3,9 +3,6 @@
     Toward a matrix-free covariance matrix adaptation evolution strategy.
     IEEE Transactions on Evolutionary Computation, 24(1), pp.84-98.
     https://doi.org/10.1109/TEVC.2019.2907266
-
-    Very close performance can be obtained by our code. Therefore, we argue that the repeatability of
-    `DES` can be well-documented (*at least partly*).
 """
 import numpy as np
 
@@ -18,29 +15,39 @@ if __name__ == '__main__':
 
     problem = {'fitness_function': sphere,
                'ndim_problem': ndim_problem,
-               'lower_boundary': -100*np.ones((ndim_problem,)),
-               'upper_boundary': 100*np.ones((ndim_problem,))}
-    options = {'max_function_evaluations': 1000 * ndim_problem,
-               # 'n_individuals': 4 * ndim_problem,
+               'lower_boundary': -5*np.ones((ndim_problem,)),
+               'upper_boundary': 5*np.ones((ndim_problem,))}
+    options = {'max_function_evaluations': 8000,
                'seed_rng': 0,  # undefined in the original paper
-               }
+               'record_fitness': True}
     des = DES(problem, options)
     results = des.optimize()
     print(results)
     print(results['best_so_far_y'])
-    # 6617
-    # vs 1.0e-10 (from the original paper)
+    # 21.036636245588326 vs ~1.0e-8 (from the original paper)
 
-    problem = {'fitness_function': rosenbrock,
+    problem = {'fitness_function': cigar,
                'ndim_problem': ndim_problem,
-               'lower_boundary': -100*np.ones((ndim_problem,)),
-               'upper_boundary': 100*np.ones((ndim_problem,))}
-    options = {'max_function_evaluations': 2000 * ndim_problem,
-               # 'n_individuals': 4 * ndim_problem,
+               'lower_boundary': -5*np.ones((ndim_problem,)),
+               'upper_boundary': 5*np.ones((ndim_problem,))}
+    options = {'max_function_evaluations': 14000,
                'seed_rng': 0,  # undefined in the original paper
-               }
+               'record_fitness': True}
     des = DES(problem, options)
     results = des.optimize()
     print(results)
-    # 224142425
-    # vs 1.0e+2 (from the original paper)
+    print(results['best_so_far_y'])
+    # 10065230.954989625 vs ~1.0e-10 (from the original paper)
+
+    problem = {'fitness_function': rosenbrock,
+               'ndim_problem': ndim_problem,
+               'lower_boundary': -5*np.ones((ndim_problem,)),
+               'upper_boundary': 5*np.ones((ndim_problem,))}
+    options = {'max_function_evaluations': 50000,
+               'seed_rng': 0,  # undefined in the original paper
+               'record_fitness': True}
+    des = DES(problem, options)
+    results = des.optimize()
+    print(results)
+    print(results['best_so_far_y'])
+    # 29.87078709080472 vs ~1.0e+2 (from the original paper)
