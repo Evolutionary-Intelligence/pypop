226ae6334e6c274b5b9a55c88e2d235a082e72a9
Evolutionary-Intelligence <duanqq077@163.com>
2022-09-11 17:03:52 +0800

    Add pypop7/optimizers/pso/__init__.py

--- pypop7/optimizers/pso/__init__.py
+++ pypop7/optimizers/pso/__init__.py
@@ -2,8 +2,10 @@ from pypop7.optimizers.pso.pso import PSO
 from pypop7.optimizers.pso.spso import SPSO
 from pypop7.optimizers.pso.spsol import SPSOL
 from pypop7.optimizers.pso.clpso import CLPSO
+from pypop7.optimizers.pso.ccpso2 import CCPSO2
 
 
 __all__ = [PSO,  # base (abstract) class
-           SPSO, SPSOL,
-           CLPSO]
+           SPSO, SPSOL,  # standard and canonical versions
+           CLPSO,  # competitors for medium- and small-scale black-box problems
+           CCPSO2]  # variants for large-scale black-box optimization
