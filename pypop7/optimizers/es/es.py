40f8aa5b70a7ec875a1ac6feeb57569aceeee26d
Evolutionary-Intelligence <duanqq077@163.com>
2022-09-17 16:43:55 +0800

    Standardize optimizer class ES

--- pypop7/optimizers/es/es.py
+++ pypop7/optimizers/es/es.py
@@ -7,15 +7,14 @@ class ES(Optimizer):
     """Evolution Strategies (ES).
 
     This is the **base** (abstract) class for all `ES` classes. Please use any of its instantiated subclasses to
-    optimize the black-box problem at hand. At least its three methods (`initialize`, `iterate`, `optimize`)
-    should be implemented by any of its subclasses.
+    optimize the black-box problem at hand.
 
     .. note:: `ES` are a well-established family of randomized population-based search algorithms, proposed by two
-       German computer scientists Rechenberg and Schwefel (recipients of `Evolutionary Computation Pioneer Award 2002
-       <https://tinyurl.com/456as566>`_). One key property of `ES` lies in its adaptability of strategy parameters,
-       which can *significantly* accelerate the (local) convergence rate in many cases. Recently, the theoretical
-       foundation of its most representative (modern) version called **CMA-ES** has been built on the very interesting
-       `Information-Geometric Optimization (IGO) <https://www.jmlr.org/papers/v18/14-467.html>`_ framework via
+       German computer scientists Ingo Rechenberg and Hans-Paul Schwefel (recipients of `Evolutionary Computation
+       Pioneer Award 2002 <https://tinyurl.com/456as566>`_). One key property of `ES` is **adaptability of strategy
+       parameters**, which generally can *significantly* accelerate the (local) convergence rate. Recently, the
+       theoretical foundation of its most representative (modern) version called **CMA-ES** has been well built on
+       the `Information-Geometric Optimization (IGO) <https://www.jmlr.org/papers/v18/14-467.html>`_ framework via
        invariance principles (inspired by `NES <https://jmlr.org/papers/v15/wierstra14a.html>`_).
 
        According to the latest `Nature <https://www.nature.com/articles/nature14544.>`_ review, *"the CMA-ES algorithm
@@ -33,21 +32,12 @@ class ES(Optimizer):
               optimizer options with the following common settings (`keys`):
                 * 'max_function_evaluations' - maximum of function evaluations (`int`, default: `np.Inf`),
                 * 'max_runtime'              - maximal runtime (`float`, default: `np.Inf`),
-                * 'seed_rng'                 - seed for random number generation needed to be *explicitly* set (`int`),
-                * 'record_fitness'           - flag to record fitness list to output results (`bool`, default: `False`),
-                * 'record_fitness_frequency' - function evaluations frequency of recording (`int`, default: `1000`),
-
-                  * if `record_fitness` is set to `False`, it will be ignored,
-                  * if `record_fitness` is set to `True` and it is set to 1, all fitness generated during optimization
-                    will be saved into output results.
-
-                * 'verbose'                  - flag to print verbose info during optimization (`bool`, default: `True`),
-                * 'verbose_frequency'        - frequency of printing verbose info (`int`, default: `10`);
-              and with four particular settings (`keys`):
+                * 'seed_rng'                 - seed for random number generation needed to be *explicitly* set (`int`);
+              and with the following particular settings (`keys`):
                 * 'n_individuals' - number of offspring (λ: lambda), offspring population size (`int`),
                 * 'n_parents'     - number of parents (μ: mu), parental population size (`int`),
-                * 'mean'          - initial (starting) point, mean of Gaussian search distribution (`array_like`),
-                * 'sigma'         - initial global step-size (σ), mutation strength (`float`).
+                * 'mean'          - initial (starting) point (`array_like`),
+                * 'sigma'         - initial global step-size, mutation strength (`float`).
 
     Attributes
     ----------
@@ -56,9 +46,9 @@ class ES(Optimizer):
     n_parents     : `int`
                     number of parents (μ: mu), parental population size.
     mean          : `array_like`
-                    initial (starting) point, mean of Gaussian search distribution.
+                    mean of Gaussian search/sampling/mutation distribution.
     sigma         : `float`
-                    initial global step-size (σ), mutation strength (rate).
+                    std of Gaussian search/sampling/mutation distribution, mutation strength.
 
     Methods
     -------
@@ -107,34 +97,32 @@ class ES(Optimizer):
     def __init__(self, problem, options):
         Optimizer.__init__(self, problem, options)
         if self.n_individuals is None:  # number of offspring (λ: lambda), offspring population size
-            self.n_individuals = 4 + int(3*np.log(self.ndim_problem))  # for small populations setting
+            self.n_individuals = 4 + int(3*np.log(self.ndim_problem))  # only for small populations setting
         assert self.n_individuals > 0, f'`self.n_individuals` = {self.n_individuals}, but should > 0.'
         if self.n_parents is None:  # number of parents (μ: mu), parental population size
-            self.n_parents = int(self.n_individuals / 2)
+            self.n_parents = int(self.n_individuals/2)
             if self.n_parents > 1:
-                # for consistency and simplicity, we unify these in the base class ES.
-                w_base, w = np.log((self.n_individuals + 1) / 2), np.log(np.arange(self.n_parents) + 1)
-                self._w = (w_base - w) / (self.n_parents*w_base - np.sum(w))
-                self._mu_eff = 1 / np.sum(np.power(self._w, 2))  # μ_eff / μ_w
-                # E[||N(0,I)||]: expectation of chi distribution
-                self._e_chi = np.sqrt(self.ndim_problem)*(
-                        1 - 1 / (4*self.ndim_problem) + 1 / (21*np.power(self.ndim_problem, 2)))
-        assert self.n_parents > 0, f'`` = {self.n_parents}, but should > 0.'
-        self.mean = options.get('mean')  # mean of Gaussian search distribution
-        if self.mean is None:  # 'mean' has priority over 'x'
+                # unify these settings in the base class ES for consistency and simplicity
+                w_base, w = np.log((self.n_individuals + 1.0)/2.0), np.log(np.arange(self.n_parents) + 1.0)
+                self._w = (w_base - w)/(self.n_parents*w_base - np.sum(w))
+                self._mu_eff = 1.0/np.sum(np.power(self._w, 2))  # μ_eff / μ_w
+                self._e_chi = np.sqrt(self.ndim_problem)*(  # E[||N(0,I)||]: expectation of chi distribution
+                    1.0 - 1.0/(4.0*self.ndim_problem) + 1.0/(21.0*np.power(self.ndim_problem, 2)))
+        assert self.n_parents > 0, f'`self.n_parents` = {self.n_parents}, but should > 0.'
+        self.mean = options.get('mean')  # mean of Gaussian search/sampling/mutation distribution
+        if self.mean is None:
             self.mean = options.get('x')
-        self.sigma = options.get('sigma')  # global step-size (σ)
-        self.eta_mean = options.get('eta_mean')  # learning rate of mean
-        self.eta_sigma = options.get('eta_sigma')  # learning rate of std
-        self._n_generations = 0
-        # for restart
-        self.n_restart = 0
-        self.sigma_threshold = options.get('sigma_threshold', 1e-10)  # stopping threshold of sigma for restart
-        # maximal generation number of fitness stagnation for restart
-        self.stagnation = options.get('stagnation', np.maximum(32, self.ndim_problem))
-        self.fitness_diff = options.get('fitness_diff', 1e-10)  # stopping threshold of fitness difference for restart
-        self._sigma_bak = np.copy(self.sigma)  # bak for restart
-        self._fitness_list = [self.best_so_far_y]  # to store `best_so_far_y` generated in each generation
+        self.sigma = options.get('sigma')  # global step-size (σ), mutation strength
+        self.lr_mean = options.get('lr_mean')  # learning rate of mean update
+        self.lr_sigma = options.get('lr_sigma')  # learning rate of sigma update
+        # set options for restart
+        self.sigma_threshold = options.get('sigma_threshold', 1e-12)  # stopping threshold of sigma
+        self.stagnation = options.get('stagnation', 10 + np.ceil(30*self.ndim_problem/self.n_individuals))
+        self.fitness_diff = options.get('fitness_diff', 1e-12)  # stopping threshold of fitness difference
+        self._n_restart = 0  # only for restart
+        self._sigma_bak = np.copy(self.sigma)  # only for restart
+        self._fitness_list = [self.best_so_far_y]  # only for restart
+        self._n_generations = 0  # number of generations
 
     def initialize(self):
         raise NotImplementedError
@@ -150,10 +138,9 @@ class ES(Optimizer):
         return mean
 
     def _print_verbose_info(self, y):
-        if self.verbose and (not self._n_generations % self.verbose_frequency):
-            best_so_far_y = -self.best_so_far_y if self._is_maximization else self.best_so_far_y
+        if self.verbose and (not self._n_generations % self.verbose):
             info = '  * Generation {:d}: best_so_far_y {:7.5e}, min(y) {:7.5e} & Evaluations {:d}'
-            print(info.format(self._n_generations, best_so_far_y, np.min(y), self.n_function_evaluations))
+            print(info.format(self._n_generations, self.best_so_far_y, np.min(y), self.n_function_evaluations))
 
     def restart_initialize(self):
         self._fitness_list.append(self.best_so_far_y)
@@ -162,22 +149,22 @@ class ES(Optimizer):
             is_restart_2 = (self._fitness_list[-self.stagnation] - self._fitness_list[-1]) < self.fitness_diff
         is_restart = bool(is_restart_1) or bool(is_restart_2)
         if is_restart:
-            self.n_restart += 1
             self.sigma = np.copy(self._sigma_bak)
             self.n_individuals *= 2
-            self.n_parents = int(self.n_individuals / 2)
+            self.n_parents = int(self.n_individuals/2)
             if self.n_parents > 1:
-                w_base, w = np.log((self.n_individuals + 1) / 2), np.log(np.arange(self.n_parents) + 1)
-                self._w = (w_base - w) / (self.n_parents*w_base - np.sum(w))
-                self._mu_eff = 1 / np.sum(np.power(self._w, 2))
-            self._n_generations = 0
+                w_base, w = np.log((self.n_individuals + 1.0)/2.0), np.log(np.arange(self.n_parents) + 1.0)
+                self._w = (w_base - w)/(self.n_parents*w_base - np.sum(w))
+                self._mu_eff = 1.0/np.sum(np.power(self._w, 2))  # μ_eff / μ_w
+            self._n_restart += 1
             self._fitness_list = [np.Inf]
+            self._n_generations = 0
         return is_restart
 
-    def _collect_results(self, fitness, mean=None):
+    def _collect_results(self, fitness=None, mean=None):
         results = Optimizer._collect_results(self, fitness)
         results['mean'] = mean
         results['sigma'] = self.sigma
-        results['n_restart'] = self.n_restart
+        results['_n_restart'] = self._n_restart
         results['_n_generations'] = self._n_generations
         return results
