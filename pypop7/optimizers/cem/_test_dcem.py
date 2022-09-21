9f50ced7026397da556a7d96c6fba8cd9e5d7199
GuochenZhou <106387010+GuochenZhou@users.noreply.github.com>
2022-09-21 21:24:12 +0800

    Fix a bug of pypop7/optimizers/cem/dcem.py

--- pypop7/optimizers/cem/_test_dcem.py
+++ pypop7/optimizers/cem/_test_dcem.py
@@ -1,16 +1,21 @@
+"""This code only tests whether the corresponding optimizer could run or not.
+  In other words, it *cannot* validate the correctness regarding its working procedure.
+  For the correctness validation of programming, refer to the following link:
+    https://github.com/Evolutionary-Intelligence/pypop/wiki/Correctness-Validation-of-Optimizers.md
+"""
 import unittest
 import time
 import numpy as np
 
-from pypop7.benchmarks.base_functions import sphere, cigar
+from pypop7.benchmarks.base_functions import rastrigin, sphere, cigar
 from pypop7.optimizers.cem.dcem import DCEM as Solver
 
 
 class TestSECEM(unittest.TestCase):
     def test_optimize(self):
         start_run = time.time()
-        ndim_problem = 10
-        for f in [sphere, cigar]:
+        ndim_problem = 1000
+        for f in [rastrigin, sphere, cigar]:
             print('*' * 7 + ' ' + f.__name__ + ' ' + '*' * 7)
             problem = {'fitness_function': f,
                        'ndim_problem': ndim_problem,
@@ -19,15 +24,14 @@ class TestSECEM(unittest.TestCase):
             options = {'max_function_evaluations': 2e4,
                        'fitness_threshold': 1e-10,
                        'seed_rng': 0,
-                       'x': 4 * np.ones((ndim_problem,)),  # mean
+                       'mean': 4 * np.ones((ndim_problem,)),  # mean
                        'n_parents': 10,
                        'n_individuals': 20,
                        'sigma': 1.0,
                        'lml_verbose': 0,
                        'lml_eps': 1e-3,
-                       'verbose_frequency': 20,
-                       'record_fitness': True,
-                       'record_fitness_frequency': 1}
+                       'verbose': 1,
+                       'saving_fitness': 1}
             solver = Solver(problem, options)
             results = solver.optimize()
             print(results)
