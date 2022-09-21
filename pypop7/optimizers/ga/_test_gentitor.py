fa2ce02f4ba548cbc0cb0ee1a261d2a9132b56e5
GuochenZhou <106387010+GuochenZhou@users.noreply.github.com>
2022-09-21 20:10:03 +0800

    Fix the form of pypop7/optimizers/ga/genitor.py

--- pypop7/optimizers/ga/_test_gentitor.py
+++ pypop7/optimizers/ga/_test_gentitor.py
@@ -8,25 +8,25 @@ import unittest
 import time
 import numpy as np
 
-from pypop7.benchmarks.base_functions import ellipsoid, rosenbrock, rastrigin
+from pypop7.benchmarks.base_functions import ellipsoid, rosenbrock
 from pypop7.optimizers.ga.genitor import GENITOR as Solver
 
 
 class TestGENITOR(unittest.TestCase):
     def test_optimize(self):
         start_run = time.time()
-        ndim_problem = 1000
-        for f in [ellipsoid, rosenbrock, rastrigin]:
+        ndim_problem = 100
+        for f in [ellipsoid, rosenbrock]:
             print('*' * 7 + ' ' + f.__name__ + ' ' + '*' * 7)
             problem = {'fitness_function': f,
                        'ndim_problem': ndim_problem,
                        'lower_boundary': -5 * np.ones((ndim_problem,)),
                        'upper_boundary': 5 * np.ones((ndim_problem,))}
-            options = {'max_function_evaluations': 2e6,
+            options = {'max_function_evaluations': 2e5,
                        'fitness_threshold': 1e-10,
                        'max_runtime': 3600,  # 1 hours
                        'seed_rng': 0,
-                       'verbose': 50000,
+                       'verbose': 5000,
                        'saving_fitness': 200000}
             solver = Solver(problem, options)
             results = solver.optimize()
