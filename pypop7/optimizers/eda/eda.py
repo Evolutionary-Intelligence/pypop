b4ebbdfa366a5bdf9abf7b4d61dd225a1ee805d1
Evolutionary-Intelligence <duanqq077@163.com>
2022-09-22 22:16:58 +0800

    Standardize optimizer class EDA

--- pypop7/optimizers/eda/eda.py
+++ pypop7/optimizers/eda/eda.py
@@ -6,11 +6,10 @@ from pypop7.optimizers.core.optimizer import Optimizer
 class EDA(Optimizer):
     """Estimation of Distribution Algorithms (EDA).
 
-    This is the **base** (abstract) class for all EDA classes. Please use any of its concrete subclasses to
+    This is the **base** (abstract) class for all EDA classes. Please use any of its instantiated subclasses to
     optimize the black-box problem at hand.
 
-    .. note:: Its three methods (`initialize`, `iterate`, `optimize`) should be implemented by all its
-       subclasses. *`EDA` are a modern branch of evolutionary algorithms with some unique advantages in
+    .. note:: *`EDA` are a modern branch of evolutionary algorithms with some unique advantages in
        principle*, as recognized in `[Kabán et al., 2016, ECJ] <https://tinyurl.com/mrxpe28y>`_.
 
        AKA probabilistic model-building genetic algorithms (PMBGA), iterated density estimation
@@ -28,20 +27,11 @@ class EDA(Optimizer):
               optimizer options with the following common settings (`keys`):
                 * 'max_function_evaluations' - maximum of function evaluations (`int`, default: `np.Inf`),
                 * 'max_runtime'              - maximal runtime (`float`, default: `np.Inf`),
-                * 'seed_rng'                 - seed for random number generation needed to be *explicitly* set (`int`),
-                * 'record_fitness'           - flag to record fitness list to output results (`bool`, default: `False`),
-                * 'record_fitness_frequency' - function evaluations frequency of recording (`int`, default: `1000`),
-
-                  * if `record_fitness` is set to `False`, it will be ignored,
-                  * if `record_fitness` is set to `True` and it is set to 1, all fitness generated during optimization
-                    will be saved into output results.
-
-                * 'verbose'                  - flag to print verbose info during optimization (`bool`, default: `True`),
-                * 'verbose_frequency'        - frequency of printing verbose info (`int`, default: `10`);
+                * 'seed_rng'                 - seed for random number generation needed to be *explicitly* set (`int`);
               and with the following particular settings (`keys`):
                 * 'n_individuals' - number of offspring, offspring population size (`int`, default: `200`),
                 * 'n_parents'     - number of parents, parental population size (`int`, default:
-                  `int(self.n_individuals / 2)`).
+                  `int(self.n_individuals/2)`).
 
     Attributes
     ----------
@@ -89,9 +79,9 @@ class EDA(Optimizer):
         if self.n_individuals is None:  # number of offspring, offspring population size
             self.n_individuals = 200
         if self.n_parents is None:  # number of parents, parental population size
-            self.n_parents = int(self.n_individuals / 2)
-        self.n_restart = 0
+            self.n_parents = int(self.n_individuals/2)
         self._n_generations = 0
+        self._n_restart = 0
 
     def initialize(self):
         raise NotImplementedError
@@ -100,7 +90,7 @@ class EDA(Optimizer):
         raise NotImplementedError
 
     def _print_verbose_info(self, y):
-        if self.verbose and (not self._n_generations % self.verbose_frequency):
+        if self.verbose and (not self._n_generations % self.verbose):
             info = '  * Generation {:d}: best_so_far_y {:7.5e}, min(y) {:7.5e} & Evaluations {:d}'
             print(info.format(self._n_generations, self.best_so_far_y,
                               np.min(y), self.n_function_evaluations))
@@ -108,5 +98,5 @@ class EDA(Optimizer):
     def _collect_results(self, fitness):
         results = Optimizer._collect_results(self, fitness)
         results['_n_generations'] = self._n_generations
-        results['n_restart'] = self.n_restart
+        results['_n_restart'] = self._n_restart
         return results
