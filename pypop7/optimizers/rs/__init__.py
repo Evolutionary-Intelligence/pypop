e0c427c017111f223aec49d249f107f6442e52e2
Evolutionary-Intelligence <duanqq077@163.com>
2022-09-30 20:37:31 +0800

    Add BES for pypop7/optimizers/rs/__init__.py

--- pypop7/optimizers/rs/__init__.py
+++ pypop7/optimizers/rs/__init__.py
@@ -3,6 +3,8 @@ from pypop7.optimizers.rs.prs import PRS
 from pypop7.optimizers.rs.rhc import RHC
 from pypop7.optimizers.rs.arhc import ARHC
 from pypop7.optimizers.rs.srs import SRS
+from pypop7.optimizers.rs.bes import BES
 
 
-__all__ = [RS, PRS, RHC, ARHC, SRS]
+__all__ = [RS, PRS, RHC, ARHC, SRS,
+           BES]  # especially for large-scale black-box optimization
