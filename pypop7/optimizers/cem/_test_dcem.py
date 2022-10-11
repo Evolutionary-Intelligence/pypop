e9f0c2d08125b145e92ef3e07b55f76faffd851e
Evolutionary-Intelligence <duanqq077@163.com>
2022-10-12 02:22:16 +0800

    Add optimizer class DCEM

--- pypop7/optimizers/cem/_test_dcem.py
+++ pypop7/optimizers/cem/_test_dcem.py
@@ -5,33 +5,30 @@
 """
 import unittest
 import time
+
 import numpy as np
 
-from pypop7.benchmarks.base_functions import rastrigin, sphere, cigar
+from pypop7.benchmarks.base_functions import ellipsoid, rosenbrock, rastrigin
 from pypop7.optimizers.cem.dcem import DCEM as Solver
 
 
-class TestSECEM(unittest.TestCase):
+class TestDCEM(unittest.TestCase):
     def test_optimize(self):
         start_run = time.time()
         ndim_problem = 1000
-        for f in [rastrigin, sphere, cigar]:
+        for f in [ellipsoid, rosenbrock, rastrigin]:
             print('*' * 7 + ' ' + f.__name__ + ' ' + '*' * 7)
             problem = {'fitness_function': f,
                        'ndim_problem': ndim_problem,
                        'lower_boundary': -5 * np.ones((ndim_problem,)),
                        'upper_boundary': 5 * np.ones((ndim_problem,))}
-            options = {'max_function_evaluations': 2e4,
+            options = {'max_function_evaluations': 2e6,
                        'fitness_threshold': 1e-10,
+                       'max_runtime': 3600,  # 1 hours
                        'seed_rng': 0,
-                       'mean': 4 * np.ones((ndim_problem,)),  # mean
-                       'n_parents': 10,
-                       'n_individuals': 20,
                        'sigma': 1.0,
-                       'lml_verbose': 0,
-                       'lml_eps': 1e-3,
-                       'verbose': 1,
-                       'saving_fitness': 1}
+                       'verbose': 200,
+                       'saving_fitness': 200000}
             solver = Solver(problem, options)
             results = solver.optimize()
             print(results)
--- pypop7/optimizers/cem/dcem.py
+++ pypop7/optimizers/cem/dcem.py
@@ -1,12 +1,19 @@
 import numpy as np
-
-from pypop7.optimizers.cem.cem import CEM
 import torch
+
 from lml import LML
+from pypop7.optimizers.cem.cem import CEM
 
 
 class DCEM(CEM):
-    """Differentiable Cross-Entropy Method
+    """Differentiable Cross-entropy Method (DCEM).
+
+    .. note:: Since the `lml` library may be not successfully installed via `pip`, please run the following
+       two commands before invoking this optimizer (**a necessary manual step**):
+
+       $ `git clone https://github.com/locuslab/lml.git`
+
+       $ `pip install -e lml`
 
     Parameters
     ----------
@@ -16,114 +23,114 @@ class DCEM(CEM):
                 * 'ndim_problem'     - number of dimensionality (`int`),
                 * 'upper_boundary'   - upper boundary of search range (`array_like`),
                 * 'lower_boundary'   - lower boundary of search range (`array_like`).
-
     options : dict
               optimizer options with the following common settings (`keys`):
                 * 'max_function_evaluations' - maximum of function evaluations (`int`, default: `np.Inf`),
-                * 'max_runtime'              - maximal runtime (`float`, default: `np.Inf`),
+                * 'max_runtime'              - maximal runtime to be allowed (`float`, default: `np.Inf`),
                 * 'seed_rng'                 - seed for random number generation needed to be *explicitly* set (`int`),
               and with the following particular settings (`keys`):
-                * 'n_individuals' - population size (`int`, default: `100`),
-                * 'n_parents' - parent size (`int`, default: `200`),
-                * 'mean' - initial mean value (`array_like`, default: `4 * np.ones((ndim_problem,))`),
-                * 'sigma' - initial global step-size (σ), mutation strength (`float`, default: '1.0'),
-                * 'lml_verbose' - whether lml model is verbose (`int`, default: `0`),
-                * 'lml_eps' - parameter for lml optimization (`float`, default: `0.001`),
+                * 'sigma'         - initial global step-size (`float`),
+                * 'mean'          - initial mean of Gaussian search distribution (`array_like`),
+
+                  * if not given, it will draw a random sample from the uniform distribution whose search range is
+                    bounded by `problem['lower_boundary']` and `problem['upper_boundary']`.
+
+                * 'n_individuals' - offspring population size (`int`, default: `1000`),
+                * 'n_parents'     - parent population size (`int`, default: `200`),
+                * 'temperature'   - temperature for lml (`float`, default: `1.0`),
+                * 'lml_eps'       - epsilon for lml (`float`, default: `1e-3`).
 
     Examples
     --------
-    Use the CEM optimizer `DCEM` to minimize the well-known test function
-    `Rastrigin <http://en.wikipedia.org/wiki/Rastrigin_function>`_:
+    Use the `CEM` optimizer `DCEM` to minimize the well-known test function
+    `Rosenbrock <http://en.wikipedia.org/wiki/Rosenbrock_function>`_:
 
     .. code-block:: python
        :linenos:
 
        >>> import numpy
-       >>> from pypop7.benchmarks.base_functions import rastrigin  # function to be minimized
+       >>> from pypop7.benchmarks.base_functions import rosenbrock  # function to be minimized
        >>> from pypop7.optimizers.cem.dcem import DCEM
-       >>> problem = {'fitness_function': rastrigin,  # define problem arguments
-       ...            'ndim_problem': 1000,
-       ...            'lower_boundary': -5 * numpy.ones((1000,)),
-       ...            'upper_boundary': 5 * numpy.ones((1000,))}
+       >>> problem = {'fitness_function': rosenbrock,  # define problem arguments
+       ...            'ndim_problem': 100,
+       ...            'lower_boundary': -5*numpy.ones((100,)),
+       ...            'upper_boundary': 5*numpy.ones((100,))}
        >>> options = {'max_function_evaluations': 1000000,  # set optimizer options
-       ...            'n_individuals': 20,
-       ...            'n_parents': 10,
-       ...            'mean': 4 * np.ones((1000,)),
-       ...            'sigma': 1.0,
-       ...            'seed_rng': 2022}
+       ...            'seed_rng': 2022,
+       ...            'sigma': 0.3}  # the global step-size may need to be tuned for better performance
        >>> dcem = DCEM(problem, options)  # initialize the optimizer class
-       >>> results = decm.optimize()  # run the optimization process
+       >>> results = dcem.optimize()  # run the optimization process
        >>> # return the number of function evaluations and best-so-far fitness
        >>> print(f"DCEM: {results['n_function_evaluations']}, {results['best_so_far_y']}")
-       DCEM: 1001, 8.731149137020111e-11
+       DCEM: 1000000, 6365.13838155091
+
+    For its correctness checking of coding, refer to `this code-based repeatability report
+    <https://tinyurl.com/psd4dxm4>`_ for more details.
 
     Attributes
     ----------
-    n_individuals : `int`
-                    number of offspring (λ: lambda), offspring population size.
-    n_parents     : `int`
-                    number of parents (μ: mu), parental population size.
-    mean          : `array_like`
-                    mean of Gaussian search distribution.
-    sigma         : `float`
-                    mutation strength.
-    lml_verbose   : `int`
-                    whether lml verbose.
     lml_eps       : `float`
-                    optimization parameter of lml.
+                    epsilon for lml.
+    mean          : `array_like`
+                    initial mean of Gaussian search distribution.
+    n_individuals : `int`
+                    number of offspring, aka offspring population size.
+    n_parents     : `int`
+                    number of parents, aka parental population size.
+    sigma         : `float`
+                    final global step-size, aka mutation strength.
+    temperature   : `float`
+                    temperature for lml.
 
-    Reference
-    ---------------
-    B. Amos, D. Yarats
-    The Differentiable Cross-Entropy Method
-    Proceedings of the 37th International Conference on Machine Learning, PMLR 119:291-302, 2020.
+    References
+    ----------
+    Amos, B. and Yarats, D., 2020, November.
+    The differentiable cross-entropy method.
+    In International Conference on Machine Learning (pp. 291-302). PMLR.
     http://proceedings.mlr.press/v119/amos20a.html
+
+    See the official Python code from Amos:
+    https://github.com/facebookresearch/dcem
     """
     def __init__(self, problem, options):
         CEM.__init__(self, problem, options)
-        self.lml_verbose = options.get('lml_verbose', 0)
-        self.lml_eps = options.get('lml_eps', 0.001)
+        self.temperature = options.get('temperature', 1.0)
+        self.lml_eps = options.get('lml_eps', 1e-3)
 
     def initialize(self, is_restart=False):
-        x = np.empty((self.n_individuals, self.ndim_problem))
-        y = np.empty((self.n_individuals,))
         mean = self._initialize_mean(is_restart)
+        x = np.empty((self.n_individuals, self.ndim_problem))  # population
+        y = np.empty((self.n_individuals,))  # fitness (no evaluation)
         return mean, x, y
 
-    def iterate(self, mean, x, y):
+    def iterate(self, mean=None, x=None, y=None, args=None):
         for i in range(self.n_individuals):
-            x[i] = self.rng_optimization.normal(mean, self.sigma, self.ndim_problem)
-            x[i] = np.clip(x[i], self.lower_boundary, self.upper_boundary)
-            y[i] = self._evaluate_fitness(x[i])
             if self._check_terminations():
                 return x, y
+            x[i] = self.rng_optimization.normal(mean, self._sigmas)
+            y[i] = self._evaluate_fitness(x[i], args)
         return x, y
 
     def update_distribution(self, x, y):
-        mean_y = np.mean(y)
-        std_y = np.std(y)
-        _y = (y - mean_y) / (std_y + 1e-10)
-        # _y = _y.reshape(self.n_individuals, 1)
-        _y = torch.from_numpy(_y)
-        I = LML(N=self.n_parents, verbose=self.lml_verbose, eps=self.lml_eps)(-_y)
-        I = I.numpy()
-        I = I.reshape(self.n_individuals, 1)
-        x_I = I * x
-        mean = np.mean(x_I, axis=0)
-        self.sigma = np.sqrt(np.mean(I * (x - mean)**2, axis=0))
+        mean_y, std_y = np.mean(y), np.std(y)
+        y = torch.from_numpy((y - mean_y)/(std_y + 1e-6))
+        i = LML(N=self.n_parents, eps=self.lml_eps, verbose=0)(-y*self.temperature)
+        i = i.detach().numpy().reshape(self.n_individuals, 1)
+        i_x = i*x
+        mean = np.sum(i_x, axis=0)/self.n_parents
+        self._sigmas = np.sqrt(np.sum(i*np.power(x - mean, 2), axis=0)/self.n_parents)
         return mean
 
-    def optimize(self, fitness_function=None):
+    def optimize(self, fitness_function=None, args=None):
         fitness = CEM.optimize(self, fitness_function)
         mean, x, y = self.initialize()
         while True:
-            x, y = self.iterate(mean, x, y)
+            x, y = self.iterate(mean, x, y, args)
             if self.saving_fitness:
                 fitness.extend(y)
             if self._check_terminations():
                 break
-            self._n_generations += 1
             self._print_verbose_info(y)
+            self._n_generations += 1
             mean = self.update_distribution(x, y)
-        results = self._collect_results(fitness, mean)
-        return results
+        return self._collect_results(fitness, mean)
