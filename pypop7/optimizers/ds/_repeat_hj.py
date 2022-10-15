619f24b6bb0a4ba38b68e6ecbf1c2a0502c67dd4
Evolutionary-Intelligence <duanqq077@163.com>
2022-10-15 19:52:47 +0800

    Add pypop7/optimizers/ds/_repeat_hj.py

--- pypop7/optimizers/ds/_repeat_hj.py
+++ pypop7/optimizers/ds/_repeat_hj.py
@@ -17,7 +17,7 @@
 
     problem = Ackley(n_var=100)
     algorithm = PatternSearch(init_delta=0.1)
-    res = minimize(problem=problem, algorithm=algorithm, termination=('n_eval', 1e6),  verbose=True, seed=1)
+    res = minimize(problem=problem, algorithm=algorithm, termination=('n_eval', 1e6),  verbose=True, seed=0)
     print(res)
 """
 import time
@@ -26,6 +26,7 @@ import numpy as np
 
 from pypop7.benchmarks.base_functions import ackley
 from pypop7.optimizers.ds.hj import HJ as Solver
+
 
 if __name__ == '__main__':
     start_run = time.time()
@@ -42,5 +43,5 @@ if __name__ == '__main__':
                    'is_restart': False}
         solver = Solver(problem, options)
         results = solver.optimize()
-        print(results)  # 19.544448155486872 vs 1.937919E+01 (from pymoo)
+        print(results)  # 19.544448155486872 vs 1.924563E+01 (from `pymoo`)
         print('*** Runtime: {:7.5e}'.format(time.time() - start_run))
