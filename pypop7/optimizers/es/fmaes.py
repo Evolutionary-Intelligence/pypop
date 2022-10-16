17c72114c3c42eef19025398f5145970933eb1a3
Evolutionary-Intelligence <duanqq077@163.com>
2022-10-16 17:42:12 +0800

    Update docstring for optimizer class FMAES

--- pypop7/optimizers/es/fmaes.py
+++ pypop7/optimizers/es/fmaes.py
@@ -4,10 +4,14 @@ from pypop7.optimizers.es.maes import MAES
 class FMAES(MAES):
     """Fast Matrix Adaptation Evolution Strategy (FMAES).
 
-    .. note:: `FMAES` is a more efficient implementation of `MAES` with *quadractic* time complexity w.r.t. each
-       sampling. It is **highly recommended** to first attempt other more advanced ES variants (e.g., `LM-CMA`,
-       `LM-MA-ES`) for large-scale black-box optimization, since `FMAES` has a *quadratic* time complexity w.r.t.
-       each sampling.
+    .. note:: `FMAES` is a *more efficient* implementation of `MAES` with *quadractic* time complexity w.r.t. each
+       sampling, which replaces the computationally expensive matrix-matrix multiplication (*cubic time complexity*)
+       with the combination of matrix-matrix addition and matrix-vector multiplication (*quadractic time complexity*)
+       for covariance matrix adaptation (CMA).
+
+       It is **highly recommended** to first attempt other more advanced ES variants (e.g., `LM-CMA`, `LM-MA-ES`) for
+       large-scale black-box optimization, since `FMAES` still has a computationally-intensive *quadratic* time
+       complexity w.r.t. each sampling.
 
     Parameters
     ----------
@@ -20,23 +24,23 @@ class FMAES(MAES):
     options : dict
               optimizer options with the following common settings (`keys`):
                 * 'max_function_evaluations' - maximum of function evaluations (`int`, default: `np.Inf`),
-                * 'max_runtime'              - maximal runtime (`float`, default: `np.Inf`),
+                * 'max_runtime'              - maximal runtime to be allowed (`float`, default: `np.Inf`),
                 * 'seed_rng'                 - seed for random number generation needed to be *explicitly* set (`int`);
               and with the following particular settings (`keys`):
-                * 'sigma'         - initial global step-size (σ), mutation strength (`float`),
-                * 'mean'          - initial (starting) point, mean of Gaussian search distribution (`array_like`),
+                * 'sigma'         - initial global step-size, aka mutation strength (`float`),
+                * 'mean'          - initial (starting) point, aka mean of Gaussian search distribution (`array_like`),
 
                   * if not given, it will draw a random sample from the uniform distribution whose search range is
                     bounded by `problem['lower_boundary']` and `problem['upper_boundary']`.
 
-                * 'n_individuals' - number of offspring (λ: lambda), offspring population size (`int`, default:
+                * 'n_individuals' - number of offspring, aka offspring population size (`int`, default:
                   `4 + int(3*np.log(self.ndim_problem))`),
-                * 'n_parents'     - number of parents (μ: mu), parental population size (`int`, default:
-                  `int(self.n_individuals/2)`).
+                * 'n_parents'     - number of parents, aka parental population size (`int`, default:
+                  `int(self.n_individuals/2)`)..
 
     Examples
     --------
-    Use the ES optimizer `FMAES` to minimize the well-known test function
+    Use the `ES` optimizer `FMAES` to minimize the well-known test function
     `Rosenbrock <http://en.wikipedia.org/wiki/Rosenbrock_function>`_:
 
     .. code-block:: python
@@ -47,31 +51,31 @@ class FMAES(MAES):
        >>> from pypop7.optimizers.es.fmaes import FMAES
        >>> problem = {'fitness_function': rosenbrock,  # define problem arguments
        ...            'ndim_problem': 2,
-       ...            'lower_boundary': -5 * numpy.ones((2,)),
-       ...            'upper_boundary': 5 * numpy.ones((2,))}
+       ...            'lower_boundary': -5*numpy.ones((2,)),
+       ...            'upper_boundary': 5*numpy.ones((2,))}
        >>> options = {'max_function_evaluations': 5000,  # set optimizer options
        ...            'seed_rng': 2022,
-       ...            'mean': 3 * numpy.ones((2,)),
-       ...            'sigma': 0.1}
+       ...            'mean': 3*numpy.ones((2,)),
+       ...            'sigma': 0.1}  # the global step-size may need to be tuned for better performance
        >>> fmaes = FMAES(problem, options)  # initialize the optimizer class
        >>> results = fmaes.optimize()  # run the optimization process
        >>> # return the number of function evaluations and best-so-far fitness
        >>> print(f"FMAES: {results['n_function_evaluations']}, {results['best_so_far_y']}")
-       FMAES: 5000, 4.8405890931611924e-17
+       FMAES: 5000, 4.840560965418994e-17
 
     For its correctness checking of coding, refer to `this code-based repeatability report
     <https://tinyurl.com/37ews6h4>`_ for more details.
 
     Attributes
     ----------
-    n_individuals   : `int`
-                      number of offspring (λ: lambda), offspring population size.
-    n_parents       : `int`
-                      number of parents (μ: mu), parental population size.
-    mean            : `array_like`
-                      mean of Gaussian search distribution.
-    sigma           : `float`
-                      mutation strength.
+    mean          : `array_like`
+                    initial mean of Gaussian search distribution.
+    n_individuals : `int`
+                    number of offspring, aka offspring population size.
+    n_parents     : `int`
+                    number of parents, aka parental population size.
+    sigma         : `float`
+                    final mutation strength.
 
     References
     ----------
