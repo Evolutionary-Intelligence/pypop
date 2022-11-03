7b536ea27c757ecc92f0a777d24ea4431b425444
Evolutionary-Intelligence <duanqq077@163.com>
2022-11-03 19:11:30 +0800

    Add pypop7/benchmarks/__init__.py

---
+++ pypop7/benchmarks/__init__.py
@@ -0,0 +1,5 @@
+import pypop7.benchmarks.utils
+import pypop7.benchmarks.base_functions
+import pypop7.benchmarks.shifted_functions
+import pypop7.benchmarks.rotated_functions
+import pypop7.benchmarks.continuous_functions
