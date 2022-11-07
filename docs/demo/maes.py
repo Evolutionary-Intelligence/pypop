071c8da2efb78ff673d54448820dabfb73370647
Evolutionary-Intelligence <duanqq077@163.com>
2022-11-07 16:10:58 +0800

    Add docs/demo/maes.py

--- docs/demo/maes.py
+++ docs/demo/maes.py
@@ -14,30 +14,34 @@ def plot_contour(func, x, y, levels=None):
     plt.ylabel('y')
 
 
-def shi_cd(x):  # fitness function from https://arxiv.org/abs/1610.00040
-    return 7 * np.power(x[0], 2) + 6 * x[0] * x[1] + 8 * np.power(x[1], 2)
+def shi_cd(x):  # from https://arxiv.org/abs/1610.00040
+    return 7*np.power(x[0], 2) + 6*x[0]*x[1] + 8*np.power(x[1], 2)
 
 
 class MAES1(MAES):
     def optimize(self, fitness_function=None, args=None):  # for all generations (iterations)
         fitness = ES.optimize(self, fitness_function)
         z, d, mean, s, tm, y = self.initialize()
-        x_data, mean_data = [], []
-        x = np.empty((self.n_individuals, self.ndim_problem))
+        x_data, mean_data = [], []  # for plotting
+        x = np.empty((self.n_individuals, self.ndim_problem))  # for plotting
         while True:
-            z, d, y = self.iterate(z, d, mean, tm, y, args)  # sample and evaluate offspring population
+            # sample and evaluate offspring population
+            z, d, y = self.iterate(z, d, mean, tm, y, args)
             for k in range(self.n_individuals):
-                x[k] = mean + self.sigma * d[k]
+                x[k] = mean + self.sigma*d[k]
             x_data.append(np.copy(x))
             mean_data.append(np.copy(mean))
-            if self.record_fitness:
+            if self.saving_fitness:
                 fitness.extend(y)
             if self._check_terminations():
                 break
             mean, s, tm = self._update_distribution(z, d, mean, s, tm, y)
-            self._n_generations += 1
             self._print_verbose_info(y)
+            self._n_generations += 1
+            if self.is_restart:
+                z, d, mean, s, tm, y = self.restart_reinitialize(z, d, mean, s, tm, y)
         results = self._collect_results(fitness, mean)
+        results['s'] = s
         results['x_data'] = x_data
         results['mean_data'] = mean_data
         return results
@@ -51,10 +55,24 @@ if __name__ == '__main__':
            'seed_rng': 0,
            'x': np.array([7., -8.]),  # mean
            'sigma': 0.1,
-           'verbose_frequency': 5,
            'n_individuals': 250}
-    solver = MAES1(pro, opt)
-    res = solver.optimize()
+    # plot static figures
+    res = MAES1(pro, opt).optimize()
+    fig = plt.figure()
+    plt.rcParams['font.family'] = 'Times New Roman'
+    plt.rcParams['font.size'] = '12'
+    for i in range(len(res['mean_data'])):
+        if i % 5 == 0:
+            plt.xlim([-10, 10])
+            plt.ylim([-10, 10])
+            plt.xticks([-10, -8, -6, -4, -2, 0, 2, 4, 6, 8, 10])
+            plt.yticks([-10, -8, -6, -4, -2, 0, 2, 4, 6, 8, 10])
+            plot_contour(shi_cd, [-10, 10], [-10, 10], [0, 10, 100, 500, 1000, 2000])
+            plt.scatter(res['x_data'][i][:, 0], res['x_data'][i][:, 1], c='limegreen', s=3)
+            plt.scatter(res['mean_data'][i][0], res['mean_data'][i][1], c='magenta', s=12)
+            plt.show()
+    # plot a dynamic figure
+    res = MAES1(pro, opt).optimize()
     fig = plt.figure()
     plt.rcParams['font.family'] = 'Times New Roman'
     plt.rcParams['font.size'] = '12'
@@ -69,4 +87,4 @@ if __name__ == '__main__':
         plt.scatter(res['mean_data'][i][0], res['mean_data'][i][1], c='magenta', s=12)
         camera.snap()
     animation = camera.animate()
-    animation.save('demo_maes.gif')
+    animation.save('maes.gif')
