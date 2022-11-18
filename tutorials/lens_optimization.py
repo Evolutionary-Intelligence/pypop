5a4368a6d7f0d8cf88911814a564fbb7e0667d78
Evolutionary-Intelligence <duanqq077@163.com>
2022-11-18 17:13:33 +0800

    Add tutorials/lens_optimization.py

--- tutorials/lens_optimization.py
+++ tutorials/lens_optimization.py
@@ -1,93 +1,79 @@
-"""
-This is a demo that simulate the evolution of lens
-There are some global parameters with following meanings:
-len_param_weighting: Weight of focus function
-len_param_h: Trapezoidal slices of height
-len_param_b: Distance between lens and object
-len_param_r: Radius of lens
-len_param_eps: Refraction index
-len_param_d_init = Init of x
-"""
+"""This is a simple demo that optimizes the lens shape.
 
-
+    Reference
+    ---------
+    Beyer, H.G., 2020, July.
+    Design principles for matrix adaptation evolution strategies.
+    In Proceedings of Annual Conference on Genetic and Evolutionary Computation Companion (pp. 682-700). ACM.
+    https://dl.acm.org/doi/abs/10.1145/3377929.3389870
+"""
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.path import Path
 import matplotlib.patches as patches
-import imageio
+import imageio.v2 as imageio
 
-from pypop7.optimizers.es.maes import MAES
 from pypop7.optimizers.es.es import ES
+from pypop7.optimizers.es.maes import MAES
 
 
-len_param_weighting = 0.9
-len_param_h = 1
-len_param_b = 20
-len_param_r = 7
-len_param_eps = 1.5
-len_param_d_init = 3
+# set parameters for lens shape optimization
+weight = 0.9  # weight of focus function
+r = 7  # radius of lens
+h = 1  # trapezoidal slices of height
+b = 20  # distance between lens and object
+eps = 1.5  # refraction index
+d_init = 3  # initialization
 
 
+# define objective function to be minimized
 def f_lens(x):
-    """
-    Function that estimate the ability of lens
-    """
     n = len(x)
-    f_focus = np.sum((len_param_r - ((len_param_h * np.arange(1, n)-0.5) + len_param_b / len_param_h
-                                     * (len_param_eps - 1) * np.transpose(np.abs(x[1:n]) - np.abs(x[0:n-1])))) ** 2)
-    f_mass = len_param_h * (np.sum(np.abs(x[1:n-2])) + 0.5 * (np.abs(x[0]) + np.abs(x[n-1])))
-    qual = len_param_weighting * f_focus + (1 - len_param_weighting) * f_mass
-    return qual
+    focus = r - ((h*np.arange(1, n) - 0.5) + b/h*(eps - 1)*np.transpose(np.abs(x[1:]) - np.abs(x[:(n-1)])))
+    mass = h*(np.sum(np.abs(x[1:(n-1)])) + 0.5*(np.abs(x[0]) + np.abs(x[n-1])))
+    return weight*np.sum(focus**2) + (1.0 - weight)*mass
 
 
 def get_path(x):
-    """
-    Function to get the path of lens according to x
-    """
-    left_points, right_points = [], []
-    temp_height = len_param_r
+    left, right, height = [], [], r
     for i in range(len(x)):
         if x[i] < 0:
             x[i] *= -1
-        left_points.append((-0.5 * x[i], temp_height))
-        right_points.append((0.5 * x[i], temp_height))
-        temp_height -= 1
-    points = left_points
-    for i in range(len(right_points)):
-        points.append(right_points[-1 * i - 1])
-    points.append(left_points[0])
+        left.append((-0.5*x[i], height))
+        right.append((0.5*x[i], height))
+        height -= 1
+    points = left
+    for i in range(len(right)):
+        points.append(right[-i - 1])
+    points.append(left[0])
     codes = [Path.MOVETO]
-    for i in range(len(points)-2):
+    for i in range(len(points) - 2):
         codes.append(Path.LINETO)
     codes.append(Path.CLOSEPOLY)
-    path = Path(points, codes)
-    return path
+    return Path(points, codes)
 
 
-def draw_graph(xs):
-    img_path = "./lens"
-    file_names = []
+def plot(xs):
+    file_names, frames = [], []
     for i in range(len(xs)):
+        sub_figure = "_" + str(i) + ".png"
         fig = plt.figure()
         ax = fig.add_subplot(111)
         plt.rcParams['font.family'] = 'Times New Roman'
         plt.rcParams['font.size'] = '12'
-        ax.set_xlim(-5, 5)
+        ax.set_xlim(-10, 10)
         ax.set_ylim(-8, 8)
         path = get_path(xs[i])
         patch = patches.PathPatch(path, facecolor='orange', lw=2)
         ax.add_patch(patch)
-        temp_path = img_path + "_" + str(i) + ".png"
-        file_names.append(temp_path)
-        plt.savefig(temp_path)
-    frames = []
-    for image_name in file_names:
-        im = imageio.imread(image_name)
-        frames.append(im)
-    imageio.mimsave("./demo_lens.gif", frames, 'GIF', duration=0.3)
+        plt.savefig(sub_figure)
+        file_names.append(sub_figure)
+    for image in file_names:
+        frames.append(imageio.imread(image))
+    imageio.mimsave("./lens_optimization.gif", frames, 'GIF', duration=0.3)
 
 
-class MAES_tutorial(MAES):
+class MAES1(MAES):
     def optimize(self, fitness_function=None, args=None):  # for all generations (iterations)
         fitness = ES.optimize(self, fitness_function)
         z, d, mean, s, tm, y = self.initialize()
@@ -106,28 +92,23 @@ class MAES_tutorial(MAES):
             self._n_generations += 1
             if self.is_restart:
                 z, d, mean, s, tm, y = self.restart_reinitialize(z, d, mean, s, tm, y)
-        results = self._collect_results(fitness, mean)
-        results['s'] = s
-        results['xs'] = xs
-        return results
+        res = self._collect_results(fitness, mean)
+        res['s'] = s
+        res['xs'] = xs
+        return res
 
 
 if __name__ == '__main__':
-    d = 15
+    dim = 15  # dimension of objective function
     problem = {'fitness_function': f_lens,
-               'ndim_problem': d,
-               'lower_boundary': -2 * np.ones((d,)),
-               'upper_boundary': 2 * np.ones((d,))}
-    options = {'fitness_threshold': 1e-20,
-               'max_function_evaluations': 8e3,
-               'seed_rng': 2022,  # not given in the original paper
-               'x': len_param_d_init * np.ones((d,)),
+               'ndim_problem': dim,
+               'lower_boundary': -5*np.ones((dim,)),
+               'upper_boundary': 5*np.ones((dim,))}
+    options = {'max_function_evaluations': 7e3,
+               'seed_rng': 2022,
+               'x': d_init*np.ones((dim,)),
                'sigma': 0.3,
-               'stagnation': np.Inf,
-               'verbose': 50,
                'saving_fitness': 50,
                'is_restart': False}
-    solver = MAES_tutorial(problem, options)
-    results = solver.optimize()
-    print(results)
-    draw_graph(results['xs'])
+    results = MAES1(problem, options).optimize()
+    plot(results['xs'])
