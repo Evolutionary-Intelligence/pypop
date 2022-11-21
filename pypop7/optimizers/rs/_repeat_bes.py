4530c8afc0b62d54c6988a1ecc3e7c1de7c7f2f9
Evolutionary-Intelligence <duanqq077@163.com>
2022-11-21 22:31:46 +0800

    Add pypop7/optimizers/rs/_repeat_bes.py

--- pypop7/optimizers/rs/_repeat_bes.py
+++ pypop7/optimizers/rs/_repeat_bes.py
@@ -1,3 +1,20 @@
+"""Repeat the following paper for `BES`:
+    Gao, K. and Sener, O., 2022, June.
+    Generalizing Gaussian Smoothing for Random Search.
+    In International Conference on Machine Learning (pp. 7077-7101). PMLR.
+    https://proceedings.mlr.press/v162/gao22f.html
+    https://icml.cc/media/icml-2022/Slides/16434.pdf
+
+    All generated figures can be accessed via the following link:
+    https://github.com/Evolutionary-Intelligence/pypop/tree/main/docs/repeatability/bes
+
+    Since the current implementation is just a **highly simplified** version of the original BES algorithm
+    without noisy evaluations, its repeatability **cannot** be guaranteed.
+
+    However, we found that the current implementation could show *very similar* performance on the same
+    benchmark functions (after removing its noise part). The resulting much less (>100x) number of function
+    evaluations to be needed may be well explained by the much easier fitness landscape (i.e., without noisiness).
+"""
 import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
@@ -13,7 +30,7 @@ if __name__ == '__main__':
                'ndim_problem': 100}
     options = {'max_function_evaluations': 100*101,
                'seed_rng': 0,  # undefined in the original paper
-               'x': np.ones((100,)),
+               'x': np.random.default_rng(1).standard_normal(size=100),
                'n_individuals': 10,
                'saving_fitness': 101}
     bes = BES(problem, options)
@@ -32,7 +49,8 @@ if __name__ == '__main__':
                'lower_boundary': -2 * np.ones((100,)),
                'upper_boundary': 2 * np.ones((100,))}
     options = {'max_function_evaluations': 100*101,
-               'seed_rng': 0,  # undefined in the original paper
+               'seed_rng': 2,  # undefined in the original paper
+               'x': np.random.default_rng(3).standard_normal(size=100),
                'n_individuals': 10,
                'c': 0.1,
                'lr': 0.000001,
