e72d7146c4e1003e1385d0d5ade646307c14a9e5
Evolutionary-Intelligence <duanqq077@163.com>
2022-11-24 21:29:01 +0800

    Add pypop7/optimizers/sa/__init__.py

--- pypop7/optimizers/sa/__init__.py
+++ pypop7/optimizers/sa/__init__.py
@@ -1,5 +1,6 @@
 from pypop7.optimizers.sa.sa import SA
 from pypop7.optimizers.sa.csa import CSA
 from pypop7.optimizers.sa.esa import ESA
+from pypop7.optimizers.sa.nsa import NSA
 
-__all__ = [SA, CSA, ESA]
+__all__ = [SA, CSA, ESA, NSA]
