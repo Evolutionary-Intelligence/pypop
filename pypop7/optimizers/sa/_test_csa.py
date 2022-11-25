a80d442fc799edc186db4d60e3f0e130d1a80809
Evolutionary-Intelligence <duanqq077@163.com>
2022-11-25 15:26:29 +0800

    Update pypop7/optimizers/sa/_test_csa.py

--- pypop7/optimizers/sa/_test_csa.py
+++ pypop7/optimizers/sa/_test_csa.py
@@ -1,3 +1,8 @@
+"""This code only tests whether the corresponding optimizer could run or not.
+  In other words, it *cannot* validate the correctness regarding its working procedure.
+  For the correctness validation of programming, refer to the following link:
+    https://github.com/Evolutionary-Intelligence/pypop/wiki/Correctness-Validation-of-Optimizers.md
+"""
 import unittest
 import time
 
