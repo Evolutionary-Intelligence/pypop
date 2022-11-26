b717af565911202741af92f62b6b79a2f953fb5a
Evolutionary-Intelligence <duanqq077@163.com>
2022-11-26 17:32:00 +0800

    Update optimizer class GA

--- pypop7/optimizers/ga/ga.py
+++ pypop7/optimizers/ga/ga.py
@@ -20,19 +20,10 @@ class GA(Optimizer):
     options : dict
               optimizer options with the following common settings (`keys`):
                 * 'max_function_evaluations' - maximum of function evaluations (`int`, default: `np.Inf`),
-                * 'max_runtime'              - maximal runtime (`float`, default: `np.Inf`),
-                * 'seed_rng'                 - seed for random number generation needed to be *explicitly* set (`int`),
-                * 'record_fitness'           - flag to record fitness list to output results (`bool`, default: `False`),
-                * 'record_fitness_frequency' - function evaluations frequency of recording (`int`, default: `1000`),
-
-                  * if `record_fitness` is set to `False`, it will be ignored,
-                  * if `record_fitness` is set to `True` and it is set to 1, all fitness generated during optimization
-                    will be saved into output results.
-
-                * 'verbose'                  - flag to print verbose info during optimization (`bool`, default: `True`),
-                * 'verbose_frequency'        - frequency of printing verbose info (`int`, default: `10`);
-              and with the following particular settings (`keys`):
-                * 'n_individuals' - population size (`int`, default: `100`),
+                * 'max_runtime'              - maximal runtime to be allowed (`float`, default: `np.Inf`),
+                * 'seed_rng'                 - seed for random number generation needed to be *explicitly* set (`int`);
+              and with the following particular setting (`key`):
+                * 'n_individuals' - population size (`int`, default: `100`).
 
     Attributes
     ----------
@@ -49,30 +40,35 @@ class GA(Optimizer):
     In Handbook of Metaheuristics (pp. 245-274). Springer, Cham.
     https://link.springer.com/chapter/10.1007/978-3-319-91086-4_8
 
+    De Jong, K.A., 2006.
+    Evolutionary computation: A unified approach.
+    MIT Press.
+    https://mitpress.mit.edu/9780262041942/evolutionary-computation/
+
     Levine, D., 1997.
     Commentary—Genetic algorithms: A practitioner's view.
     INFORMS Journal on Computing, 9(3), pp.256-259.
     https://pubsonline.informs.org/doi/10.1287/ijoc.9.3.256
-
-    Forrest, S., 1996.
-    Genetic algorithms.
-    ACM Computing Surveys, 28(1), pp.77-80.
-    https://dl.acm.org/doi/10.1145/234313.234350
 
     Goldberg, D.E., 1994.
     Genetic and evolutionary algorithms come of age.
     Communications of the ACM, 37(3), pp.113-120.
     https://dl.acm.org/doi/10.1145/175247.175259
 
+    De Jong, K.A., 1993.
+    Are genetic algorithms function optimizer?.
+    Foundations of Genetic Algorithms, pp.5-17.
+    https://www.sciencedirect.com/science/article/pii/B9780080948324500064
+
     Forrest, S., 1993.
     Genetic algorithms: Principles of natural selection applied to computation.
     Science, 261(5123), pp.872-878.
     https://www.science.org/doi/10.1126/science.8346439
 
-    De Jong, K.A., 2006.
-    Evolutionary computation: A unified approach.
-    MIT Press.
-    https://mitpress.mit.edu/9780262041942/evolutionary-computation/
+    Mitchell, M., Holland, J. and Forrest, S., 1993.
+    When will a genetic algorithm outperform hill climbing.
+    Advances in Neural Information Processing Systems (pp. 51-58).
+    https://proceedings.neurips.cc/paper/1993/hash/ab88b15733f543179858600245108dd8-Abstract.html
 
     Holland, J.H., 1992.
     Genetic algorithms.
@@ -83,6 +79,11 @@ class GA(Optimizer):
     Genetic algorithms and machine learning.
     Machine Learning, 3(2), pp.95-99.
     https://link.springer.com/article/10.1023/A:1022602019183
+
+    Holland, J.H., 1973.
+    Genetic algorithms and the optimal allocation of trials.
+    SIAM Journal on Computing, 2(2), pp.88-105.
+    https://epubs.siam.org/doi/10.1137/0202009
 
     Holland, J.H., 1962.
     Outline for a logical theory of adaptive systems.
