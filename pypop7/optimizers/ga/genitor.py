af19952ad1d6f34668447eb38a2edbe2cbcb97aa
Evolutionary-Intelligence <duanqq077@163.com>
2022-11-26 17:47:23 +0800

    Update optimizer class GENITOR

--- pypop7/optimizers/ga/genitor.py
+++ pypop7/optimizers/ga/genitor.py
@@ -4,7 +4,12 @@ from pypop7.optimizers.ga.ga import GA
 
 
 class GENITOR(GA):
-    """GENITOR.
+    """GENetic ImplemenTOR (GENITOR).
+
+    .. note:: `"Selective pressure and population diversity should be controlled as directly as possible."---[Whitley,
+       1989] <https://dl.acm.org/doi/10.5555/93126.93169>`_
+
+       This is a *slightly modified* version of `GENITOR` for continuous optimization.
 
     Parameters
     ----------
@@ -17,14 +22,15 @@ class GENITOR(GA):
     options : dict
               optimizer options with the following common settings (`keys`):
                 * 'max_function_evaluations' - maximum of function evaluations (`int`, default: `np.Inf`),
-                * 'max_runtime'              - maximal runtime (`float`, default: `np.Inf`),
-                * 'seed_rng'                 - seed for random number generation needed to be *explicitly* set (`int`),
-              and with the following particular settings (`keys`):
-                * 'n_individuals' - population size (`int`, default: `100`),
+                * 'max_runtime'              - maximal runtime to be allowed (`float`, default: `np.Inf`),
+                * 'seed_rng'                 - seed for random number generation needed to be *explicitly* set (`int`);
+              and with the following particular setting (`key`):
+                * 'n_individuals'  - population size (`int`, default: `100`),
+                * 'crossover_prob' - crossover probability (`float`, default: `0.5`).
 
     Examples
     --------
-    Use the GA optimizer `GENITOR` to minimize the well-known test function
+    Use the Genetic Algorithm optimizer `GENITOR` to minimize the well-known test function
     `Rosenbrock <http://en.wikipedia.org/wiki/Rosenbrock_function>`_:
 
     .. code-block:: python
@@ -38,32 +44,43 @@ class GENITOR(GA):
        ...            'lower_boundary': -5 * numpy.ones((100,)),
        ...            'upper_boundary': 5 * numpy.ones((100,))}
        >>> options = {'max_function_evaluations': 1000000,  # set optimizer options
-       ...            'n_individuals': 100,
        ...            'seed_rng': 2022}
        >>> genitor = GENITOR(problem, options)  # initialize the optimizer class
        >>> results = genitor.optimize()  # run the optimization process
        >>> # return the number of function evaluations and best-so-far fitness
        >>> print(f"GENITOR: {results['n_function_evaluations']}, {results['best_so_far_y']}")
-       GENITOR: 200000, 1330.4144752335092
+       GENITOR: 1000000, 1340.6238831110309
+
+    For its correctness checking of coding, the code-based repeatability report cannot be provided owing to
+    the lack of its simulation envrionment.
 
     Attributes
     ----------
-    n_individuals : `int`
-                    population size.
+    crossover_prob : `float`
+                     crossover probability.
+    n_individuals  : `int`
+                     population size.
 
     References
     ----------
+    https://www.cs.colostate.edu/~genitor/
+
     Whitley, D., Dominic, S., Das, R. and Anderson, C.W., 1993.
     Genetic reinforcement learning for neurocontrol problems.
     Machine Learning, 13, pp.259-284.
     https://link.springer.com/article/10.1023/A:1022602019183
+
+    Whitley, D., 1989, December.
+    The GENITOR algorithm and selection pressure: Why rank-based allocation of reproductive trials is best.
+    In Proceedings of International Conference on Genetic Algorithms (pp. 116-121).
+    https://dl.acm.org/doi/10.5555/93126.93169
     """
     def __init__(self, problem, options):
         GA.__init__(self, problem, options)
         self.crossover_prob = options.get('crossover_prob', 0.5)  # crossover probability
         self._rank_prob = np.arange(self.n_individuals, 0, -1) - 1.0
 
-    def initialize(self):
+    def initialize(self, args=None):
         x = self.rng_initialization.uniform(self.initial_lower_boundary, self.initial_upper_boundary,
                                             size=(self.n_individuals, self.ndim_problem))  # population
         y = np.empty((self.n_individuals,))  # fitness
@@ -71,11 +88,10 @@ class GENITOR(GA):
         for i in range(self.n_individuals):
             if self._check_terminations():
                 break
-            y[i] = self._evaluate_fitness(x[i])
-        self._n_generations += 1
+            y[i] = self._evaluate_fitness(x[i], args)
         return x, y, crossover_probs
 
-    def iterate(self, x=None, y=None, crossover_probs=None):
+    def iterate(self, x=None, y=None, crossover_probs=None, args=None):
         order, xx, yy = np.argsort(y), None, None
         # use rank-based selection for two parents
         rank_prob = self._rank_prob/np.sum(self._rank_prob)
@@ -83,7 +99,7 @@ class GENITOR(GA):
         if self.rng_optimization.random() < crossover_probs[offspring[0]]:  # crossover
             # use intermediate crossover (not one-point crossover proposed in the original paper)
             xx = (x[offspring[0]] + x[offspring[1]])/2.0
-            yy = self._evaluate_fitness(xx)
+            yy = self._evaluate_fitness(xx, args)
             if yy < y[order[-1]]:  # to replace the worst individual
                 x[order[-1]], y[order[-1]] = xx, yy
                 crossover_probs[offspring[0]] += 0.1
@@ -93,21 +109,23 @@ class GENITOR(GA):
         else:  # mutation
             xx = np.copy(x[offspring[0]])  # offspring
             xx += self.rng_optimization.uniform(self.lower_boundary, self.upper_boundary)/10.0
-            yy = self._evaluate_fitness(xx)
+            yy = self._evaluate_fitness(xx, args)
             if yy < y[order[-1]]:  # to replace the worst individual
                 x[order[-1]], y[order[-1]] = xx, yy
         self._n_generations += 1
-        return x, y, crossover_probs
+        return x, yy, crossover_probs
 
-    def optimize(self, fitness_function=None):
+    def optimize(self, fitness_function=None, args=None):
         fitness = GA.optimize(self, fitness_function)
-        x, y, crossover_probs = self.initialize()
-        fitness.extend(y)
+        x, y, crossover_probs = self.initialize(args)
+        if self.saving_fitness:
+            fitness.extend(y)
+        self._print_verbose_info(y)
         while True:
-            x, y, crossover_probs = self.iterate(x, y, crossover_probs)
+            x, yy, crossover_probs = self.iterate(x, y, crossover_probs, args)
             if self.saving_fitness:
-                fitness.extend(y)
+                fitness.append(yy)
             if self._check_terminations():
                 break
-            self._print_verbose_info(y)
+            self._print_verbose_info(yy)
         return self._collect_results(fitness)
