356d675a18ed386ac936d30be0165fea78497f76
Evolutionary-Intelligence <duanqq077@163.com>
2022-11-27 21:06:29 +0800

    Add optimizer class ASGA for pypop7/optimizers/ga/__init__.py

--- pypop7/optimizers/ga/__init__.py
+++ pypop7/optimizers/ga/__init__.py
@@ -1,5 +1,6 @@
 from pypop7.optimizers.ga.ga import GA
 from pypop7.optimizers.ga.genitor import GENITOR
+from pypop7.optimizers.ga.asga import ASGA
 
 
-__all__ = [GA, GENITOR]
+__all__ = [GA, GENITOR, ASGA]
