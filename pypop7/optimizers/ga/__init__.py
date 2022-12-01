adf3737973d36f133a330a840383823ae7e95f25
Evolutionary-Intelligence <duanqq077@163.com>
2022-12-01 14:24:05 +0800

    Add G3PCX for pypop7/optimizers/ga/__init__.py

--- pypop7/optimizers/ga/__init__.py
+++ pypop7/optimizers/ga/__init__.py
@@ -1,6 +1,7 @@
 from pypop7.optimizers.ga.ga import GA
 from pypop7.optimizers.ga.genitor import GENITOR
 from pypop7.optimizers.ga.asga import ASGA
+from pypop7.optimizers.ga.g3pcx import G3PCX
 
 
-__all__ = [GA, GENITOR, ASGA]
+__all__ = [GA, GENITOR, ASGA, G3PCX]
