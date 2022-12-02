47e7843ce84ddcee63ce6594131f7424f7a8904a
Evolutionary-Intelligence <duanqq077@163.com>
2022-12-03 00:42:48 +0800

    Add pypop7/optimizers/pso/_repeat_spsol.py

--- pypop7/optimizers/pso/_repeat_spsol.py
+++ pypop7/optimizers/pso/_repeat_spsol.py
@@ -1,4 +1,9 @@
 """Repeat the following paper for `SPSOL`:
+    Shi, Y. and Eberhart, R., 1998, May.
+    A modified particle swarm optimizer.
+    In IEEE World Congress on Computational Intelligence (pp. 69-73). IEEE.
+    https://ieeexplore.ieee.org/abstract/document/699146
+
     Kennedy, J. and Eberhart, R., 1995, November.
     Particle swarm optimization.
     In Proceedings of International Conference on Neural Networks (pp. 1942-1948). IEEE.
@@ -18,7 +23,7 @@
     problem = Ackley(n_var=100)
     algorithm = PSO(pop_size=20)
     res = minimize(problem=problem, algorithm=algorithm, termination=('n_eval', 1e6),  verbose=True, seed=1)
-    print("Best solution found: \nX = %s\nF = %s" % (res.X, res.F))
+    print("Best solution found: \nX = %s\nF = %s" % (res.X, res.F))  # F = [0.19331169]
 """
 import time
 
@@ -26,6 +31,7 @@ import numpy as np
 
 from pypop7.benchmarks.base_functions import ackley
 from pypop7.optimizers.pso.spsol import SPSOL as Solver
+
 
 if __name__ == '__main__':
     start_run = time.time()
@@ -37,8 +43,9 @@ if __name__ == '__main__':
                    'lower_boundary': -32.768 * np.ones((ndim_problem,)),
                    'upper_boundary': 32.768 * np.ones((ndim_problem,))}
         options = {'max_function_evaluations': 1e6,
+                   'seed_rng': 1,
                    'verbose': 1e3}
         solver = Solver(problem, options)
         results = solver.optimize()
-        print(results)      # 9.312894093671531e-06 vs 0.19331169 (from pymoo)
+        print(results)      # 1.745270594710746e-13 vs 0.19331169 (from pymoo)
         print('*** Runtime: {:7.5e}'.format(time.time() - start_run))
