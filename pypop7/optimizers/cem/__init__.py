0548e984cd25065f438b19a0232c02faf41b5211
Evolutionary-Intelligence <duanqq077@163.com>
2022-12-08 19:43:14 +0800

    Update pypop7/optimizers/cem/__init__.py

--- pypop7/optimizers/cem/__init__.py
+++ pypop7/optimizers/cem/__init__.py
@@ -1,8 +1,9 @@
 from pypop7.optimizers.cem.cem import CEM
 from pypop7.optimizers.cem.scem import SCEM
 from pypop7.optimizers.cem.dscem import DSCEM
+from pypop7.optimizers.cem.mras import MRAS
 from pypop7.optimizers.cem.dcem import DCEM
 
 
 __all__ = [CEM,  # base (abstract) class
-           SCEM, DSCEM, DCEM]
+           SCEM, DSCEM, MRAS, DCEM]
