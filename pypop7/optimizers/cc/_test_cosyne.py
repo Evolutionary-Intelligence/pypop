24c0c0a7b29a0b536965cf7395d7f8be58d99a40
Evolutionary-Intelligence <duanqq077@163.com>
2022-12-14 16:51:34 +0800

    Add pypop7/optimizers/cc/_test_cosyne.py

--- pypop7/optimizers/cc/_test_cosyne.py
+++ pypop7/optimizers/cc/_test_cosyne.py
@@ -5,31 +5,29 @@
 """
 import unittest
 import time
+
 import numpy as np
 
-from pypop7.benchmarks.base_functions import ellipsoid, sphere, cigar
+from pypop7.benchmarks.base_functions import ellipsoid, rosenbrock, rastrigin
 from pypop7.optimizers.cc.cosyne import COSYNE as Solver
 
 
-class TestSCEM(unittest.TestCase):
+class TestCOSYNE(unittest.TestCase):
     def test_optimize(self):
         start_run = time.time()
-        ndim_problem = 10
-        for f in [ellipsoid, sphere, cigar]:
+        ndim_problem = 1000
+        for f in [ellipsoid, rosenbrock, rastrigin]:
             print('*' * 7 + ' ' + f.__name__ + ' ' + '*' * 7)
             problem = {'fitness_function': f,
                        'ndim_problem': ndim_problem,
                        'lower_boundary': -5 * np.ones((ndim_problem,)),
                        'upper_boundary': 5 * np.ones((ndim_problem,))}
-            options = {'max_function_evaluations': 3e5,
+            options = {'max_function_evaluations': 2e6,
                        'fitness_threshold': 1e-10,
-                       'n_individuals': 20,
-                       'n_combine': 2,
-                       'prob_mutate': 0.3,
-                       'crossover_type': "one_point",
-                       'verbose_frequency': 20,
-                       'record_fitness': True,
-                       'record_fitness_frequency': 1}
+                       'sigma': 0.3,
+                       'seed_rng': 0,
+                       'verbose': 2000,
+                       'saving_fitness': 200000}
             solver = Solver(problem, options)
             results = solver.optimize()
             print(results)
