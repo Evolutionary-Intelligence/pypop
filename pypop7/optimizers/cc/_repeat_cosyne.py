22f0ea1686b102371bb7ba4472e5659b614df861
Evolutionary-Intelligence <duanqq077@163.com>
2022-12-14 18:06:07 +0800

    Add pypop7/optimizers/cc/_repeat_cosyne.py

--- pypop7/optimizers/cc/_repeat_cosyne.py
+++ pypop7/optimizers/cc/_repeat_cosyne.py
@@ -14,15 +14,16 @@
     Therefore, we argue that the repeatability of `COSYNE` could be **well-documented**.
 
     The reference code based on EvoTorch is given below:
+
     import torch
     from evotorch import Problem
     from evotorch.algorithms import ga
     from evotorch.logging import StdOutLogger
 
     def norm(x: torch.Tensor) -> torch.Tensor:
-        return torch.linalg.norm(x, dim=-1)
+        return (torch.linalg.norm(x, dim=-1)).pow(2)
 
-    problem = Problem('min', norm, initial_bounds=(-5.0, 5.0), solution_length=10)
+    problem = Problem('min', norm, initial_bounds=(-5.0, 5.0), solution_length=1000)
     searcher = ga.Cosyne(problem, popsize=100, tournament_size=10, mutation_probability=1.0, mutation_stdev=1.0)
     logger = StdOutLogger(searcher)
     searcher.run(num_generations=3000)
@@ -37,7 +38,7 @@ from pypop7.optimizers.cc.cosyne import COSYNE as Solver
 
 if __name__ == '__main__':
     start_run = time.time()
-    ndim_problem = 10
+    ndim_problem = 1000
     for f in [sphere]:
         print('*' * 7 + ' ' + f.__name__ + ' ' + '*' * 7)
         problem = {'fitness_function': f,
@@ -49,5 +50,6 @@ if __name__ == '__main__':
                    'seed_rng': 0}
         solver = Solver(problem, options)
         results = solver.optimize()
-        print(results)  # 0.5348416948073422 vs 0.24382416903972626 (from the EvoTorch library)
+        print(results)  # 7613.71992419764 vs 7388.05517578125  (from the EvoTorch library)
+        # 10 dimension: 0.5348416948073422 vs 0.03605746477842331 (from the EvoTorch library)
         print('*** Runtime: {:7.5e}'.format(time.time() - start_run))
