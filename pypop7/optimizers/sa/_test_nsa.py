29a528899922438330e15e133f23be5b67fc34f5
Evolutionary-Intelligence <duanqq077@163.com>
2022-12-18 17:55:17 +0800

    Update pypop7/optimizers/sa/_test_nsa.py

--- pypop7/optimizers/sa/_test_nsa.py
+++ pypop7/optimizers/sa/_test_nsa.py
@@ -29,7 +29,6 @@ class TestNSA(unittest.TestCase):
                        'sigma': 0.1,
                        'temperature': 100,
                        'is_noisy': False,
-                       'n_samples': 1,  # for static optimization
                        'verbose': 20000,
                        'saving_fitness': 200000}
             solver = Solver(problem, options)
