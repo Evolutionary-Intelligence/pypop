eb9d310aa8f04de9535c7e5b7593e8c84cc088c5
Evolutionary-Intelligence <duanqq077@163.com>
2023-01-08 18:22:13 +0800

    Add pypop7/optimizers/es/_repeat_opoc2009.py

--- pypop7/optimizers/es/_repeat_opoc2009.py
+++ pypop7/optimizers/es/_repeat_opoc2009.py
@@ -8,8 +8,8 @@
     All generated figures can be accessed via the following link:
     https://github.com/Evolutionary-Intelligence/pypop/tree/main/docs/repeatability/opoc2009
 
-    Luckily our code could repeat the data reported in the original paper *well*.
-    Therefore, we argue that the repeatability of `OPOC2009` could be **well-documented**.
+    Luckily our Python code could repeat the data reported in the paper *well*.
+    Therefore, we argue that its repeatability could be **well-documented**.
 """
 import pickle
 
