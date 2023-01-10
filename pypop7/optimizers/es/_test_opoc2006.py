d8e442782d2e2033e91129a14cd3179a6b735730
Evolutionary-Intelligence <duanqq077@163.com>
2023-01-10 17:11:17 +0800

    Add pypop7/optimizers/es/_test_opoc2006.py

--- pypop7/optimizers/es/_test_opoc2006.py
+++ pypop7/optimizers/es/_test_opoc2006.py
@@ -28,7 +28,7 @@ class TestOPOC2006(unittest.TestCase):
                        'seed_rng': 0,
                        'x': 4 * np.ones((ndim_problem,)),  # mean
                        'sigma': 0.1,
-                       'verbose': 20000,
+                       'verbose': 50000,
                        'saving_fitness': 200000}
             solver = Solver(problem, options)
             results = solver.optimize()
