71aff2036fb06b244527e88ca6a1a821cd21a5ff
Evolutionary-Intelligence <duanqq077@163.com>
2023-01-13 13:40:52 +0800

    Add pypop7/optimizers/es/opoa2015.py

--- pypop7/optimizers/es/opoa2015.py
+++ pypop7/optimizers/es/opoa2015.py
@@ -9,7 +9,7 @@ def cholesky_update(rm, z, downdate):
     # https://github.com/scipy/scipy/blob/d20f92fce9f1956bfa65365feeec39621a071932/
     #     scipy/linalg/_decomp_cholesky_update.py
     rm, z, alpha, beta = rm.T, z, np.empty_like(z), np.empty_like(z)
-    alpha[-1], beta[-1] = 1., 1.
+    alpha[-1], beta[-1] = 1.0, 1.0
     sign = -1 if downdate else 1
     for r in range(len(z)):
         a = z[r]/rm[r, r]
@@ -36,11 +36,17 @@ class OPOA2015(ES):
               optimizer options with the following common settings (`keys`):
                 * 'max_function_evaluations' - maximum of function evaluations (`int`, default: `np.Inf`),
                 * 'max_runtime'              - maximal runtime to be allowed (`float`, default: `np.Inf`),
-                * 'seed_rng'                 - seed for random number generation needed to be *explicitly* set (`int`).
+                * 'seed_rng'                 - seed for random number generation needed to be *explicitly* set (`int`),
+              and with the following particular settings (`keys`):
+                * 'sigma' - initial global step-size, aka mutation strength (`float`),
+                * 'mean'  - initial (starting) point, aka mean of Gaussian search distribution (`array_like`),
+
+                  * if not given, it will draw a random sample from the uniform distribution whose search range is
+                    bounded by `problem['lower_boundary']` and `problem['upper_boundary']`.
 
     Examples
     --------
-    Use the `ES` optimizer `OPOA2015` to minimize the well-known test function
+    Use the optimizer `OPOA2015` to minimize the well-known test function
     `Rosenbrock <http://en.wikipedia.org/wiki/Rosenbrock_function>`_:
 
     .. code-block:: python
@@ -61,7 +67,7 @@ class OPOA2015(ES):
        >>> results = opoa2015.optimize()  # run the optimization process
        >>> # return the number of function evaluations and best-so-far fitness
        >>> print(f"OPOA2015: {results['n_function_evaluations']}, {results['best_so_far_y']}")
-       OPOA2015: 5000, 6.710440903901067e-16
+       OPOA2015: 5000, 5.955151843487958e-17
 
     For its correctness checking of coding, refer to `this code-based repeatability report
     <https://tinyurl.com/mrxu4suj>`_ for more details.
@@ -74,8 +80,8 @@ class OPOA2015(ES):
     https://dl.acm.org/doi/abs/10.1145/2725494.2725496
     """
     def __init__(self, problem, options):
-        options['n_individuals'] = 1  # mandatory setting for OPOA2015
-        options['n_parents'] = 1  # mandatory setting for OPOA2015
+        options['n_individuals'] = 1  # mandatory setting
+        options['n_parents'] = 1  # mandatory setting
         ES.__init__(self, problem, options)
         if self.lr_sigma is None:
             self.lr_sigma = 1.0/(1.0 + self.ndim_problem/2.0)
@@ -90,7 +96,7 @@ class OPOA2015(ES):
         self._c_cf = 1.0 - self.c_cov + self.c_cov*self.c_c*(2.0 - self.c_c)
 
     def initialize(self, args=None, is_restart=False):
-        mean = self._initialize_mean(is_restart)
+        mean = self._initialize_mean(is_restart)  # mean of Gaussian search distribution
         y = self._evaluate_fitness(mean, args)  # fitness
         cf = np.diag(np.ones(self.ndim_problem,))  # Cholesky factorization
         best_so_far_y, p_s = np.copy(y), self.p_ts
@@ -105,7 +111,7 @@ class OPOA2015(ES):
             downdate = False
         return cholesky_update(np.sqrt(alpha)*cf, np.sqrt(beta)*v, downdate)
 
-    def iterate(self, args=None, mean=None, cf=None, best_so_far_y=None, p_s=None, p_c=None):
+    def iterate(self, mean=None, cf=None, best_so_far_y=None, p_s=None, p_c=None, args=None):
         # sample and evaluate only one offspring
         z = self.rng_optimization.standard_normal((self.ndim_problem,))
         cf_z = np.dot(cf, z)
@@ -130,45 +136,37 @@ class OPOA2015(ES):
             del self._ancestors[0]
             c_m = np.minimum(self.c_m, 1.0/(2.0*np.dot(z, z) - 1.0))
             cf = self._cholesky_update(cf, 1.0 + c_m, -c_m, cf_z)
-        return mean, cf, best_so_far_y, p_s, p_c, y
+        self._n_generations += 1
+        return mean, y, cf, best_so_far_y, p_s, p_c
 
-    def restart_reinitialize(self, args=None, mean=None, y=None, cf=None,
-                             best_so_far_y=None, p_s=None, p_c=None, fitness=None):
-        self._fitness_list.append(self.best_so_far_y)
+    def restart_reinitialize(self, args=None, mean=None, y=None, cf=None, best_so_far_y=None,
+                             p_s=None, p_c=None, fitness=None):
+        self._list_fitness.append(best_so_far_y)
         is_restart_1, is_restart_2 = self.sigma < self.sigma_threshold, False
-        if len(self._fitness_list) >= self.stagnation:
-            is_restart_2 = (self._fitness_list[-self.stagnation] - self._fitness_list[-1]) < self.fitness_diff
+        if len(self._list_fitness) >= self.stagnation:
+            is_restart_2 = (self._list_fitness[-self.stagnation] - self._list_fitness[-1]) < self.fitness_diff
         is_restart = bool(is_restart_1) or bool(is_restart_2)
         if is_restart:
+            self._print_verbose_info(fitness, y, True)
+            if self.verbose:
+                print(' ....... *** restart *** .......')
             self._n_restart += 1
+            self._list_generations.append(self._n_generations)  # for each restart
+            self._n_generations = 0
             self.sigma = np.copy(self._sigma_bak)
             mean, y, cf, best_so_far_y, p_s, p_c = self.initialize(args, True)
-            if self.saving_fitness:
-                fitness.append(y)
-            self._fitness_list = [best_so_far_y]
+            self._list_fitness = [best_so_far_y]
             self._ancestors = []
-            self._n_generations = 0
-            if self.verbose:
-                print(' ....... restart .......')
-            self._print_verbose_info(y)
         return mean, y, cf, best_so_far_y, p_s, p_c
 
     def optimize(self, fitness_function=None, args=None):  # for all generations (iterations)
         fitness = ES.optimize(self, fitness_function)
         mean, y, cf, best_so_far_y, p_s, p_c = self.initialize(args)
-        if self.saving_fitness:
-            fitness.append(y)
-        self._print_verbose_info(y)
-        while True:
-            mean, cf, best_so_far_y, p_s, p_c, y = self.iterate(
-                args, mean, cf, best_so_far_y, p_s, p_c)
-            if self.saving_fitness:
-                fitness.append(y)
-            if self._check_terminations():
-                break
-            self._n_generations += 1
-            self._print_verbose_info(y)
+        while not self._check_terminations():
+            self._print_verbose_info(fitness, y)
+            mean, y, cf, best_so_far_y, p_s, p_c = self.iterate(
+                mean, cf, best_so_far_y, p_s, p_c, args)
             if self.is_restart:
                 mean, y, cf, best_so_far_y, p_s, p_c = self.restart_reinitialize(
                     args, mean, y, cf, best_so_far_y, p_s, p_c, fitness)
-        return self._collect_results(fitness, mean)
+        return self._collect(fitness, y, mean)
