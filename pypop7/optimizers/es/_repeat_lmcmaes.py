deff6857cfae6eb2693739b7de09076048093030
Evolutionary-Intelligence <duanqq077@163.com>
2023-01-29 20:01:29 +0800

    Add pypop7/optimizers/es/_repeat_lmcmaes.py

--- pypop7/optimizers/es/_repeat_lmcmaes.py
+++ pypop7/optimizers/es/_repeat_lmcmaes.py
@@ -2,15 +2,15 @@
     Loshchilov, I., 2014, July.
     A computationally efficient limited memory CMA-ES for large scale optimization.
     In Proceedings of Annual Conference on Genetic and Evolutionary Computation (pp. 397-404). ACM.
-    https://dl.acm.org/doi/abs/10.1145/2576768.2598294 (See Algorithm 6 for details.)
+    https://dl.acm.org/doi/abs/10.1145/2576768.2598294
     https://sites.google.com/site/lmcmaeses/ (Note that use the modified C++ (rather Matlab) version.)
 
-    Luckily our code could repeat the data reported in the original paper *well*.
-    Therefore, we argue that the repeatability of `LMCMAES` could be **well-documented**.
+    Luckily our Python code could repeat the data reported in the original code *well*.
+    Therefore, we argue that its repeatability could be **well-documented**.
 
     For the C++ code, you may first need to change Line 1594 `void main()` to `int main()` and
     Line 1616 `bool sample_symmetry=false` to `bool sample_symmetry=true`.
-    Then you can run the following command successfully (if using Windows OS).
+    Then you can run the following command successfully (if using Windows).
     $ gcc lmcma.cpp -lstdc++ -o lmcma.exe
     $ .\lmcma.exe
 """
@@ -43,5 +43,5 @@ if __name__ == '__main__':
         print('*** Runtime: {:7.5e}'.format(time.time() - start_run))
     # compare `n_function_evaluations`:
     #   ellipsoid: 4.780563e+06 vs 4893900 (from the C++ code)
-    #   discus: 1.175605e+06 vs 1216460 (from the C++ code)
-    #   cigar:  5.4790e+04 vs 60520 (from the C++ code)
+    #   discus:    1.175605e+06 vs 1216460 (from the C++ code)
+    #   cigar:     5.4790e+04   vs 60520   (from the C++ code)
