9b3e619ba6713bebc22edb0ba3987011e3cd79aa
Evolutionary-Intelligence <duanqq077@163.com>
2023-02-01 21:02:55 +0800

    Add pypop7/optimizers/es/fcmaes.py

--- pypop7/optimizers/es/fcmaes.py
+++ pypop7/optimizers/es/fcmaes.py
@@ -4,120 +4,88 @@ from pypop7.optimizers.es.es import ES
 
 
 class FCMAES(ES):
-    """Fast Covariance Matrix Adaptation Evolution Strategies (FCMAES).
+    """Fast Covariance Matrix Adaptation Evolution Strategy (FCMAES).
 
-        Li, Z., Zhang, Q., Lin, X. and Zhen, H.L., 2018.
-        Fast covariance matrix adaptation for large-scale black-box optimization.
-        IEEE transactions on cybernetics, 50(5), pp.2073-2083.
-        https://ieeexplore.ieee.org/abstract/document/8533604
+    References
+    ----------
+    Li, Z., Zhang, Q., Lin, X. and Zhen, H.L., 2018.
+    Fast covariance matrix adaptation for large-scale black-box optimization.
+    IEEE Transactions on Cybernetics, 50(5), pp.2073-2083.
+    https://ieeexplore.ieee.org/abstract/document/8533604
     """
-    def __init__(self, prolblem, options):
-        ES.__init__(self, prolblem, options)
-        self.n_evolution_paths = self.n_individuals
-        self.c = 2 / (self.ndim_problem + 5)
-        self.c_1 = 1 / (3 * np.sqrt(self.ndim_problem) + 5)
-        self.c_sigma = 0.3
-        self.d_sigma = 1
-        self.q_star = 0.27
-        self.T = self.ndim_problem
-        self.sigma_threshold = 1e-12
-        self.fitness_diff = 1e-12
-        self.fitness_rel_diff = 1e-6
-        self.s_1 = 1 - self.c_1
-        self.s_2 = np.sqrt((1 - self.c_1) * self.c_1)
-        self.s_3 = np.sqrt(self.c_1)
-        self.d_1 = 1 - self.c
-        self.d_2 = np.sqrt(self.c * (2 - self.c) * self._mu_eff)
+    def __init__(self, problem, options):
+        ES.__init__(self, problem, options)
+        self.m = self.n_individuals  # number of evolution paths
+        self.c = 2.0/(self.ndim_problem + 5.0)  # learning rate of evolution path update
+        self.c_1 = 1.0/(3.0*np.sqrt(self.ndim_problem) + 5.0)  # sampling factor
+        self.c_s = 0.3  # learning rate of rank-based success rule for global step-size adaptation
+        self.q_star = 0.27  # target of rank-based success rule for global step-size adaptation
+        self.d_s = 1.0  # damping factor of rank-based success rule for global step-size adaptation
+        self.n_steps = self.ndim_problem  # updating frequency of direction vector set
+        self._x_1 = 1.0 - self.c_1
+        self._x_2 = np.sqrt((1.0 - self.c_1)*self.c_1)
+        self._x_3 = np.sqrt(self.c_1)
+        self._p_1 = 1.0 - self.c
+        self._p_2 = None
+        self._rr = None
 
     def initialize(self, is_restart=False):
         mean = self._initialize_mean(is_restart)  # mean of Gaussian search distribution
-        x = np.empty((self.n_individuals, self.ndim_problem))
-        y = np.empty((self.n_individuals, ))
-        p = np.zeros((self.ndim_problem, ))
-        p_hat = np.zeros((self.n_evolution_paths, self.ndim_problem))
+        x = np.empty((self.n_individuals, self.ndim_problem))  # offspring population
+        y = np.empty((self.n_individuals,))  # fitness (no evaluation)
+        p = np.zeros((self.ndim_problem,))  # evolution path
+        p_hat = np.zeros((self.m, self.ndim_problem))  # direction vector set
         s = 0
-        f = np.inf * np.ones((self.n_parents, ))
-        return mean, x, y, p, p_hat, s, f
+        self._p_2 = np.sqrt(self.c*(2.0 - self.c)*self._mu_eff)
+        self._rr = np.arange(self.n_parents*2) + 1
+        return mean, x, y, p, p_hat, s
 
-    def iterate(self, mean, x, y, p, p_hat):
+    def iterate(self, mean=None, x=None, y=None, p=None, p_hat=None, args=None):
         for i in range(self.n_individuals):
-            z = self.rng_optimization.standard_normal((self.ndim_problem, ))
-            r1 = self.rng_optimization.standard_normal()
-            r2 = self.rng_optimization.standard_normal()
-            x[i] = mean + self.sigma * (self.s_1 * z + self.s_2 * r1 * p_hat[i] + self.s_3 * r2 * p)
-            y[i] = self._evaluate_fitness(x[i])
+            z = self.rng_optimization.standard_normal((self.ndim_problem,))
+            if self._n_generations < self.m:  # unbiased sampling when starting
+                x[i] = mean + self.sigma*z
+            else:
+                x[i] = mean + self.sigma*(self._x_1*z +
+                                          self._x_2*self.rng_optimization.standard_normal()*p_hat[i] +
+                                          self._x_3*self.rng_optimization.standard_normal()*p)
+            y[i] = self._evaluate_fitness(x[i], args)
         return x, y
 
-    def _update_distribution(self, mean, x, y, p, p_hat, s, f):
-        new_mean = 0
-        new_f = np.empty((self.n_parents, ))
-        order = np.argsort(y)
-        r1, r2 = np.empty((self.n_parents, )), np.empty((self.n_parents, ))
-        for i in range(self.n_parents):
-            new_mean += self._w[i] * x[order[i]]
-            new_f[i] = y[order[i]]
-        p = self.d_1 * p + self.d_2 * (new_mean - mean) / self.sigma
-        mean = new_mean
-        if self._n_generations % self.T == 0:
-            for i in range(self.n_evolution_paths - 1):
-                p_hat[i] = p_hat[i+1]
+    def _update_distribution(self, mean=None, x=None, y=None, p=None, p_hat=None, s=None, y_bak=None):
+        order = np.argsort(y)[:self.n_parents]
+        y.sort()
+        mean_bak = np.dot(self._w[:self.n_parents], x[order])
+        p = self._p_1*p + self._p_2*(mean_bak - mean)/self.sigma
+        if self._n_generations % self.n_steps == 0:
+            p_hat[:-1] = p_hat[1:]
             p_hat[-1] = p
-        F = np.append(f, new_f, axis=0)
-        F = np.sort(F)
-        p1, p2 = 0, 0
-        for i in range(len(F)):
-            if p1 != self.n_parents and f[p1] == F[i]:
-                r1[p1] = i
-                p1 += 1
-            elif new_f[p2] == F[i]:
-                r2[p2] = i
-                p2 += 1
-        assert p1 == self.n_parents
-        assert p2 == self.n_parents
-        q = 0
-        for i in range(self.n_parents):
-            q += self._w[i] * (r1[i] - r2[i])
-        q /= self.n_parents
-        f = new_f
-        s = (1 - self.c_sigma) * s + self.c_sigma * (q - self.q_star)
-        self.sigma *= np.exp(s / self.d_sigma)
-        return mean, p, p_hat, s, f
+        if self._n_generations > 0:
+            r = np.argsort(np.hstack((y_bak[:self.n_parents], y[:self.n_parents])))
+            rr = self._rr[r < self.n_parents] - self._rr[r >= self.n_parents]
+            q = np.dot(self._w, rr)/self.n_parents
+            s = (1.0 - self.c_s)*s + self.c_s*(q - self.q_star)
+            self.sigma *= np.exp(s/self.d_s)
+        self._n_generations += 1
+        return mean_bak, p, p_hat, s
 
-    def restart_initialize(self, args=None, mean=None, x=None, y=None, p=None, p_hat=None,
-                           s=None, f=None):
-        self._fitness_list.append(self.best_so_far_y)
-        is_restart_1, is_restart_2, is_restart_3 = self.sigma < self.sigma_threshold, False, False
-        if len(self._fitness_list) >= self.stagnation:
-            is_restart_2 = (self._fitness_list[-self.stagnation] - self._fitness_list[-1]) < self.fitness_diff
-            is_restart_3 = (self._fitness_list[-self.stagnation] - self._fitness_list[-1]) /\
-                           self._fitness_list[-1] < self.fitness_rel_diff
-        is_restart = bool(is_restart_1) or bool(is_restart_2) or bool(is_restart_3)
-        if is_restart:
-            self.n_restart += 1
-            self.sigma = np.copy(self._sigma_bak)
-            self.n_individuals *= 2
-            self.n_parents = int(self.n_individuals / 2)
-            self.n_evolution_paths = self.n_individuals
-            w_base, w = np.log((self.n_individuals + 1) / 2), np.log(np.arange(self.n_parents) + 1)
-            self._w = (w_base - w) / (self.n_parents * w_base - np.sum(w))
-            self._mu_eff = 1 / np.sum(np.power(self._w, 2))
-            self._fitness_list = [np.Inf]
-            self.d_sigma *= 2
-            mean, x, y, p, p_hat, s, f = self.initialize(args, is_restart)
-        return mean, x, y, p, p_hat, s, f
+    def restart_reinitialize(self, mean=None, x=None, y=None, p=None, p_hat=None, s=None):
+        if self.is_restart and ES.restart_reinitialize(self, y):
+            self.d_s *= 2.0
+            self.m = self.n_individuals
+            mean, x, y, p, p_hat, s = self.initialize(True)
+        return mean, x, y, p, p_hat, s
 
     def optimize(self, fitness_function=None, args=None):
         fitness = ES.optimize(self, fitness_function)
-        mean, x, y, p, p_hat, s, f = self.initialize()
-        while True:
-            x, y = self.iterate(mean, x, y, p, p_hat)
-            if self.record_fitness:
-                fitness.extend(y)
-            if self._check_terminations():
-                break
-            mean, p, p_hat, s, f = self._update_distribution(mean, x, y, p, p_hat, s, f)
-            self._n_generations += 1
-            self._print_verbose_info(y)
-            mean, x, y, p, p_hat, s, f = self.restart_initialize(args, mean, x, y, p, p_hat, s, f)
-        results = self._collect_results(fitness, mean)
+        mean, x, y, p, p_hat, s = self.initialize()
+        while not self._check_terminations():
+            y_bak = np.copy(y)
+            x, y = self.iterate(mean, x, y, p, p_hat, args)
+            self._print_verbose_info(fitness, y)
+            mean, p, p_hat, s = self._update_distribution(mean, x, y, p, p_hat, s, y_bak)
+            mean, x, y, p, p_hat, s = self.restart_reinitialize(mean, x, y, p, p_hat, s)
+        results = self._collect(fitness, y, mean)
+        results['p'] = p
+        results['s'] = s
         return results
