5f8bcd88606d301a61a4ea3c865a1e89c3d6167d
Evolutionary-Intelligence <duanqq077@163.com>
2023-02-05 02:52:30 +0800

    Update pypop7/optimizers/es/__init__.py

--- pypop7/optimizers/es/__init__.py
+++ pypop7/optimizers/es/__init__.py
@@ -7,7 +7,7 @@ from pypop7.optimizers.es.saes import SAES  # Self-Adaptation Evolution Strategy
 from pypop7.optimizers.es.samaes import SAMAES  # Self-Adaptation Matrix Adaptation Evolution Strategy
 from pypop7.optimizers.es.cmaes import CMAES  # Covariance Matrix Adaptation Evolution Strategy
 from pypop7.optimizers.es.opoc2006 import OPOC2006  # (1+1)-Cholesky-CMA-ES (2006)
-from pypop7.optimizers.es.sepcmaes import SEPCMAES  # Separable Covariance Matrix Adaptation Evolution Strategy
+from pypop7.optimizers.es.sepcmaes import SEPCMAES  # Separable Covariance Matrix Adaptation Evolution Strategy (2008)
 from pypop7.optimizers.es.opoc2009 import OPOC2009  # (1+1)-Cholesky-CMA-ES (2009)
 from pypop7.optimizers.es.ccmaes2009 import CCMAES2009  # Cholesky-CMA-ES (2009)
 from pypop7.optimizers.es.opoa2010 import OPOA2010  # (1+1)-Active-CMA-ES (2010)
@@ -15,6 +15,7 @@ from pypop7.optimizers.es.vdcma import VDCMA  # Linear Covariance Matrix Adaptat
 from pypop7.optimizers.es.lmcmaes import LMCMAES  # Limited-Memory CMA-ES (2014)
 from pypop7.optimizers.es.opoa2015 import OPOA2015  # (1+1)-Active-CMA-ES (2015)
 from pypop7.optimizers.es.ccmaes2016 import CCMAES2016
+from pypop7.optimizers.es.vkdcma import VKDCMA  # Restricted Covariance Matrix Adaptation (2016)
 from pypop7.optimizers.es.lmcma import LMCMA  # Limited-Memory Covariance Matrix Adaptation (2017)
 from pypop7.optimizers.es.maes import MAES  # Matrix Adaptation Evolution Strategy (2017)
 from pypop7.optimizers.es.r1es import R1ES  # Rank-One Evolution Strategy (2018)
@@ -30,4 +31,4 @@ __all__ = [ES,  # base (abstract) class
            RES, SSAES, DSAES, CSAES, SAES, SAMAES,  # representative ES versions during early development
            CMAES,  # state-of-the-art ES version
            OPOC2006, OPOC2009, CCMAES2009, OPOA2010, OPOA2015, CCMAES2016, MAES, FMAES, DDCMA,  # modern ES versions
-           SEPCMAES, VDCMA, LMCMAES, LMCMA, R1ES, RMES, LMMAES, FCMAES, MMES]  # especially for large-scale BBO
+           SEPCMAES, VDCMA, LMCMAES, VKDCMA, LMCMA, R1ES, RMES, LMMAES, FCMAES, MMES]  # especially for large-scale BBO
