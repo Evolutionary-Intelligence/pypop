477f9a19c0e0e2472f8b55a597babbfe1a481ac2
Evolutionary-Intelligence <duanqq077@163.com>
2023-02-10 23:21:41 +0800

    Add pypop7/optimizers/nes/ones.py

--- pypop7/optimizers/nes/ones.py
+++ pypop7/optimizers/nes/ones.py
@@ -28,20 +28,26 @@ class ONES(SGES):
     """
     def __init__(self, problem, options):
         SGES.__init__(self, problem, options)
+        if options.get('lr_mean') is None:
+            self.lr_mean = 1.0
+        if options.get('lr_sigma') is None:
+            self.lr_sigma = 1.0
 
     def _update_distribution(self, x=None, y=None, mean=None, cv=None):
-        inv_cv = np.linalg.inv(cv)  # inverse of covariance matrix
-        grad_mean = np.zeros((self.n_individuals, self.ndim_problem))  # gradients of mean
-        grad_cv = np.zeros((self.n_individuals, self.ndim_problem*self.ndim_problem))
+        order = np.argsort(-y)
+        u = np.empty((self.n_individuals,))
+        for i, o in enumerate(order):
+            u[o] = self._u[i]
+        inv_cv = np.linalg.inv(cv)
+        phi = np.ones((self.n_individuals, self._n_distribution + 1))
         for k in range(self.n_individuals):
             diff = x[k] - mean
-            grad_mean[k] = np.dot(inv_cv, diff)
+            phi[k, :self.ndim_problem] = np.dot(inv_cv, diff)
             _grad_cv = 0.5*(np.dot(np.dot(inv_cv, np.outer(diff, diff)), inv_cv) - inv_cv)
-            grad_cv[k] = np.ravel(np.dot(self._d_cv, _grad_cv + _grad_cv.T))
-        _grad = np.hstack((np.hstack((grad_mean, grad_cv)), np.ones((self.n_individuals, 1))))
-        grad = np.dot(np.linalg.pinv(_grad), self._u[np.argsort(y)])[:-1]
+            phi[k, self.ndim_problem:-1] = self._triu2flat(np.dot(self._d_cv, _grad_cv + _grad_cv.T))
+        grad = np.dot(np.linalg.pinv(phi), u)[:-1]
         mean += self.lr_mean*grad[:self.ndim_problem]
-        self._d_cv += self.lr_sigma*(grad[self.ndim_problem:].reshape((self.ndim_problem, self.ndim_problem)))
+        self._d_cv += self.lr_sigma*self._flat2triu(grad[self.ndim_problem:])
         cv = np.dot(self._d_cv.T, self._d_cv)
         return x, y, mean, cv
 
