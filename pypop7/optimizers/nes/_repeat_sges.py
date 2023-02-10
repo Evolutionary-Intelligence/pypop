837b1058755e90a76e23aa219d0d71628d3bf58f
Evolutionary-Intelligence <duanqq077@163.com>
2023-02-10 21:16:58 +0800

    Add pypop7/optimizers/nes/_repeat_sges.py

--- pypop7/optimizers/nes/_repeat_sges.py
+++ pypop7/optimizers/nes/_repeat_sges.py
@@ -7,22 +7,20 @@
 
 
 
-    import time
     import numpy as np
     from pybrain.optimization.distributionbased.ves import VanillaGradientEvolutionStrategies as VGES
 
 
-    def sphere(x):
-        return np.sum(np.power(x, 2))
+    def sphere(x):  # for maximization
+        return -np.sum(np.power(x, 2))
 
 
-    solver = VGES(sphere, 4*np.ones((10,)), minimize=True, maxEvaluations=2e6, verbose=True, importanceMixing=False)
-    start_time = time.time()
+    np.random.seed(5)
+    solver = VGES(sphere, 4*np.ones((10,)), maxEvaluations=5e5, verbose=True, importanceMixing=False)
+    solver.x = 4*np.ones((10,))
     solver.learn()
-    print("Runtime: {:7.5e}".format(time.time() - start_time))
-    # Numerical Instability. Stopping.
-    # Evals: 0 Step: 0 best: None
-    # (array([4., 4., 4., 4., 4., 4., 4., 4., 4., 4.]), None)
+    # ('Evals:', 500000)
+    # ('Step:', 4999, 'best:', -2.375081667889928)
 """
 import time
 
@@ -39,15 +37,19 @@ if __name__ == '__main__':
         print('*' * 7 + ' ' + f.__name__ + ' ' + '*' * 7)
         problem = {'fitness_function': f,
                    'ndim_problem': ndim_problem}
-        options = {'max_function_evaluations': 100000 * ndim_problem,
-                   'seed_rng': 0,
+        options = {'max_function_evaluations': 5e5,
+                   'seed_rng': 1,
                    'x': 4 * np.ones((ndim_problem,)),
-                   'n_individuals': 50,
-                   'lr_sigma': 0.02,
                    'saving_fitness': 1,
                    'is_restart': False}
         solver = Solver(problem, options)
         results = solver.optimize()
         print(results)
-        print(results['best_so_far_y'])  # 2.30768171e+00
+        print(results['best_so_far_y'])
+        # seed_rng | best_so_far_y
+        # 0          4.84043450e+00
+        # 1          2.52480970e+00
+        # 2          9.66536022e+00
+        # 3          2.56613399e+00
+        # 4          1.04503107e+01
         print('*** Runtime: {:7.5e}'.format(time.time() - start_run))
