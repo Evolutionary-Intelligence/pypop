8c10a8d8fcbf5a08db5d5419775a18abd6132822
Evolutionary-Intelligence <duanqq077@163.com>
2023-02-10 23:08:38 +0800

    Add pypop7/optimizers/nes/_repeat_ones.py

--- pypop7/optimizers/nes/_repeat_ones.py
+++ pypop7/optimizers/nes/_repeat_ones.py
@@ -7,22 +7,20 @@
 
 
 
-    import time
     import numpy as np
     from pybrain.optimization.distributionbased.nes import OriginalNES as ONES
 
 
     def sphere(x):
-        return np.sum(np.power(x, 2))
+        return -np.sum(np.power(x, 2))
 
 
-    solver = ONES(sphere, 4*np.ones((10,)), minimize=True, maxEvaluations=2e6, verbose=True, importanceMixing=False)
-    start_time = time.time()
+    np.random.seed(5)
+    solver = ONES(sphere, 4*np.ones((10,)), maxEvaluations=5e5, verbose=True, importanceMixing=False)
+    solver.x = 4*np.ones((10,))
     solver.learn()
-    print("Runtime: {:7.5e}".format(time.time() - start_time))
-    # Numerical Instability. Stopping.
-    # Evals: 0 Step: 0 best: None
-    # (array([4., 4., 4., 4., 4., 4., 4., 4., 4., 4.]), None)
+    # ('Evals:', 28200)
+    # ('Step:', 281, 'best:', -5.104246707867472e-20)
 """
 import time
 
@@ -39,15 +37,19 @@ if __name__ == '__main__':
         print('*' * 7 + ' ' + f.__name__ + ' ' + '*' * 7)
         problem = {'fitness_function': f,
                    'ndim_problem': ndim_problem}
-        options = {'max_function_evaluations': 100000 * ndim_problem,
-                   'seed_rng': 0,
+        options = {'max_function_evaluations': 28200,
+                   'seed_rng': 4,
                    'x': 4 * np.ones((ndim_problem,)),
-                   'n_individuals': 50,
-                   'lr_sigma': 0.02,
                    'saving_fitness': 1,
                    'is_restart': False}
         solver = Solver(problem, options)
         results = solver.optimize()
         print(results)
-        print(results['best_so_far_y'])  # 2.04121087e+00
+        print(results['best_so_far_y'])
+        # seed_rng | best_so_far_y
+        # 0          1.21231855e-19
+        # 1          9.55072179e-20
+        # 2          2.02843481e-19
+        # 3          1.38791180e-19
+        # 4          5.10282137e-20
         print('*** Runtime: {:7.5e}'.format(time.time() - start_run))
