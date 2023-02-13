26a09745bc16bef11afd1b120bed14ec54bb2597
Evolutionary-Intelligence <duanqq077@163.com>
2023-02-13 11:02:38 +0800

    Add pypop7/optimizers/cc/__init__.py

--- pypop7/optimizers/cc/__init__.py
+++ pypop7/optimizers/cc/__init__.py
@@ -1,6 +1,6 @@
 from pypop7.optimizers.cc.cc import CC
-from pypop7.optimizers.cc.coea import COEA
-from pypop7.optimizers.cc.cosyne import COSYNE
+from pypop7.optimizers.cc.coea import COEA  # CoOperative co-Evolutionary Algorithm (1994)
+from pypop7.optimizers.cc.cosyne import COSYNE  # CoOperative SYnapse NEuroevolution (COSYNE)
 
 
 __all__ = [CC, COEA, COSYNE]
