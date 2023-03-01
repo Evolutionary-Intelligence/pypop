b12c60778c11e6b092197dc0a74456a51e91b6a1
Evolutionary-Intelligence <duanqq077@163.com>
2023-03-01 12:47:46 +0800

    Update pypop7/optimizers/rs/_test_srs.py

--- pypop7/optimizers/rs/_test_srs.py
+++ pypop7/optimizers/rs/_test_srs.py
@@ -1,7 +1,9 @@
 """This code only tests whether the corresponding optimizer could run or not.
   In other words, it *cannot* validate the correctness regarding its working procedure.
-  For the correctness validation of programming, refer to the following link:
-    https://github.com/Evolutionary-Intelligence/pypop/wiki/Correctness-Validation-of-Optimizers.md
+
+  For its correctness checking of coding, the *code-based repeatability report* cannot be provided owing to
+    the lack of its simulation environment in the original paper. Instead, we used the comparison-based strategy
+    to validate its correctness as much as possible (though there still has a risk to be wrong).
 """
 import unittest
 import time
@@ -20,12 +22,12 @@ class TestSRS(unittest.TestCase):
             print('*' * 7 + ' ' + f.__name__ + ' ' + '*' * 7)
             problem = {'fitness_function': f,
                        'ndim_problem': ndim_problem,
-                       'lower_boundary': -5 * np.ones((ndim_problem,)),
-                       'upper_boundary': 5 * np.ones((ndim_problem,))}
+                       'lower_boundary': -5*np.ones((ndim_problem,)),
+                       'upper_boundary': 5*np.ones((ndim_problem,))}
             options = {'max_function_evaluations': 2e6,
                        'fitness_threshold': 1e-10,
                        'seed_rng': 0,
-                       'x': 4 * np.ones((ndim_problem,)),
+                       'x': 4*np.ones((ndim_problem,)),
                        'sigma': 0.1,
                        'verbose': 200000,
                        'saving_fitness': 200000}
