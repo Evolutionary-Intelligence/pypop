14239bb55d09912a445c920eefcd99e5d6c654b9
Evolutionary-Intelligence <duanqq077@163.com>
2023-03-01 12:21:10 +0800

    Update pypop7/optimizers/rs/__init__.py

--- pypop7/optimizers/rs/__init__.py
+++ pypop7/optimizers/rs/__init__.py
@@ -1,4 +1,4 @@
-from pypop7.optimizers.rs.rs import RS
+from pypop7.optimizers.rs.rs import RS  # abstract class for all RS subclasses
 from pypop7.optimizers.rs.prs import PRS
 from pypop7.optimizers.rs.rhc import RHC
 from pypop7.optimizers.rs.arhc import ARHC
@@ -6,5 +6,9 @@ from pypop7.optimizers.rs.srs import SRS
 from pypop7.optimizers.rs.bes import BES
 
 
-__all__ = [RS, PRS, RHC, ARHC, SRS,
-           BES]  # especially for large-scale black-box optimization
+__all__ = [RS,  # Random (stochastic) Search (optimization) (RS)
+           PRS,  # Pure Random Search (PRS)
+           RHC,  # Random (stochastic) Hill Climber (RHC)
+           ARHC,  # Annealed Random Hill Climber (ARHC)
+           SRS,  # Simple Random Search (SRS)
+           BES]  # BErnoulli Smoothing (BES), especially for large-scale black-box optimization
