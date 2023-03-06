8541629c3367fa4a235aee19ff80d7fafafa9380
Evolutionary-Intelligence <duanqq077@163.com>
2023-03-06 19:28:24 +0800

    Update pypop7/optimizers/cc/__init__.py

--- pypop7/optimizers/cc/__init__.py
+++ pypop7/optimizers/cc/__init__.py
@@ -1,7 +1,12 @@
-from pypop7.optimizers.cc.cc import CC
-from pypop7.optimizers.cc.coea import COEA  # CoOperative co-Evolutionary Algorithm (1994)
-from pypop7.optimizers.cc.cosyne import COSYNE  # CoOperative SYnapse NEuroevolution (2008)
-from pypop7.optimizers.cc.cocma import COCMA  # CoOperative CO-evolutionary Covariance Matrix Adaptation
+from pypop7.optimizers.cc.cc import CC  # abstract class for all CC classes
+from pypop7.optimizers.cc.coea import COEA
+from pypop7.optimizers.cc.hcc import HCC
+from pypop7.optimizers.cc.cosyne import COSYNE
+from pypop7.optimizers.cc.cocma import COCMA
 
 
-__all__ = [CC, COEA, COSYNE, COCMA]
+__all__ = [CC,  # Cooperative Coevolution (CC)
+           COEA,  # CoOperative co-Evolutionary Algorithm (COEA)
+           HCC,  # Hierarchical Cooperative Co-evolution (HCC)
+           COSYNE,  # CoOperative SYnapse NEuroevolution (COSYNE)
+           COCMA]  # CoOperative CO-evolutionary Covariance Matrix Adaptation (COCMA)
