1bc51ac676ef73bdcbee5655ea1ab36dfcd8564f
Evolutionary-Intelligence <duanqq077@163.com>
2023-03-08 17:04:48 +0800

    Update pypop7/optimizers/ga/__init__.py

--- pypop7/optimizers/ga/__init__.py
+++ pypop7/optimizers/ga/__init__.py
@@ -1,8 +1,12 @@
-from pypop7.optimizers.ga.ga import GA
+from pypop7.optimizers.ga.ga import GA  # abstract class for all GA subclasses
 from pypop7.optimizers.ga.genitor import GENITOR
 from pypop7.optimizers.ga.g3pcx import G3PCX
 from pypop7.optimizers.ga.gl25 import GL25
 from pypop7.optimizers.ga.asga import ASGA
 
 
-__all__ = [GA, GENITOR, G3PCX, GL25, ASGA]
+__all__ = [GA,  # Genetic Algorithm (GA)
+           GENITOR,  # GENetic ImplemenTOR (GENITOR)
+           G3PCX,  # Generalized Generation Gap with Parent-Centric Recombination (G3PCX)
+           GL25,  # Global and Local genetic algorithm (GL25)
+           ASGA]  # Active Subspace Genetic Algorithm (ASGA)
