e78c4019eadb92dccefa98afbc357fe581935a7d
Evolutionary-Intelligence <duanqq077@163.com>
2023-03-08 20:55:06 +0800

    Update pypop7/optimizers/ga/_test_g3pcx.py

--- pypop7/optimizers/ga/_test_g3pcx.py
+++ pypop7/optimizers/ga/_test_g3pcx.py
@@ -1,7 +1,7 @@
 """This code only tests whether the corresponding optimizer could run or not.
   In other words, it *cannot* validate the correctness regarding its working procedure.
   For the correctness validation of programming, refer to the following link:
-    https://github.com/Evolutionary-Intelligence/pypop/wiki/Correctness-Validation-of-Optimizers.md
+    https://github.com/Evolutionary-Intelligence/pypop/blob/main/pypop7/optimizers/ga/g3pcx.py
 """
 import unittest
 import time
@@ -20,8 +20,8 @@ class TestG3PCX(unittest.TestCase):
             print('*' * 7 + ' ' + f.__name__ + ' ' + '*' * 7)
             problem = {'fitness_function': f,
                        'ndim_problem': ndim_problem,
-                       'lower_boundary': -5 * np.ones((ndim_problem,)),
-                       'upper_boundary': 5 * np.ones((ndim_problem,))}
+                       'lower_boundary': -5*np.ones((ndim_problem,)),
+                       'upper_boundary': 5*np.ones((ndim_problem,))}
             options = {'max_function_evaluations': 2e6,
                        'fitness_threshold': 1e-10,
                        'max_runtime': 3600,  # 1 hours
