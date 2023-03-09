07ccd5c55ff24e4ffff0517c7dd4c2511229496e
Evolutionary-Intelligence <duanqq077@163.com>
2023-03-09 22:54:08 +0800

    Update pypop7/optimizers/pso/_repeat_ipsols.py

--- pypop7/optimizers/pso/_repeat_ipsols.py
+++ pypop7/optimizers/pso/_repeat_ipsols.py
@@ -1,11 +1,11 @@
-"""Repeat the following paper for `IPSO`:
+"""Repeat the following paper for `IPSOLS`:
     De Oca, M.A.M., Stutzle, T., Van den Enden, K. and Dorigo, M., 2010.
     Incremental social learning in particle swarms.
     IEEE Transactions on Systems, Man, and Cybernetics, Part B (Cybernetics), 41(2), pp.368-384.
     https://ieeexplore.ieee.org/document/5582312
 
     Luckily our code could repeat the data reported in the original paper *well*.
-    Therefore, we argue that the repeatability of `IPSO` could be **well-documented**.
+    Therefore, we argue that its repeatability could be **well-documented**.
 """
 import time
 
@@ -21,8 +21,8 @@ if __name__ == '__main__':
 
     problem = {'fitness_function': sphere,
                'ndim_problem': ndim_problem,
-               'lower_boundary': -100 * np.ones((ndim_problem,)),
-               'upper_boundary': 100 * np.ones((ndim_problem,))}
+               'lower_boundary': -100*np.ones((ndim_problem,)),
+               'upper_boundary': 100*np.ones((ndim_problem,))}
     options = {'seed_rng': 0,  # not given in the original paper
                'max_function_evaluations': 1e6}
     solver = Solver(problem, options)
@@ -32,19 +32,19 @@ if __name__ == '__main__':
 
     problem = {'fitness_function': griewank,
                'ndim_problem': ndim_problem,
-               'lower_boundary': -600 * np.ones((ndim_problem,)),
-               'upper_boundary': 600 * np.ones((ndim_problem,))}
+               'lower_boundary': -600*np.ones((ndim_problem,)),
+               'upper_boundary': 600*np.ones((ndim_problem,))}
     options = {'seed_rng': 0,  # not given in the original paper
                'max_function_evaluations': 1e6}
     solver = Solver(problem, options)
     results = solver.optimize()
-    print(results)  # 0.0e0 vs 0.0e0 (from original paper)
+    print(results)  # 1.1102230246251565e-16 vs 0.0e0 (from original paper)
     print('*** Runtime: {:7.5e}'.format(time.time() - start_run))
 
     problem = {'fitness_function': rastrigin,
                'ndim_problem': ndim_problem,
-               'lower_boundary': -5.12 * np.ones((ndim_problem,)),
-               'upper_boundary': 5.12 * np.ones((ndim_problem,))}
+               'lower_boundary': -5.12*np.ones((ndim_problem,)),
+               'upper_boundary': 5.12*np.ones((ndim_problem,))}
     options = {'seed_rng': 0,  # not given in the original paper
                'max_function_evaluations': 1e6}
     solver = Solver(problem, options)
