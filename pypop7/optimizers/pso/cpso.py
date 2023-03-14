8c0ec916bd69fe152cb8d8fa2b5b14470b6bc8af
Evolutionary-Intelligence <duanqq077@163.com>
2023-03-14 16:18:51 +0800

    Update pypop7/optimizers/pso/cpso.py

--- pypop7/optimizers/pso/cpso.py
+++ pypop7/optimizers/pso/cpso.py
@@ -80,14 +80,15 @@ class CPSO(PSO):
         self._w = 1.0 - (np.arange(self._max_generations) + 1.0)/self._max_generations  # from 1.0 to 0.0
 
     def iterate(self, v=None, x=None, y=None, p_x=None, p_y=None, n_x=None, args=None):
+        fitness = []
         for j in range(self.ndim_problem):
             if self._check_terminations():
-                return v, x, y, p_x, p_y, n_x
+                return v, x, y, p_x, p_y, n_x, fitness
             cognition_rand = self.rng_optimization.uniform(size=(self.n_individuals, self.ndim_problem))
             society_rand = self.rng_optimization.uniform(size=(self.n_individuals, self.ndim_problem))
             for i in range(self.n_individuals):
                 if self._check_terminations():
-                    return v, x, y, p_x, p_y, n_x
+                    return v, x, y, p_x, p_y, n_x, fitness
                 n_x[i, j] = p_x[np.argmin(p_y), j]
                 v[i, j] = (self._w[self._n_generations]*v[i, j] +
                            self.cognition*cognition_rand[i, j]*(p_x[i, j] - x[i, j]) +
@@ -97,15 +98,17 @@ class CPSO(PSO):
                 xx = np.copy(self.best_so_far_x)
                 xx[j] = x[i, j]
                 y[i] = self._evaluate_fitness(xx, args)
+                fitness.append(y[i])
                 if y[i] < p_y[i]:  # online update
                     p_x[i, j], p_y[i] = x[i, j], y[i]
         self._n_generations += 1
-        return v, x, y, p_x, p_y, n_x
+        return v, x, y, p_x, p_y, n_x, fitness
 
     def optimize(self, fitness_function=None, args=None):
         fitness = Optimizer.optimize(self, fitness_function)
         v, x, y, p_x, p_y, n_x = self.initialize(args)
+        yy = y  # only for printing
         while not self.termination_signal:
-            self._print_verbose_info(fitness, y)
-            v, x, y, p_x, p_y, n_x = self.iterate(v, x, y, p_x, p_y, n_x, args)
-        return self._collect(fitness, y)
+            self._print_verbose_info(fitness, yy)
+            v, x, y, p_x, p_y, n_x, yy = self.iterate(v, x, y, p_x, p_y, n_x, args)
+        return self._collect(fitness, yy)
