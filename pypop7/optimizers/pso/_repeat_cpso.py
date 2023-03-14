8fc6af8931b0a6b3c15621e63d43764dbc9e4724
Evolutionary-Intelligence <duanqq077@163.com>
2023-03-14 14:03:46 +0800

    Update pypop7/optimizers/pso/_repeat_cpso.py

--- pypop7/optimizers/pso/_repeat_cpso.py
+++ pypop7/optimizers/pso/_repeat_cpso.py
@@ -4,7 +4,7 @@
     IEEE Transactions on Evolutionary Computation, 8(3), pp.225-239.
     https://ieeexplore.ieee.org/document/1304845
 
-    Luckily our code could repeat the data reported in the original paper *well*.
+    Luckily our Python code could repeat the data reported in the original paper *well*.
     Therefore, we argue that its repeatability could be **well-documented**.
 """
 import time
@@ -21,8 +21,8 @@ if __name__ == '__main__':
 
     problem = {'fitness_function': rosenbrock,
                'ndim_problem': ndim_problem,
-               'lower_boundary': -2.048 * np.ones((ndim_problem,)),
-               'upper_boundary': 2.048 * np.ones((ndim_problem,))}
+               'lower_boundary': -2.048*np.ones((ndim_problem,)),
+               'upper_boundary': 2.048*np.ones((ndim_problem,))}
     options = {'seed_rng': 0,  # not given in the original paper
                'max_function_evaluations': 2e5,
                'fitness_threshold': 0.0}
@@ -33,8 +33,8 @@ if __name__ == '__main__':
 
     problem = {'fitness_function': griewank,
                'ndim_problem': ndim_problem,
-               'lower_boundary': -600 * np.ones((ndim_problem,)),
-               'upper_boundary':600 * np.ones((ndim_problem,))}
+               'lower_boundary': -600*np.ones((ndim_problem,)),
+               'upper_boundary': 600*np.ones((ndim_problem,))}
     options = {'seed_rng': 0,  # not given in the original paper
                'max_function_evaluations': 2e5,
                'fitness_threshold': 0.0}
