7d2ab8465c408bbcf68cd13515c9c85494f09e61
Evolutionary-Intelligence <duanqq077@163.com>
2023-03-19 05:01:31 +0800

    Update pypop7/optimizers/ds/gps.py

--- pypop7/optimizers/ds/gps.py
+++ pypop7/optimizers/ds/gps.py
@@ -6,11 +6,11 @@ from pypop7.optimizers.ds.ds import DS
 class GPS(DS):
     """Generalized Pattern Search (GPS).
 
-    .. note:: "To converge to a local minimum, certain conditions must be met. The set of directions must
+    .. note:: `"To converge to a local minimum, certain conditions must be met. The set of directions must
         be a positive spanning set, which means that we can construct any point using a nonnegative
         linear combination of the directions. A positive spanning set ensures that at least one of the
-        directions is a descent direction from a location with a nonzero gradient."
-        ---[Kochenderfer&Wheeler, 2019]
+        directions is a descent direction from a location with a nonzero gradient."---[Kochenderfer&Wheeler, 2019]
+        <https://algorithmsbook.com/optimization/files/chapter-7.pdf>`_
 
     Parameters
     ----------
@@ -23,16 +23,20 @@ class GPS(DS):
     options : dict
               optimizer options with the following common settings (`keys`):
                 * 'max_function_evaluations' - maximum of function evaluations (`int`, default: `np.Inf`),
-                * 'max_runtime'              - maximal runtime (`float`, default: `np.Inf`),
+                * 'max_runtime'              - maximal runtime to be allowed (`float`, default: `np.Inf`),
                 * 'seed_rng'                 - seed for random number generation needed to be *explicitly* set (`int`);
               and with the following particular settings (`keys`):
+                * 'sigma' - initial global step-size (`float`, default: `1.0`),
                 * 'x'     - initial (starting) point (`array_like`),
-                * 'sigma' - initial (global) step-size (`float`),
+
+                  * if not given, it will draw a random sample from the uniform distribution whose search range is
+                    bounded by `problem['lower_boundary']` and `problem['upper_boundary']`.
+
                 * 'gamma' - decreasing factor of step-size (`float`, default: `0.5`).
 
     Examples
     --------
-    Use the Pattern Search optimizer `GPS` to minimize the well-known test function
+    Use the optimizer to minimize the well-known test function
     `Rosenbrock <http://en.wikipedia.org/wiki/Rosenbrock_function>`_:
 
     .. code-block:: python
@@ -43,11 +47,11 @@ class GPS(DS):
        >>> from pypop7.optimizers.ds.gps import GPS
        >>> problem = {'fitness_function': rosenbrock,  # define problem arguments
        ...            'ndim_problem': 2,
-       ...            'lower_boundary': -5 * numpy.ones((2,)),
-       ...            'upper_boundary': 5 * numpy.ones((2,))}
+       ...            'lower_boundary': -5*numpy.ones((2,)),
+       ...            'upper_boundary': 5*numpy.ones((2,))}
        >>> options = {'max_function_evaluations': 5000,  # set optimizer options
        ...            'seed_rng': 2022,
-       ...            'x': 3 * numpy.ones((2,)),
+       ...            'x': 3*numpy.ones((2,)),
        ...            'sigma': 0.1,
        ...            'verbose_frequency': 500}
        >>> gps = GPS(problem, options)  # initialize the optimizer class
@@ -58,12 +62,12 @@ class GPS(DS):
 
     Attributes
     ----------
-    x     : `array_like`
-            starting search point.
-    sigma : `float`
-            final (global) step-size.
     gamma : `float`
             decreasing factor of step-size.
+    sigma : `float`
+            final global step-size (changed during optimization).
+    x     : `array_like`
+            initial (starting) point.
 
     References
     ----------
@@ -95,22 +99,21 @@ class GPS(DS):
         i_d = [i for i in range(d.shape[0])]  # index of used directions
         return x, y, d, i_d
 
-    def iterate(self, args=None, x=None, d=None, i_d=None, fitness=None):
-        improved, best_so_far_y = False, self.best_so_far_y
+    def iterate(self, x=None, d=None, i_d=None, args=None):
+        improved, best_so_far_y, fitness = False, self.best_so_far_y, []
         for i in range(d.shape[0]):
             if self._check_terminations():
-                return i_d
+                return i_d, fitness
             x = self.best_so_far_x + self.sigma*d[i_d[i]]  # opportunistic
             y = self._evaluate_fitness(x, args)
-            if self.saving_fitness:
-                fitness.append(y)
+            fitness.append(y)
             if y < best_so_far_y:
                 improved = True
                 i_d = [i_d[i]] + i_d[:i] + i_d[(i + 1):]  # dynamic ordering
                 break
         if not improved:
             self.sigma *= self.gamma  # alpha
-        return i_d
+        return i_d, fitness
 
     def restart_reinitialize(self, args=None, x=None, y=None, d=None, i_d=None, fitness=None):
         self._fitness_list.append(self.best_so_far_y)
@@ -119,30 +122,25 @@ class GPS(DS):
             is_restart_2 = (self._fitness_list[-self.stagnation] - self._fitness_list[-1]) < self.fitness_diff
         is_restart = bool(is_restart_1) or bool(is_restart_2)
         if is_restart:
+            self._print_verbose_info(fitness, y)
             self.sigma = np.copy(self._sigma_bak)
             x, y, d, i_d = self.initialize(args, is_restart)
-            if self.saving_fitness:
-                fitness.append(y)
             self._fitness_list = [self.best_so_far_y]
             self._n_generations = 0
             self._n_restart += 1
             if self.verbose:
-                print(' ....... restart .......')
-            self._print_verbose_info(y)
+                print(' ....... *** restart *** .......')
         return x, y, d, i_d
 
     def optimize(self, fitness_function=None, args=None):
         fitness = DS.optimize(self, fitness_function)
         x, y, d, i_d = self.initialize(args)
-        if self.saving_fitness:
-            fitness.append(y)
-        self._print_verbose_info(y)
         while True:
-            i_d = self.iterate(args, x, d, i_d, fitness)
+            self._print_verbose_info(fitness, y)
+            i_d, y = self.iterate(x, d, i_d, args)
             if self._check_terminations():
                 break
             self._n_generations += 1
-            self._print_verbose_info(y)
             if self.is_restart:
                 x, y, d, i_d = self.restart_reinitialize(args, x, y, d, i_d, fitness)
-        return self._collect_results(fitness)
+        return self._collect(fitness, y)
