4339046614bb1aa4954e78b420f0f7a0682ffae4
Evolutionary-Intelligence <duanqq077@163.com>
2023-03-18 22:55:21 +0800

    Update pypop7/optimizers/ds/hj.py

--- pypop7/optimizers/ds/hj.py
+++ pypop7/optimizers/ds/hj.py
@@ -6,10 +6,10 @@ from pypop7.optimizers.ds.ds import DS
 class HJ(DS):
     """Hooke-Jeeves direct (pattern) search method (HJ).
 
-    .. note:: `HJ` is one of the most-popular and most-cited `DS` methods, originally published in one of
-       the *top-tier* Computer Science journals (**JACM**) in 1961. Although sometimes it is still used to optimize
-       *low-dimensional* black-box problems, it is **highly recommended** to attempt other more advanced methods
-       for large-scale black-box optimization.
+    .. note:: `HJ` is one of the most-popular and most-cited `DS` methods, originally published in one *top-tier*
+       Computer Science journal (i.e., `JACM <http://garfield.library.upenn.edu/classics1980/A1980JK10100001.pdf>`_)
+       in 1961. Although sometimes it is still used to optimize *low-dimensional* black-box problems, it is **highly
+       recommended** to attempt other more advanced methods for large-scale black-box optimization.
 
     Parameters
     ----------
@@ -25,7 +25,7 @@ class HJ(DS):
                 * 'max_runtime'              - maximal runtime to be allowed (`float`, default: `np.Inf`),
                 * 'seed_rng'                 - seed for random number generation needed to be *explicitly* set (`int`);
               and with the following particular settings (`keys`):
-                * 'sigma' - initial global step-size (`float`),
+                * 'sigma' - initial global step-size (`float`, default: `1.0`),
                 * 'x'     - initial (starting) point (`array_like`),
 
                   * if not given, it will draw a random sample from the uniform distribution whose search range is
@@ -35,7 +35,7 @@ class HJ(DS):
 
     Examples
     --------
-    Use the `DS` optimizer `HJ` to minimize the well-known test function
+    Use the optimizer to minimize the well-known test function
     `Rosenbrock <http://en.wikipedia.org/wiki/Rosenbrock_function>`_:
 
     .. code-block:: python
@@ -67,9 +67,9 @@ class HJ(DS):
     gamma : `float`
             decreasing factor of global step-size.
     sigma : `float`
-            final global step-size.
+            final global step-size (changed during optimization).
     x     : `array_like`
-            starting search point.
+            initial (starting) point.
 
     References
     ----------
@@ -78,6 +78,8 @@ class HJ(DS):
     MIT Press.
     https://algorithmsbook.com/optimization/files/chapter-7.pdf
     (See Algorithm 7.5 (Page 104) for details.)
+
+    http://garfield.library.upenn.edu/classics1980/A1980JK10100001.pdf
 
     Kaupe Jr, A.F., 1963.
     Algorithm 178: Direct search.
@@ -92,29 +94,29 @@ class HJ(DS):
     def __init__(self, problem, options):
         DS.__init__(self, problem, options)
         self.gamma = options.get('gamma', 0.5)  # decreasing factor of global step-size (γ)
-        assert self.gamma > 0.0, f'`self.gamma` == {self.gamma}, but should > 0.0.'
+        assert self.gamma > 0.0
 
     def initialize(self, args=None, is_restart=False):
         x = self._initialize_x(is_restart)  # initial (starting) search point
         y = self._evaluate_fitness(x, args)  # fitness
         return x, y
 
-    def iterate(self, args=None, x=None, fitness=None):
+    def iterate(self, x=None, args=None):
+        fitness = []
         improved, best_so_far_x, best_so_far_y = False, self.best_so_far_x, self.best_so_far_y
         for i in range(self.ndim_problem):  # to search along each coordinate
             for sgn in [-1, 1]:  # for two opponent directions
                 if self._check_terminations():
-                    return None
+                    return fitness
                 xx = np.copy(best_so_far_x)
                 xx[i] += sgn*self.sigma
                 y = self._evaluate_fitness(xx, args)
-                if self.saving_fitness:
-                    fitness.append(y)
+                fitness.append(y)
                 if y < best_so_far_y:
                     best_so_far_y, improved = y, True
         if not improved:  # to decrease step-size if no improvement
             self.sigma *= self.gamma  # alpha
-        return None
+        return fitness
 
     def restart_reinitialize(self, args=None, x=None, y=None, fitness=None):
         self._fitness_list.append(self.best_so_far_y)
@@ -123,30 +125,25 @@ class HJ(DS):
             is_restart_2 = (self._fitness_list[-self.stagnation] - self._fitness_list[-1]) < self.fitness_diff
         is_restart = bool(is_restart_1) or bool(is_restart_2)
         if is_restart:
+            self._print_verbose_info(fitness, y)
             self.sigma = np.copy(self._sigma_bak)
             x, y = self.initialize(args, is_restart)
-            if self.saving_fitness:
-                fitness.append(y)
             self._fitness_list = [self.best_so_far_y]
             self._n_generations = 0
             self._n_restart += 1
             if self.verbose:
-                print(' ....... restart .......')
-            self._print_verbose_info(y)
+                print(' ....... *** restart *** .......')
         return x, y
 
     def optimize(self, fitness_function=None, args=None):
         fitness = DS.optimize(self, fitness_function)
         x, y = self.initialize(args)
-        if self.saving_fitness:
-            fitness.append(y)
-        self._print_verbose_info(y)
         while True:
-            self.iterate(args, x, fitness)
+            self._print_verbose_info(fitness, y)
+            y = self.iterate(x, args)
             if self._check_terminations():
                 break
             self._n_generations += 1
-            self._print_verbose_info(y)
             if self.is_restart:
                 x, y = self.restart_reinitialize(args, x, y, fitness)
-        return self._collect_results(fitness)
+        return self._collect(fitness, y)
