2aa07b709212f1e2031c2e3cd93e0ee363305a87
Evolutionary-Intelligence <duanqq077@163.com>
2023-03-19 19:34:25 +0800

    Update pypop7/optimizers/ds/__init__.py

--- pypop7/optimizers/ds/__init__.py
+++ pypop7/optimizers/ds/__init__.py
@@ -1,8 +1,14 @@
-from pypop7.optimizers.ds.ds import DS
+from pypop7.optimizers.ds.ds import DS  # abstract class for all DS classes
 from pypop7.optimizers.ds.cs import CS
 from pypop7.optimizers.ds.hj import HJ
 from pypop7.optimizers.ds.nm import NM
 from pypop7.optimizers.ds.gps import GPS
+from pypop7.optimizers.ds.powell import POWELL
 
 
-__all__ = [DS, CS, HJ, NM, GPS]
+__all__ = [DS,  # Direct Search (DS)
+           CS,  # Coordinate Search (CS)
+           HJ,  # Hooke-Jeeves direct (pattern) search method (HJ)
+           NM,  # Nelder-Mead simplex method (NM)
+           GPS,  # Generalized Pattern Search (GPS)
+           POWELL]  # Powell's method (POWELL)
