51be71a4cea5452976317e071dbb5c9223a38e70
Evolutionary-Intelligence <duanqq077@163.com>
2023-03-20 20:49:24 +0800

    Update pypop7/optimizers/ds/powell.py

--- pypop7/optimizers/ds/powell.py
+++ pypop7/optimizers/ds/powell.py
@@ -1,8 +1,86 @@
 import numpy as np
-from scipy.optimize.optimize import _wrap_function as wf,\
-    _line_for_search as ls, _minimize_scalar_bounded as msb
+from scipy.optimize.optimize import _wrap_function as wf, _line_for_search as ls
 
 from pypop7.optimizers.ds.ds import DS
+
+
+def _minimize_scalar_bounded(func, bounds,
+                             max_function_evaluations, fitness_threshold,
+                             tol=1e-5, max_iterations=500):
+    # this is adopted from https://github.com/scipy/scipy/blob/main/scipy/optimize/_optimize.py
+    #   with slight modifications
+    n_function_evaluations, num_iterations, yy = 0, 0, []
+    a, b = bounds
+    sqrt_eps, golden_mean = 1.4832396974191326e-08, 0.3819660112501051
+    gm = a + golden_mean*(b - a)
+    gm_1 = gm_2 = gm
+    rat = e = 0.0
+    y = func(gm_2)
+    n_function_evaluations += 1
+    yy.append(y)
+    if (n_function_evaluations == max_function_evaluations) or (y < fitness_threshold):
+        return y, gm_2, yy
+    y_1 = y_2 = y
+    middle = 0.5*(a + b)
+    tol_1 = sqrt_eps*np.abs(gm_2) + tol/3.0
+    tol_2 = 2.0*tol_1
+    while np.abs(gm_2 - middle) > (tol_2 - 0.5*(b - a)):
+        golden = 1
+        if np.abs(e) > tol_1:
+            golden = 0
+            r = (gm_2 - gm_1)*(y - y_1)
+            q = (gm_2 - gm)*(y - y_2)
+            p = (gm_2 - gm)*q - (gm_2 - gm_1)*r
+            q = 2.0*(q - r)
+            if q > 0.0:
+                p = -p
+            q = np.abs(q)
+            r = e
+            e = rat
+            if (np.abs(p) < np.abs(0.5*q*r)) and (p > q*(a - gm_2)) and (p < q*(b - gm_2)):
+                rat = (p + 0.0)/q
+                x = gm_2 + rat
+                if ((x - a) < tol_2) or ((b - x) < tol_2):
+                    rat = tol_1*(np.sign(middle - gm_2) + ((middle - gm_2) == 0))
+            else:
+                golden = 1
+        if golden:
+            if gm_2 >= middle:
+                e = a - gm_2
+            else:
+                e = b - gm_2
+            rat = golden_mean*e
+        x = gm_2 + (np.sign(rat) + (rat == 0))*np.maximum(np.abs(rat), tol_1)
+        yyy = func(x)
+        n_function_evaluations += 1
+        yy.append(y)
+        if (n_function_evaluations == max_function_evaluations) or (y < fitness_threshold):
+            return y, gm_2, yy
+        if yyy <= y:
+            if x >= gm_2:
+                a = gm_2
+            else:
+                b = gm_2
+            gm, y_1 = gm_1, y_2
+            gm_1, y_2 = gm_2, y
+            gm_2, y = x, yyy
+        else:
+            if x < gm_2:
+                a = x
+            else:
+                b = x
+            if (yyy <= y_2) or (gm_1 == gm_2):
+                gm, y_1 = gm_1, y_2
+                gm_1, y_2 = x, yyy
+            elif (yyy <= y_1) or (gm == gm_2) or (gm == gm_1):
+                gm, y_1 = x, yyy
+        middle = 0.5*(a + b)
+        tol_1 = sqrt_eps*np.abs(gm_2) + tol/3.0
+        tol_2 = 2.0*tol_1
+        num_iterations += 1
+        if num_iterations == max_iterations - 1:
+            break
+    return y, gm_2, yy
 
 
 class POWELL(DS):
@@ -43,17 +121,18 @@ class POWELL(DS):
        >>> from pypop7.benchmarks.base_functions import rosenbrock  # function to be minimized
        >>> from pypop7.optimizers.ds.powell import POWELL
        >>> problem = {'fitness_function': rosenbrock,  # define problem arguments
-       ...            'ndim_problem': 2,
-       ...            'lower_boundary': -5*numpy.ones((2,)),
-       ...            'upper_boundary': 5*numpy.ones((2,))}
+       ...            'ndim_problem': 20,
+       ...            'lower_boundary': -5*numpy.ones((20,)),
+       ...            'upper_boundary': 5*numpy.ones((20,))}
        >>> options = {'max_function_evaluations': 5000,  # set optimizer options
        ...            'seed_rng': 2022,
-       ...            'x': 3*numpy.ones((2,)),
+       ...            'x': 3*numpy.ones((20,)),
        ...            'verbose_frequency': 500}
        >>> powell = POWELL(problem, options)  # initialize the optimizer class
        >>> results = powell.optimize()  # run the optimization process
        >>> # return the number of function evaluations and best-so-far fitness
        >>> print(f"POWELL: {results['n_function_evaluations']}, {results['best_so_far_y']}")
+       POWELL: 50000, 0.0
 
     Attributes
     ----------
@@ -88,27 +167,29 @@ class POWELL(DS):
         _, self._func = wf(self._evaluate_fitness, args=args)
         return x, y, u, y
 
-    def _line_search(self, x, d, tol):
+    def _line_search(self, x, d, tol=1e-4*100):
         def _func(alpha):  # only for line search
             return self._func(x + alpha*d)
 
         bound = ls(x, d, self.lower_boundary, self.upper_boundary)
-        res = msb(_func, bound, xatol=tol/100.0)
-        d *= res.x
-        return res.fun, x + d, d
+        y, gm, yy = _minimize_scalar_bounded(_func, bound,
+                                             self.max_function_evaluations - self.n_function_evaluations,
+                                             self.fitness_threshold, tol/100.0)
+        d *= gm
+        return y, x + d, d, yy
 
     def iterate(self, x=None, y=None, u=None, args=None):
         xx, yy = np.copy(x), np.copy(y)
-        ind, delta, ys = 0, 0.0, []
+        big_ind, delta, ys = 0, 0.0, []
         for i in range(self.ndim_problem):
             if self._check_terminations():
                 return x, y, u, ys
             d, diff = u[i], y
-            y, x, d = self._line_search(x, d, 1e-4*100)
-            ys.append(y)
+            y, x, d, fitness = self._line_search(x, d)
+            ys.extend(fitness)
             diff -= y
             if diff > delta:
-                delta, ind = diff, i
+                delta, big_ind = diff, i
         d = x - xx  # extrapolated point
         _, ratio_e = ls(x, d, self.lower_boundary, self.upper_boundary)
         xxx = x + min(ratio_e, 1.0)*d
@@ -119,10 +200,10 @@ class POWELL(DS):
             temp = yy - yyy
             t -= delta*np.square(temp)
             if t < 0.0:
-                y, x, d = self._line_search(x, d, 1e-4*100)
-                ys.append(y)
+                y, x, d, fitness = self._line_search(x, d)
+                ys.extend(fitness)
                 if np.any(d):
-                    u[ind] = u[-1]
+                    u[big_ind] = u[-1]
                     u[-1] = d
         self._n_generations += 1
         return x, y, u, ys
