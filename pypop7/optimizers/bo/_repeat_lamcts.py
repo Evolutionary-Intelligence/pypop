a394c2821b7a21089c7ebbb019296a49c9f81e67
Evolutionary-Intelligence <duanqq077@163.com>
2023-03-25 02:32:06 +0800

    Update pypop7/optimizers/bo/_repeat_lamcts.py

--- pypop7/optimizers/bo/_repeat_lamcts.py
+++ pypop7/optimizers/bo/_repeat_lamcts.py
@@ -1,87 +1,34 @@
-"""Repeat Figure 6 in paper
-    Latent Action Monto Carlo Tree Search(LA-MCTS)
-    Reference
-    --------------
-    L. Wang, R. Fonseca, Y. Tian
-    Learning Search Space Partition for Black-box Optimization using Monte Carlo Tree Search
-    NeurIPS 2020
-    https://proceedings.neurips.cc/paper/2020/hash/e2ce14e81dba66dbff9cbc35ecfdb704-Abstract.html
-    Code of paper:
-    https://github.com/facebookresearch/LaMCTS/tree/main/LA-MCTS
-"""
-import numpy as np
-import matplotlib.pyplot as plt
-import seaborn as sns
-import pickle
+import time
 
-from pypop7.benchmarks.base_functions import ackley, rosenbrock
+import numpy as np
+
 from pypop7.optimizers.bo.lamcts import LAMCTS as Solver
 
-sns.set_theme(style='darkgrid')
+
+class Levy(object):
+    def __init__(self, ndim):
+        self.ndim = ndim
+
+    def __call__(self, x):
+        w = 1.0 + (x - 1.0)/4.0
+        y = np.sin(np.pi*w[0])**2 + np.sum((w[1:self.ndim - 1] - 1.0)**2 * (
+                1.0 + 10.0*np.sin(np.pi*w[1:self.ndim - 1] + 1.0)**2)) + (
+                w[self.ndim - 1] - 1.0)**2 * (1.0 + np.sin(2.0*np.pi*w[self.ndim - 1])**2)
+        return y
 
 
-def read_pickle(function, ndim):
-    file = function + '_' + str(ndim) + '.pickle'
-    with open(file, 'rb') as handle:
-        result = pickle.load(handle)
-        return result
-
-
-def write_pickle(function, ndim, result):
-    file = open(function + '_' + str(ndim) + '.pickle', 'wb')
-    pickle.dump(result, file)
-    file.close()
-
-
-def plot(function, problem_dim):
-    result = read_pickle(function, problem_dim)
-    print("1")
-    plt.figure()
-    if function == "ackley":
-        plt.ylim([0, 20])
-        plt.yticks([0, 5, 10, 15])
-    elif function == "rosenbrock":
-        plt.yscale('log')
-        plt.ylim([1e1, 1e8])
-        plt.yticks([1e2, 1e3, 1e4, 1e5, 1e6, 1e7])
-    if problem_dim == 20:
-        plt.xlim([0, 1000])
-        plt.xticks([0, 200, 400, 600, 800, 1000])
-    elif problem_dim == 100:
-        plt.xlim([0, 10000])
-        plt.yticks([0, 2e3, 4e3, 6e3, 8e3, 1e4])
-    plt.plot(result['fitness'][:, 0], result['fitness'][:, 1], color='orange')
-    plt.xlabel("# samples")
-    plt.ylabel("f(x)")
-    plt.title(function.capitalize() + "-" + str(problem_dim) + "d")
-    plt.show()
-
-
-if __name__ == "__main__":
-    for f in [ackley, rosenbrock]:
-        for d in [20, 100]:
-            print('*' * 7 + ' ' + f.__name__ + ' ' + '*' * 7)
-            problem = {'fitness_function': f,
-                       'ndim_problem': d,
-                       'lower_boundary': -5 * np.ones((d,)),
-                       'upper_boundary': 10 * np.ones((d,))}
-            options = {'max_function_evaluations': 1000,
-                       'fitness_threshold': 1e-5,
-                       'seed_rng': 0,
-                       'n_individuals': 40,  # the number of random samples used in initialization
-                       'Cp': 1,  # Cp for MCTS
-                       'leaf_size': 10,  # tree leaf size
-                       'kernel_type': "rbf",  # used in SVM
-                       'gamma_type': "auto",  # used in SVM
-                       'solver_type': 'bo',  # solver can be bo or turbo
-                       'verbose': 1,
-                       'saving_fitness': 1}
-            if f.__name__ == "rosenbrock":
-                problem['lower_boundary'] = -10 * np.ones((d,))
-            if d == 100:
-                options['max_function_evaluations'] = 1e4
-            solver = Solver(problem, options)
-            results = solver.optimize()
-            print(results)
-            write_pickle(f.__name__, d, results)
-            plot(f.__name__, d)
+if __name__ == '__main__':
+    start_run = time.time()
+    ndim_problem = 100
+    problem = {'fitness_function': Levy(ndim_problem),
+               'ndim_problem': ndim_problem,
+               'lower_boundary': -10*np.ones((ndim_problem,)),
+               'upper_boundary': 10*np.ones((ndim_problem,))}
+    options = {'seed_rng': 3,
+               'verbose': 1,
+               'saving_fitness': 10,
+               'max_function_evaluations': 15000}
+    solver = Solver(problem, options)
+    results = solver.optimize()
+    print(results)
+    print('*** Runtime: {:7.5e}'.format(time.time() - start_run))
