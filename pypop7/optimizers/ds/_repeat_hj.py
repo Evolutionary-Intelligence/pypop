031ea71ce7443ca5433cd2506c07686eddf01c21
Evolutionary-Intelligence <duanqq077@163.com>
2023-03-27 01:47:12 +0800

    Update pypop7/optimizers/ds/_repeat_hj.py

--- pypop7/optimizers/ds/_repeat_hj.py
+++ pypop7/optimizers/ds/_repeat_hj.py
@@ -11,7 +11,6 @@
 
     The Python reference script is given below (note that first install `pymoo` via `pip install pymoo`):
     -----------------------------------------------------------------------------------------------------
-
     from pymoo.algorithms.soo.nonconvex.pattern import PatternSearch
     from pymoo.problems.single import Ackley
     from pymoo.optimize import minimize
