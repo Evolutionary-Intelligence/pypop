d0a5e3396083d096549e85a005fb66775b252732
Evolutionary-Intelligence <duanqq077@163.com>
2023-03-27 11:49:43 +0800

    Update pypop7/optimizers/eda/__init__.py

--- pypop7/optimizers/eda/__init__.py
+++ pypop7/optimizers/eda/__init__.py
@@ -1,10 +1,12 @@
-from pypop7.optimizers.eda.eda import EDA
+from pypop7.optimizers.eda.eda import EDA  # abstract class for all EDA classes
 from pypop7.optimizers.eda.umda import UMDA
 from pypop7.optimizers.eda.emna import EMNA
 from pypop7.optimizers.eda.aemna import AEMNA
 from pypop7.optimizers.eda.rpeda import RPEDA
 
 
-__all__ = [EDA,  # base (abstract) class
-           EMNA, AEMNA,  # competitors for medium- and small-scale black-box optimization (often < 100)
-           UMDA, RPEDA]  # EDAs especially for large-scale black-box optimization (generally >> 100)
+__all__ = [EDA,  # Estimation of Distribution Algorithms (EDA)
+           EMNA,  # Estimation of Multivariate Normal Algorithm (EMNA)
+           AEMNA,  # Adaptive Estimation of Multivariate Normal Algorithm (AEMNA)
+           UMDA,  # Univariate Marginal Distribution Algorithm for normal models (UMDA)
+           RPEDA]  # Random-Projection Estimation of Distribution Algorithm (RPEDA)
