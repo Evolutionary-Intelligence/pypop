2f07975be351bb171486ffb705b6ed704e1bd689
Evolutionary-Intelligence <duanqq077@163.com>
2023-03-27 15:26:23 +0800

    Update pypop7/optimizers/de/__init__.py

--- pypop7/optimizers/de/__init__.py
+++ pypop7/optimizers/de/__init__.py
@@ -3,10 +3,12 @@ from pypop7.optimizers.de.cde import CDE
 from pypop7.optimizers.de.tde import TDE
 from pypop7.optimizers.de.jade import JADE
 from pypop7.optimizers.de.code import CODE
+from pypop7.optimizers.de.shade import SHADE
 
 
 __all__ = [DE,  # Differential Evolution (DE)
            CDE,  # Classic Differential Evolution (CDE)
            TDE,  # Trigonometric-mutation Differential Evolution (TDE)
            JADE,  # Adaptive Differential Evolution (JADE)
-           CODE]  # COmposite Differential Evolution (CODE)
+           CODE,  # COmposite Differential Evolution (CODE)
+           SHADE]  # Success-History based Adaptive Differential Evolution (SHADE)
