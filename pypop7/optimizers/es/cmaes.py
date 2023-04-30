a167e24cbc8b23eb622dd641a5dfc6c536339ea7
Evolutionary-Intelligence <78018333+Evolutionary-Intelligence@users.noreply.github.com>
2023-04-30 22:37:55 +0800

    Update cmaes.py

--- pypop7/optimizers/es/cmaes.py
+++ pypop7/optimizers/es/cmaes.py
@@ -119,7 +119,7 @@ class CMAES(ES):
         self._p_c_2 = None
         self.n_generations, self._n_generations = None, None
 
-    def _set_c_c(self):
+    def _set_c_c(self):  # decay rate for cumulation (evolution) path for rank-one update of covariance matrix
         return (4.0 + self._mu_eff/self.ndim_problem)/(self.ndim_problem + 4.0 + 2.0*self._mu_eff/self.ndim_problem)
 
     def _set_c_w(self):
