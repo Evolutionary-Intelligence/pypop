fb304e33d14dc0ea7d13dcdc7014ff6443c49509
Evolutionary-Intelligence <78018333+Evolutionary-Intelligence@users.noreply.github.com>
2023-04-30 22:45:03 +0800

    Update cmaes.py

--- pypop7/optimizers/es/cmaes.py
+++ pypop7/optimizers/es/cmaes.py
@@ -139,7 +139,7 @@ class CMAES(ES):
         self.c_s = self.options.get('c_s', (self._mu_eff + 2.0)/(self._mu_eff + self.ndim_problem + 5.0))
         self.d_sigma = self.options.get('d_sigma', self._set_d_sigma())
         self.c_c = self.options.get('c_c', self._set_c_c())
-        self.c_1 = self.options.get('c_1', self._alpha_cov/(np.power(self.ndim_problem + 1.3, 2) + self._mu_eff))
+        self.c_1 = self.options.get('c_1', self._alpha_cov/(np.square(self.ndim_problem + 1.3) + self._mu_eff))
         self.c_w = self.options.get('c_w', self._set_c_w())
         w_min = np.min([1.0 + self.c_1/self.c_w, 1.0 + 2.0*self._mu_eff_minus/(self._mu_eff + 2.0),
                         (1.0 - self.c_1 - self.c_w)/(self.ndim_problem*self.c_w)])
