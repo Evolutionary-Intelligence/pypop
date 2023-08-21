1961c5e190ff24602d8fb15d951640fef916d08a
Evolutionary-Intelligence <78018333+Evolutionary-Intelligence@users.noreply.github.com>
2023-08-21 21:20:53 +0800

    Update cde.py

--- pypop7/optimizers/de/cde.py
+++ pypop7/optimizers/de/cde.py
@@ -21,7 +21,7 @@ class CDE(DE):
               optimizer options with the following common settings (`keys`):
                 * 'max_function_evaluations' - maximum of function evaluations (`int`, default: `np.Inf`),
                 * 'max_runtime'              - maximal runtime to be allowed (`float`, default: `np.Inf`),
-                * 'seed_rng'                 - seed for random number generation needed to be *explicitly* set (`int`);
+                * 'seed_rng'                 - seed for random number generation (RNG) needed to be *explicitly* set (`int`);
               and with the following particular settings (`keys`):
                 * 'n_individuals' - number of offspring, aka offspring population size (`int`, default: `100`),
                 * 'f'             - mutation factor (`float`, default: `0.5`),
@@ -29,7 +29,7 @@ class CDE(DE):
 
     Examples
     --------
-    Use the optimizer to minimize the well-known test function
+    Use the optimizer `CDE` to minimize the well-known test function
     `Rosenbrock <http://en.wikipedia.org/wiki/Rosenbrock_function>`_:
 
     .. code-block:: python
