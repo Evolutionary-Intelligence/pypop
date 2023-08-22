8496f06ba693e04cfec9438bd175aea925e2499a
C Shao <shaoch09@gmail.com>
2023-08-22 19:05:19 +0800

    Update pypop7/optimizers/de/cde.py

--- pypop7/optimizers/de/_repeat_cde.py
+++ pypop7/optimizers/de/_repeat_cde.py
@@ -8,9 +8,10 @@
     Therefore, we argue that its repeatability could be **well-documented**.
 """
 import numpy as np
+import time
 
 from pypop7.benchmarks.base_functions import sphere, rosenbrock, griewank
-from pypop7.optimizers.de.cde import CDE
+from cde import CDE
 
 
 if __name__ == '__main__':
@@ -19,28 +20,30 @@ if __name__ == '__main__':
                'lower_boundary': -5.12*np.ones((3,)),
                'upper_boundary': 5.12*np.ones((3,))}
     options = {'fitness_threshold': 1e-6,
-               'seed_rng': 0,
+               'seed_rng': 2,
                'n_individuals': 5,
                'f': 0.9,
                'cr': 0.1}
+    start_time = time.time()
     cde = CDE(problem, options)
     results = cde.optimize()
-    print(results['n_function_evaluations'], results['best_so_far_y'])
-    # 343 vs 406 (from the original paper)
+    print(results['n_function_evaluations'], results['best_so_far_y'], time.time() - start_time)
+    # 364 vs 406 (from the original paper) 0.03568911552429199
 
     problem = {'fitness_function': rosenbrock,
                'ndim_problem': 2,
                'lower_boundary': -2.048*np.ones((2,)),
                'upper_boundary': 2.048*np.ones((2,))}
     options = {'fitness_threshold': 1e-6,
-               'seed_rng': 0,
+               'seed_rng': 1,
                'n_individuals': 10,
                'f': 0.9,
                'cr': 0.9}
+    start_time = time.time()
     cde = CDE(problem, options)
     results = cde.optimize()
-    print(results['n_function_evaluations'], results['best_so_far_y'])
-    # 579 vs 654 (from the original paper)
+    print(results['n_function_evaluations'], results['best_so_far_y'], time.time() - start_time)
+    # 528 vs 654 (from the original paper) 0.05886220932006836
 
     problem = {'fitness_function': griewank,
                'ndim_problem': 10,
@@ -51,7 +54,8 @@ if __name__ == '__main__':
                'n_individuals': 25,
                'f': 0.5,
                'cr': 0.2}
+    start_time = time.time()
     cde = CDE(problem, options)
     results = cde.optimize()
-    print(results['n_function_evaluations'], results['best_so_far_y'])
-    # 12265 vs 12752 (from the original paper)
+    print(results['n_function_evaluations'], results['best_so_far_y'], time.time() - start_time)
+    # 11554 vs 12752 (from the original paper) 1.1777536869049072
