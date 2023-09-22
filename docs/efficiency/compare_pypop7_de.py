9cdc263a4fc1282dd82c0aff3b5eed8abff5e0ee
Evolutionary-Intelligence <78018333+Evolutionary-Intelligence@users.noreply.github.com>
2023-09-22 19:38:18 +0800

    Add files via upload

---
+++ docs/efficiency/compare_pypop7_de.py
@@ -0,0 +1,25 @@
+# Please first install pypop7 (see https://pypop.rtfd.io/ for details):
+#    $ pip install pypop7
+import pickle
+
+import numpy
+
+from pypop7.benchmarks.base_functions import sphere  # function to be minimized
+from pypop7.optimizers.de.cde import CDE
+
+
+if __name__ == "__main__":
+    ndim_problem = 20
+    problem = {'fitness_function': sphere,  # define problem arguments
+               'ndim_problem': ndim_problem,
+               'lower_boundary': -10.0*numpy.ones((ndim_problem,)),
+               'upper_boundary': 10.0*numpy.ones((ndim_problem,))}
+    options = {'max_runtime': 60*3,  # set optimizer options
+               'verbose': False,
+               'seed_rng': 0}
+    cde = CDE(problem, options)  # initialize the optimizer class
+    results = cde.optimize()  # run the optimization process
+    # return the number of function evaluations and best-so-far fitness
+    print(f"CDE: {results['n_function_evaluations']}, {results['best_so_far_y']}")
+    with open('PYPOP7-DE.pickle', 'wb') as handle:
+        pickle.dump(results, handle, protocol=pickle.HIGHEST_PROTOCOL)
