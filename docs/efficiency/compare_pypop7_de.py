9e9140129cfb3709c2e8cd47d844c5e70e2f6f0f
Evolutionary-Intelligence <78018333+Evolutionary-Intelligence@users.noreply.github.com>
2023-09-22 19:39:23 +0800

    Update compare_pypop7_de.py

--- docs/efficiency/compare_pypop7_de.py
+++ docs/efficiency/compare_pypop7_de.py
@@ -9,12 +9,12 @@ from pypop7.optimizers.de.cde import CDE
 
 
 if __name__ == "__main__":
-    ndim_problem = 20
+    ndim_problem = 2000
     problem = {'fitness_function': sphere,  # define problem arguments
                'ndim_problem': ndim_problem,
                'lower_boundary': -10.0*numpy.ones((ndim_problem,)),
                'upper_boundary': 10.0*numpy.ones((ndim_problem,))}
-    options = {'max_runtime': 60*3,  # set optimizer options
+    options = {'max_runtime': 60*60*3,  # set optimizer options
                'verbose': False,
                'seed_rng': 0}
     cde = CDE(problem, options)  # initialize the optimizer class
