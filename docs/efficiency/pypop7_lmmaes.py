92d00c337199f79a832a8e6b020ca507612e28e2
Evolutionary-Intelligence <78018333+Evolutionary-Intelligence@users.noreply.github.com>
2023-09-27 06:23:31 +0800

    Add files via upload

---
+++ docs/efficiency/pypop7_lmmaes.py
@@ -0,0 +1,32 @@
+# Please first install pypop7 (see https://pypop.rtfd.io/ for details):
+#    $ pip install pypop7
+import pickle
+import argparse
+
+import numpy
+
+from pypop7.benchmarks.base_functions import sphere  # function to be minimized
+from pypop7.optimizers.es.lmmaes import LMMAES  # Limited-Memory Matrix Adaptation Evolution Strategy
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--index', '-i', type=int)  # index of the experiment
+    args = parser.parse_args()
+    params = vars(args)
+    ndim_problem = 2000
+    problem = {'fitness_function': sphere,  # define problem arguments
+               'ndim_problem': ndim_problem,
+               'lower_boundary': -10.0*numpy.ones((ndim_problem,)),
+               'upper_boundary': 10.0*numpy.ones((ndim_problem,))}
+    options = {'max_runtime': 60*60*3,  # set optimizer options
+               'verbose': False,
+               'saving_fitness': 2000,
+               'sigma': 20.0/3.0, 
+               'seed_rng': params['index']}
+    lmmaes = LMMAES(problem, options)  # initialize the optimizer class
+    results = lmmaes.optimize()  # run the optimization process
+    # return the number of function evaluations and best-so-far fitness
+    print(f"LMMAES: {results['n_function_evaluations']}, {results['best_so_far_y']}")
+    with open('PYPOP7-LMMAES.pickle', 'wb') as handle:
+        pickle.dump(results, handle, protocol=pickle.HIGHEST_PROTOCOL)
