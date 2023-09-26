4767128cc92ab5784e500eb5003243b164a863c7
Evolutionary-Intelligence <78018333+Evolutionary-Intelligence@users.noreply.github.com>
2023-09-27 03:02:35 +0800

    Update compare_pypop7_lmcma.py

--- docs/efficiency/compare_pypop7_lmcma.py
+++ docs/efficiency/compare_pypop7_lmcma.py
@@ -1,6 +1,7 @@
 # Please first install pypop7 (see https://pypop.rtfd.io/ for details):
 #    $ pip install pypop7
 import pickle
+import argparse
 
 import numpy
 
@@ -9,6 +10,10 @@ from pypop7.optimizers.es.lmcma import LMCMA  # Limited-Memory Covariance Matrix
 
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--index', '-i', type=int)  # index of the experiment
+    args = parser.parse_args()
+    params = vars(args)
     ndim_problem = 2000
     problem = {'fitness_function': sphere,  # define problem arguments
                'ndim_problem': ndim_problem,
@@ -18,7 +23,7 @@ if __name__ == "__main__":
                'verbose': False,
                'saving_fitness': 2000,
                'sigma': 20.0/3.0, 
-               'seed_rng': 0}
+               'seed_rng': params['index']}
     lmcma = LMCMA(problem, options)  # initialize the optimizer class
     results = lmcma.optimize()  # run the optimization process
     # return the number of function evaluations and best-so-far fitness
