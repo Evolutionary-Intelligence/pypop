4b181d8647ca77f72894efc77d5c5719a0b9244f
Evolutionary-Intelligence <78018333+Evolutionary-Intelligence@users.noreply.github.com>
2023-09-26 16:39:45 +0800

    Update compare_deap_psonp.py

--- docs/efficiency/compare_deap_psonp.py
+++ docs/efficiency/compare_deap_psonp.py
@@ -85,7 +85,7 @@ def main():
                'n_function_evaluations': n_fe,
                'runtime': time.time() - start_time,
                'fitness': fitness}
-    with open('DEAP-PSONP.pickle', 'wb') as handle:
+    with open('DEAP-PSO.pickle', 'wb') as handle:
         pickle.dump(results, handle, protocol=pickle.HIGHEST_PROTOCOL)
     print('*** runtime (seconds) ***:', time.time() - start_time)
 
