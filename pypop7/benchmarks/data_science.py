7c970306244b23621a43c1f9da00b5845928218b
Evolutionary-Intelligence <78018333+Evolutionary-Intelligence@users.noreply.github.com>
2023-09-29 20:54:42 +0800

    Update data_science.py

--- pypop7/benchmarks/data_science.py
+++ pypop7/benchmarks/data_science.py
@@ -3,14 +3,14 @@ import numpy as np
 from pypop7.benchmarks.base_functions import BaseFunction
 
 
-def log_loss_logistics_regression(x, y, w):
+def logistic_regression(w, x, y):
     loss = []
-    for xx, yy in zip(x, y):
-        p = 1/(1 + np.exp(np.dot(xx, w)))
-        loss.append(-yy*np.log(p) - (1 - yy)*np.log(1 - p))
+    for i in range(len(w)):
+        p = 1.0/(1.0 + np.exp(np.dot(x[i], w)))
+        loss.append(-y[i]*np.log(p) - (1 - y[i])*np.log(1 - p))
     return np.mean(loss)
 
 
-class LogLossLogisticsRegression(BaseFunction):
-    def __call__(self, x, y, w):
-        return log_loss_logistics_regression(x, y, w)
+class LogisticRegression(BaseFunction):
+    def __call__(self, w, x, y):
+        return logistic_regression(w, x, y)
