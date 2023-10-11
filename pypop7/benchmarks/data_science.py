803879b4c6e54fdb27810e2043ac493bbdccf696
Evolutionary-Intelligence <duanqq077@163.com>
2023-10-12 01:43:31 +0800

    Add pypop7/benchmarks/data_science.py

--- pypop7/benchmarks/data_science.py
+++ pypop7/benchmarks/data_science.py
@@ -4,18 +4,18 @@ from pypop7.benchmarks.base_functions import BaseFunction
 
 
 def cross_entropy_loss_lr(w, x, y):
-    """"Cross-Entropy Loss Function of Logistic Regression (with binary labels/classes {0, 1}).
+    """Cross-Entropy Loss Function of Logistic Regression (with binary labels/classes {0, 1}).
 
         Note that this loss function is convex.
 
         https://web.stanford.edu/~jurafsky/slp3/5.pdf
         https://developer.ibm.com/articles/implementing-logistic-regression-from-scratch-in-python/
         https://jermwatt.github.io/machine_learning_refined/notes/6_Linear_twoclass_classification/6_2_Cross_entropy.html
-    ""
+    """
     loss = np.empty(len(y))
     for i in range(len(y)):
         p = 1.0/(1.0 + np.exp(-(w[0] + np.dot(x[i], w[1:]))))
-        loss[i] = -y[i]*np.log(p) - (1.0 - y[i])*np.log(1.0 - p))
+        loss[i] = -y[i]*np.log(p) - (1.0 - y[i])*np.log(1.0 - p)
     return np.mean(loss)
 
 
@@ -25,12 +25,12 @@ class CrossEntropyLossLR(BaseFunction):
 
 
 def square_loss_lr(w, x, y):
-    """"Square Loss Function of Logistic Regression (with binary labels/classes {0, 1}).
+    """Square Loss Function of Logistic Regression (with binary labels/classes {0, 1}).
 
         Note that this loss function is non-convex.
 
         https://epubs.siam.org/doi/abs/10.1137/1.9781611976236.23
-    ""
+    """
     loss = np.empty(len(y))
     for i in range(len(y)):
         p = 1.0/(1.0 + np.exp(-(w[0] + np.dot(x[i], w[1:]))))
