00a652aef0a45261bfb2058138c174dac2c9e06e
Evolutionary-Intelligence <78018333+Evolutionary-Intelligence@users.noreply.github.com>
2023-10-12 01:27:52 +0800

    Update __init__.py

--- pypop7/benchmarks/__init__.py
+++ pypop7/benchmarks/__init__.py
@@ -3,3 +3,4 @@ import pypop7.benchmarks.base_functions
 import pypop7.benchmarks.shifted_functions
 import pypop7.benchmarks.rotated_functions
 import pypop7.benchmarks.continuous_functions
+import pypop7.benchmarks.data_science
