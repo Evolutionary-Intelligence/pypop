7a5b734cd5ee07d08d40e34bbd89273087ed9e65
Evolutionary-Intelligence <78018333+Evolutionary-Intelligence@users.noreply.github.com>
2023-10-12 00:42:32 +0800

    Update data_science.py

--- pypop7/benchmarks/data_science.py
+++ pypop7/benchmarks/data_science.py
@@ -6,8 +6,11 @@ from pypop7.benchmarks.base_functions import BaseFunction
 def logistic_regression(w, x, y):
     """"Cross-Entropy Loss Function of Logistic Regression (with binary labels {0, 1}).
 
+        Note that this loss function is convex.
+
         https://web.stanford.edu/~jurafsky/slp3/5.pdf
         https://developer.ibm.com/articles/implementing-logistic-regression-from-scratch-in-python/
+        https://jermwatt.github.io/machine_learning_refined/notes/6_Linear_twoclass_classification/6_2_Cross_entropy.html
     ""
     loss = np.empty(len(y))
     for i in range(len(y)):
