3ee0d063a72b0effdc3af109d757212ef8aca25e
Evolutionary-Intelligence <duanqq077@163.com>
2023-10-12 11:27:04 +0800

    Add docs/data-science/_test_data_science.py

--- docs/data-science/_test_data_science.py
+++ docs/data-science/_test_data_science.py
@@ -2,9 +2,18 @@ import pypop7.benchmarks.data_science as ds
 
 
 if __name__ == '__main__':
+    x, y = ds.read_cnae9()
+    assert x.shape == (1080, 856)
+    assert y.shape == (1080,)
+    assert sum(y == 0) == 960
+    assert sum(y == 1) == 120
+    assert sum(y == 0) + sum(y == 1) == len(y)
+    print(x.dtype, y.dtype)
+
     x, y = ds.read_qsar_androgen_receptor()
     assert x.shape == (1687, 1024)
     assert y.shape == (1687,)
     assert sum(y == -1) == 1488
     assert sum(y == 1) == 199
+    assert sum(y == -1) + sum(y == 1) == len(y)
     print(x.dtype, y.dtype)
