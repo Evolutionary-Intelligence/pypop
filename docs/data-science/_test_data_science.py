c70359ac02201ffede94098ac58c871faef6dbd6
Evolutionary-Intelligence <duanqq077@163.com>
2023-10-13 18:08:13 +0800

    Add docs/data-science/_test_data_science.py

--- docs/data-science/_test_data_science.py
+++ docs/data-science/_test_data_science.py
@@ -4,7 +4,43 @@ from sklearn.preprocessing import Normalizer
 import pypop7.benchmarks.data_science as ds
 
 
+def test_read_qsar_androgen_receptor():
+    x, y = ds.read_qsar_androgen_receptor()
+    assert x.shape == (1687, 1024)
+    assert y.shape == (1687,)
+    assert sum(y == -1) == 1488
+    assert sum(y == 1) == 199
+    assert sum(y == -1) + sum(y == 1) == len(y)
+    print(x.dtype, y.dtype)
+
+    transformer = Normalizer().fit(x)
+    print(x)
+    x = transformer.transform(x)
+    print(x)
+    w = 3.0*np.ones((x.shape[1] + 1,))
+    loss = ds.logistic_loss_lr(w, x, y)
+    print(loss)  # 26.06566955795496
+    loss = ds.logistic_loss_l2(w, x, y)
+    print(loss)  # 28.799813007866042
+
+    x, y = ds.read_qsar_androgen_receptor(is_10=True)
+    assert x.shape == (1687, 1024)
+    assert y.shape == (1687,)
+    assert sum(y == 0) == 1488
+    assert sum(y == 1) == 199
+    assert sum(y == 0) + sum(y == 1) == len(y)
+    print(x.dtype, y.dtype)
+
+    w = np.zeros((x.shape[1] + 1,))
+    loss = ds.cross_entropy_loss_lr(w, x, y)
+    print(loss)  # 0.6931471805599453
+    loss = ds.square_loss_lr(w, x, y)
+    print(loss)  # 0.25
+
+
 if __name__ == '__main__':
+    test_read_qsar_androgen_receptor()
+
     x, y = ds.read_parkinson_disease_classification()
     assert x.shape == (756, 753)
     assert y.shape == (756,)
@@ -64,21 +100,3 @@ if __name__ == '__main__':
     print(loss)  # 34.97093739858563
     loss = ds.logistic_loss_l2(w, x, y)
     print(loss)  # 36.09818739858564
-
-    x, y = ds.read_qsar_androgen_receptor()
-    assert x.shape == (1687, 1024)
-    assert y.shape == (1687,)
-    assert sum(y == -1) == 1488
-    assert sum(y == 1) == 199
-    assert sum(y == -1) + sum(y == 1) == len(y)
-    print(x.dtype, y.dtype)
-
-    transformer = Normalizer().fit(x)
-    print(x)
-    x = transformer.transform(x)
-    print(x)
-    w = 3.0*np.ones((x.shape[1] + 1,))
-    loss = ds.logistic_loss_lr(w, x, y)
-    print(loss)  # 26.06566955795496
-    loss = ds.logistic_loss_l2(w, x, y)
-    print(loss)  # 28.799813007866042
