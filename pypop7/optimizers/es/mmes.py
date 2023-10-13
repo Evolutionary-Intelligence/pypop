b92f57e84fc915d2be680d92a46f7408e779ee93
fmyzckj <43314658+fmyzckj@users.noreply.github.com>
2023-10-13 20:41:19 +0800

    Update mmes.py

--- pypop7/optimizers/es/mmes.py
+++ pypop7/optimizers/es/mmes.py
@@ -1,4 +1,4 @@
-import numpy as np
+import numpy as np  # engine for numerical computing
 from scipy.stats import norm
 
 from pypop7.optimizers.es.es import ES
@@ -49,7 +49,7 @@ class MMES(ES):
     .. code-block:: python
        :linenos:
 
-       >>> import numpy
+       >>> import numpy  # engine for numerical computing
        >>> from pypop7.benchmarks.base_functions import rosenbrock  # function to be minimized
        >>> from pypop7.optimizers.es.mmes import MMES
        >>> problem = {'fitness_function': rosenbrock,  # define problem arguments
