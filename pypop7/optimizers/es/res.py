bd93030941830982645a4075a56b59ca288c42a5
fmyzckj <43314658+fmyzckj@users.noreply.github.com>
2023-10-15 02:33:26 +0800

    Update res.py

--- pypop7/optimizers/es/res.py
+++ pypop7/optimizers/es/res.py
@@ -1,4 +1,4 @@
-import numpy as np
+import numpy as np  # engine for numerical computing
 
 from pypop7.optimizers.es.es import ES
 
@@ -49,7 +49,7 @@ class RES(ES):
     .. code-block:: python
        :linenos:
 
-       >>> import numpy
+       >>> import numpy  # engine for numerical computing
        >>> from pypop7.benchmarks.base_functions import rosenbrock  # function to be minimized
        >>> from pypop7.optimizers.es.res import RES
        >>> problem = {'fitness_function': rosenbrock,  # define problem arguments
