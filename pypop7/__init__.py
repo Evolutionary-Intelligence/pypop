1d2d85f9e7775cf484486c8f7f893df5a3e7b544
Evolutionary-Intelligence <78018333+Evolutionary-Intelligence@users.noreply.github.com>
2023-10-14 23:12:41 +0800

    Update __init__.py

--- pypop7/__init__.py
+++ pypop7/__init__.py
@@ -1,7 +1,7 @@
-"""PyPop7 is a Pure-PYthon library of POPulation-based OPtimization for single-objective, real-parameter, black-box
-    problems (currently still actively developed). Its main goal is to provide a unified interface and elegant
+"""PyPop7 is a Pure-PYthon library of POPulation-based (e.g., evolutionary/swarm) OPtimization for single-objective,
+    real-parameter, black-box problems. Its main goal is to provide a **unified** interface and **elegant**
     implementations for Black-Box Optimization (BBO), particularly population-based optimizers, in order to
-    facilitate research repeatability and also real-world applications.
+    facilitate research repeatability, BBO benchmarking, and also (possible) real-world applications.
 
     More specifically, for alleviating the notorious **curse of dimensionality** of BBO (based on iterative sampling),
     the primary focus of PyPop7 is to cover their State-Of-The-Art (SOTA) implementations for Large-Scale Optimization
