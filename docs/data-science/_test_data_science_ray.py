bf0535b1f2837d09e9f6df5e10c57d23dfe2ee8c
Evolutionary-Intelligence <duanqq077@163.com>
2023-10-14 19:26:15 +0800

    Add docs/data-science/_test_data_science_ray.py

--- docs/data-science/_test_data_science_ray.py
+++ docs/data-science/_test_data_science_ray.py
@@ -2,12 +2,24 @@ import ray  # for distributed computing
 import numpy as np
 from sklearn.preprocessing import Normalizer
 
-import data_science as ds
+import pypop7.benchmarks.data_science as ds
 
 
 @ray.remote
 def ray_problem(r_w, r_x, r_y):  # to be shared across all nodes
     return ds.tanh_loss_lr(r_w, r_x, r_y)
+
+
+@ray.remote
+class Optimizer:
+    def __init__(self, ndim_problem):
+        self.ndim_problem = ndim_problem
+
+    def optimize(self, fitness_function, r_w, r_x, r_y):
+        assert self.ndim_problem == len(r_w)
+        assert self.ndim_problem == r_x.shape[1] + 1
+        assert r_x.shape[0] == len(r_y)
+        return fitness_function(r_w, r_x, r_y)
 
 
 if __name__ == '__main__':
@@ -19,3 +31,5 @@ if __name__ == '__main__':
     ray_w = ray.put(w)
     ray_loss = ray_problem.remote(w, x, y)
     print(ray.get(ray_loss))  # 3.52815643380549
+    optimizer = Optimizer.remote(len(w))
+    print(ray.get(optimizer.optimize.remote(ds.tanh_loss_lr, w, x, y)))  # 3.52815643380549
