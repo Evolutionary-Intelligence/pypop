e8d6dce1cc658761dedd52a6f99970fddca34d21
Evolutionary-Intelligence <duanqq077@163.com>
2023-10-18 05:47:08 +0800

    Update docs/data-science/_test_data_science_ray.py

--- docs/data-science/_test_data_science_ray.py
+++ docs/data-science/_test_data_science_ray.py
@@ -29,6 +29,11 @@ def ray_cross_entropy_loss_lr(r_w, r_x, r_y):  # to be shared across all nodes
     return ds.cross_entropy_loss_lr(r_w, r_x, r_y)
 
 
+@ray.remote
+def ray_square_loss_lr(r_w, r_x, r_y):  # to be shared across all nodes
+    return ds.square_loss_lr(r_w, r_x, r_y)
+
+
 if __name__ == '__main__':
     x, y = ds.read_qsar_androgen_receptor()
     transformer = Normalizer().fit(x)
@@ -41,7 +46,7 @@ if __name__ == '__main__':
     optimizer = Optimizer.remote(len(w))
     print(ray.get(optimizer.optimize.remote(ds.tanh_loss_lr, w, x, y)))  # 3.52815643380549
 
-    n_trials = 10_000
+    n_trials = 100_000
     x, y = ds.read_parkinson_disease_classification()
     transformer = Normalizer().fit(x)
     x = transformer.transform(x)
@@ -52,7 +57,7 @@ if __name__ == '__main__':
     for i in range(n_trials):
         d.append(ds.cross_entropy_loss_lr(w, x, y))
     print('* Runtime: ', time.time() - start_time, ' - Sum: ', np.sum(d))
-    # * Runtime:  78.31092715263367  - Sum:  17379.784430717365
+    # *Runtime: 423.94107270240784 - Sum:  173797.8443071736
 
     start_time = time.time()
     s, d = 0.0, []
@@ -60,7 +65,7 @@ if __name__ == '__main__':
         d.append(ray_cross_entropy_loss_lr.remote(w, x, y))
     d = ray.get(d)
     print('* Runtime: ', time.time() - start_time, ' - Sum: ', np.sum(d))
-    # * Runtime:  16.65428137779236  - Sum:  17379.784430717365
+    # *Runtime: 184.14105248451233 - Sum:  173797.8443071736
 
     start_time = time.time()
     s, d = 0.0, []
@@ -69,4 +74,31 @@ if __name__ == '__main__':
         d.append(ray_cross_entropy_loss_lr.remote(w, x, y))
     d = ray.get(d)
     print('* Runtime: ', time.time() - start_time, ' - Sum: ', np.sum(d))
-    # * Runtime:  4.1619696617126465  - Sum:  17379.784430717365
+    # *Runtime: 30.191553831100464 - Sum:  173797.8443071736
+
+    x, y = ds.read_qsar_androgen_receptor(is_10=True)
+    w = 3.0*np.ones((x.shape[1] + 1,))
+
+    start_time = time.time()
+    s, d = 0.0, []
+    for i in range(n_trials):
+        d.append(ds.square_loss_lr(w, x, y))
+    print('* Runtime: ', time.time() - start_time, ' - Sum: ', np.sum(d))
+    # *Runtime: 721.3659870624542 - Sum:  88203.91227030235
+
+    start_time = time.time()
+    s, d = 0.0, []
+    for i in range(n_trials):
+        d.append(ray_square_loss_lr.remote(w, x, y))
+    d = ray.get(d)
+    print('* Runtime: ', time.time() - start_time, ' - Sum: ', np.sum(d))
+    # *Runtime: 1497.7815556526184 - Sum:  88203.91227030235
+
+    start_time = time.time()
+    s, d = 0.0, []
+    w, x, y = ray.put(w), ray.put(x), ray.put(y)  # to be shared
+    for i in range(n_trials):
+        d.append(ray_square_loss_lr.remote(w, x, y))
+    d = ray.get(d)
+    print('* Runtime: ', time.time() - start_time, ' - Sum: ', np.sum(d))
+    # *Runtime: 124.87141919136047 - Sum:  88203.91227030235
