c89c591d138151f7f675c039f99ead3d3d50b7ac
Evolutionary-Intelligence <duanqq077@163.com>
2023-10-17 22:33:25 +0800

    Update docs/data-science/_test_data_science_ray.py

--- docs/data-science/_test_data_science_ray.py
+++ docs/data-science/_test_data_science_ray.py
@@ -1,3 +1,5 @@
+import time
+
 import ray  # for distributed computing
 import numpy as np
 from sklearn.preprocessing import Normalizer
@@ -6,7 +8,7 @@ import pypop7.benchmarks.data_science as ds
 
 
 @ray.remote
-def ray_problem(r_w, r_x, r_y):  # to be shared across all nodes
+def ray_tanh_loss_lr(r_w, r_x, r_y):  # to be shared across all nodes
     return ds.tanh_loss_lr(r_w, r_x, r_y)
 
 
@@ -22,6 +24,11 @@ class Optimizer:
         return fitness_function(r_w, r_x, r_y)
 
 
+@ray.remote
+def ray_cross_entropy_loss_lr(r_w, r_x, r_y):  # to be shared across all nodes
+    return ds.cross_entropy_loss_lr(r_w, r_x, r_y)
+
+
 if __name__ == '__main__':
     x, y = ds.read_qsar_androgen_receptor()
     transformer = Normalizer().fit(x)
@@ -29,7 +36,37 @@ if __name__ == '__main__':
     ray_x, ray_y = ray.put(x), ray.put(y)
     w = 3.0*np.ones((x.shape[1] + 1,))
     ray_w = ray.put(w)
-    ray_loss = ray_problem.remote(w, x, y)
+    ray_loss = ray_tanh_loss_lr.remote(w, x, y)
     print(ray.get(ray_loss))  # 3.52815643380549
     optimizer = Optimizer.remote(len(w))
     print(ray.get(optimizer.optimize.remote(ds.tanh_loss_lr, w, x, y)))  # 3.52815643380549
+
+    n_trials = 10_000
+    x, y = ds.read_parkinson_disease_classification()
+    transformer = Normalizer().fit(x)
+    x = transformer.transform(x)
+    w = 3.0*np.ones((x.shape[1] + 1,))
+
+    start_time = time.time()
+    s, d = 0.0, []
+    for i in range(n_trials):
+        d.append(ds.cross_entropy_loss_lr(w, x, y))
+    print('* Runtime: ', time.time() - start_time, ' - Sum: ', np.sum(d))
+    # * Runtime:  78.31092715263367  - Sum:  17379.784430717365
+
+    start_time = time.time()
+    s, d = 0.0, []
+    for i in range(n_trials):
+        d.append(ray_cross_entropy_loss_lr.remote(w, x, y))
+    d = ray.get(d)
+    print('* Runtime: ', time.time() - start_time, ' - Sum: ', np.sum(d))
+    # * Runtime:  16.65428137779236  - Sum:  17379.784430717365
+
+    start_time = time.time()
+    s, d = 0.0, []
+    w, x, y = ray.put(w), ray.put(x), ray.put(y)  # to be shared
+    for i in range(n_trials):
+        d.append(ray_cross_entropy_loss_lr.remote(w, x, y))
+    d = ray.get(d)
+    print('* Runtime: ', time.time() - start_time, ' - Sum: ', np.sum(d))
+    # * Runtime:  4.1619696617126465  - Sum:  17379.784430717365
