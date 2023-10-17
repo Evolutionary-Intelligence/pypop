41974ff83e774d0b1de1bdca034312bc08998001
fmyzckj <43314658+fmyzckj@users.noreply.github.com>
2023-10-17 20:08:15 +0800

    Update dsaes.py

--- pypop7/optimizers/es/dsaes.py
+++ pypop7/optimizers/es/dsaes.py
@@ -1,4 +1,4 @@
-import numpy as np
+import numpy as np  # engine for numerical computing
 
 from pypop7.optimizers.es.es import ES
 
@@ -41,7 +41,7 @@ class DSAES(ES):
     .. code-block:: python
        :linenos:
 
-       >>> import numpy
+       >>> import numpy  # engine for numerical computing
        >>> from pypop7.benchmarks.base_functions import rosenbrock  # function to be minimized
        >>> from pypop7.optimizers.es.dsaes import DSAES
        >>> problem = {'fitness_function': rosenbrock,  # define problem arguments
