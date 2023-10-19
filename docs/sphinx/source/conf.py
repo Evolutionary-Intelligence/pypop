dc6e829a69bf8d9fda6c7bdeabb899073cf7ceac
Evolutionary-Intelligence <duanqq077@163.com>
2023-10-19 13:30:44 +0800

    Update docs/sphinx/source/conf.py

--- docs/sphinx/source/conf.py
+++ docs/sphinx/source/conf.py
@@ -18,7 +18,7 @@ sys.path.insert(0, os.path.abspath('../../../'))
 # -- Project information -----------------------------------------------------
 
 project = 'pypop7'
-copyright = '2022, Evolutionary-Intelligence'
+copyright = '2023, Evolutionary-Intelligence'
 author = 'Evolutionary-Intelligence'
 
 # The full version, including alpha/beta/rc tags
@@ -58,7 +58,7 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'nature'
+html_theme = 'sphinx_rtd_theme'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
