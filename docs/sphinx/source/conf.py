f4771fc7bbcb2afa29845313d98839b64a538471
Evolutionary-Intelligence <78018333+Evolutionary-Intelligence@users.noreply.github.com>
2023-10-27 00:58:26 +0800

    Update conf.py

--- docs/sphinx/source/conf.py
+++ docs/sphinx/source/conf.py
@@ -32,7 +32,8 @@ release = '0.0.1'
 # ones.
 extensions = ['sphinx.ext.autodoc',
               'sphinx.ext.napoleon',
-              'sphinx.ext.viewcode']
+              'sphinx.ext.viewcode',
+              'sphinx_copybutton']
 
 # Napoleon settings
 napoleon_google_docstring = False
