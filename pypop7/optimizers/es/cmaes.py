fef11cf67923ee479d498d6a5b0db4ccd51e470d
Evolutionary-Intelligence <78018333+Evolutionary-Intelligence@users.noreply.github.com>
2023-11-14 02:05:49 +0800

    Update cmaes.py

--- pypop7/optimizers/es/cmaes.py
+++ pypop7/optimizers/es/cmaes.py
@@ -149,7 +149,7 @@ class CMAES(ES):
         self._p_s_2 = np.sqrt(self._mu_eff*self.c_s*(2.0 - self.c_s))
         self._p_c_1 = 1.0 - self.c_c
         self._p_c_2 = np.sqrt(self._mu_eff*self.c_c*(2.0 - self.c_c))
-        x = np.empty((self.n_individuals, self.ndim_problem))  # offspring
+        x = np.empty((self.n_individuals, self.ndim_problem))  # a population of new search points (individuals, offspring)
         mean = self._initialize_mean(is_restart)  # mean of Gaussian search distribution
         p_s = np.zeros((self.ndim_problem,))  # evolution path for cumulative step-length adaptation (CSA)
         p_c = np.zeros((self.ndim_problem,))  # evolution path for covariance matrix adaptation (CMA)
