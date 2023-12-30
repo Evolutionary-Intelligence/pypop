4070721e5f1123ca8467e9c37691685be023d7bb
Evolutionary-Intelligence <78018333+Evolutionary-Intelligence@users.noreply.github.com>
2023-12-30 22:07:41 +0800

    Update conf.py

--- docs/sphinx/source/conf.py
+++ docs/sphinx/source/conf.py
@@ -18,7 +18,7 @@ sys.path.insert(0, os.path.abspath('../../../'))
 # -- Project information -----------------------------------------------------
 
 project = 'pypop7'
-copyright = '2023, Evolutionary-Intelligence'
+copyright = '2024, Evolutionary-Intelligence'
 author = 'Evolutionary-Intelligence'
 
 # The full version, including alpha/beta/rc tags
