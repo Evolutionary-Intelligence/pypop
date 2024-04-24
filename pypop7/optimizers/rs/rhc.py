17f21b3074357d30bfcf388a3244ec0bca98a4e4
Evolutionary-Intelligence <78018333+Evolutionary-Intelligence@users.noreply.github.com>
2024-04-24 20:36:06 +0800

    Update rhc.py

--- pypop7/optimizers/rs/rhc.py
+++ pypop7/optimizers/rs/rhc.py
@@ -103,12 +103,12 @@ class RHC(PRS):
     Baluja, S., 1996.
     `Genetic algorithms and explicit search statistics.
     <https://proceedings.neurips.cc/paper/1996/hash/e6d8545daa42d5ced125a4bf747b3688-Abstract.html>`_
-    In Advances in Neural Information Processing Systems, pp.319-325.
+    In Advances in Neural Information Processing Systems (pp.319-325).
 
     Juels, A. and Wattenberg, M., 1995.
     `Stochastic hillclimbing as a baseline method for evaluating genetic algorithms.
     <https://proceedings.neurips.cc/paper/1995/hash/36a1694bce9815b7e38a9dad05ad42e0-Abstract.html>`_
-    In Advances in Neural Information Processing Systems (pp. 430-436). MIT Press.
+    In Advances in Neural Information Processing Systems (pp. 430-436).
     """
     def __init__(self, problem, options):
         # only support normally-distributed random sampling during optimization
