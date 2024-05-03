f9c1ff061edc5bbc5da084ebe3c977be026803a3
Evolutionary-Intelligence <78018333+Evolutionary-Intelligence@users.noreply.github.com>
2024-05-03 21:50:12 +0800

    Update res.py

--- pypop7/optimizers/es/res.py
+++ pypop7/optimizers/es/res.py
@@ -60,10 +60,10 @@ class RES(ES):
        >>> options = {'max_function_evaluations': 5000,  # to set optimizer options
        ...            'seed_rng': 2022,
        ...            'mean': 3.0*numpy.ones((2,)),
-       ...            'sigma': 3.0}  # global step-size may need to be tuned
+       ...            'sigma': 3.0}  # global step-size may need to be tuned for optimality
        >>> res = RES(problem, options)  # to initialize the black-box optimizer class
        >>> results = res.optimize()  # to run its optimization/evolution process
-       >>> # to return the number of function evaluations and the best-so-far fitness
+       >>> # to return the used number of function evaluations and the found best-so-far fitness
        >>> print(f"RES: {results['n_function_evaluations']}, {results['best_so_far_y']}")
        RES: 5000, 0.00011689296624022443
 
