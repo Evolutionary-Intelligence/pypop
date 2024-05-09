87215b1d44c951fbdd7da447bb92c6cdf9d27907
Evolutionary-Intelligence <duanqq077@163.com>
2024-05-09 22:33:57 +0800

    Update pypop7/optimizers/es/test_samaes.py

--- pypop7/optimizers/es/test_samaes.py
+++ pypop7/optimizers/es/test_samaes.py
@@ -9,8 +9,8 @@ def test_optimize():
     options = {'max_function_evaluations': 5000,  # to set optimizer options
                'seed_rng': 2022,
                'mean': 3.0 * numpy.ones((2,)),
-               'sigma': 3.0}  # global step-size may need to be tuned
-    samaes = SAMAES(problem, options)  # to initialize the optimizer class
+               'sigma': 3.0}  # global step-size may need to be tuned for optimality
+    samaes = SAMAES(problem, options)  # to initialize the black-box optimizer class
     results = samaes.optimize()  # to run the optimization/evolution process
     assert results['n_function_evaluations'] == 5000
     assert results['best_so_far_y'] < 1.0
