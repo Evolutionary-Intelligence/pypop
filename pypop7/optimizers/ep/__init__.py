b8114ebb34125972dd3d393d040d69c5cfb5fffb
Evolutionary-Intelligence <duanqq077@163.com>
2024-05-12 03:56:45 +0800

    Update pypop7/optimizers/ep/__init__.py

--- pypop7/optimizers/ep/__init__.py
+++ pypop7/optimizers/ep/__init__.py
@@ -1,10 +1,10 @@
 from pypop7.optimizers.ep.ep import EP  # abstract class for all evolutionary programming (EP) classes [1962-1966]
-from pypop7.optimizers.ep.cep import CEP
+from pypop7.optimizers.ep.cep import CEP  # Classical Evolutionary Programming with self-adaptive mutation [1993]
 from pypop7.optimizers.ep.fep import FEP
 from pypop7.optimizers.ep.lep import LEP
 
 
 __all__ = [EP,  # Evolutionary Programming [1962-1966]
-           CEP,  # Classical Evolutionary Programming with self-adaptive mutation (CEP)
+           CEP,  # Classical Evolutionary Programming with self-adaptive mutation [1993]
            FEP,  # Fast Evolutionary Programming with self-adaptive mutation (FEP)
-           LEP]  # Lévy distribution based Evolutionary Programming (LEP)
+           LEP]  # Lévy-distribution based Evolutionary Programming (LEP)
