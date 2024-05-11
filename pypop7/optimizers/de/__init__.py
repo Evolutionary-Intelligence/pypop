69f4b755bf316447d2f5e1ecf8cbad6d56c4cf21
Evolutionary-Intelligence <duanqq077@163.com>
2024-05-12 02:50:10 +0800

    Update pypop7/optimizers/de/__init__.py

--- pypop7/optimizers/de/__init__.py
+++ pypop7/optimizers/de/__init__.py
@@ -1,13 +1,13 @@
-from pypop7.optimizers.de.de import DE  # abstract class for all DE classes
-from pypop7.optimizers.de.cde import CDE
+from pypop7.optimizers.de.de import DE  # abstract class for all differential evolution (DE) classes
+from pypop7.optimizers.de.cde import CDE  # classic differential evolution [1997]
 from pypop7.optimizers.de.tde import TDE
 from pypop7.optimizers.de.jade import JADE
 from pypop7.optimizers.de.code import CODE
 from pypop7.optimizers.de.shade import SHADE
 
 
-__all__ = [DE,  # Differential Evolution (DE)
-           CDE,  # Classic Differential Evolution (CDE)
+__all__ = [DE,  # Differential Evolution [1997]
+           CDE,  # Classic Differential Evolution [1997]
            TDE,  # Trigonometric-mutation Differential Evolution (TDE)
            JADE,  # Adaptive Differential Evolution (JADE)
            CODE,  # COmposite Differential Evolution (CODE)
