550342870dc3187c1cd892e4e8dcfdf27278f4e6
Evolutionary-Intelligence <duanqq077@163.com>
2024-05-12 02:01:06 +0800

    Update pypop7/optimizers/de/test_cde.py

---
+++ pypop7/optimizers/de/test_cde.py
@@ -0,0 +1,14 @@
+def test_optimize():
+    import numpy  # engine for numerical computing
+    from pypop7.benchmarks.base_functions import rosenbrock  # function to be minimized
+    from pypop7.optimizers.de.cde import CDE
+    problem = {'fitness_function': rosenbrock,  # to define problem arguments
+               'ndim_problem': 2,
+               'lower_boundary': -5.0 * numpy.ones((2,)),
+               'upper_boundary': 5.0 * numpy.ones((2,))}
+    options = {'max_function_evaluations': 5000,  # to set optimizer options
+               'seed_rng': 2022}
+    cde = CDE(problem, options)  # to initialize the black-box optimizer class
+    results = cde.optimize()  # to run its optimization/evolution process
+    assert results['n_function_evaluations'] == 5000
+    assert results['best_so_far_y'] < 1.0
