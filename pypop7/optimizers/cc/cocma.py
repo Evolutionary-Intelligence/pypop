dd9754f270f10fdc0d036dedc8c880f9161069a6
Evolutionary-Intelligence <duanqq077@163.com>
2024-05-12 11:26:29 +0800

    Update pypop7/optimizers/cc/cocma.py

--- pypop7/optimizers/cc/cocma.py
+++ pypop7/optimizers/cc/cocma.py
@@ -1,9 +1,9 @@
 import time
 
-import numpy as np
+import numpy as np  # engine for numerical computing
 
-from pypop7.optimizers.es.cmaes import CMAES
-from pypop7.optimizers.cc import CC
+from pypop7.optimizers.es.cmaes import CMAES  # covariance matrix adaptation evolution strategy
+from pypop7.optimizers.cc import CC  # abstract class of all cooperative coevolution (CC) classes
 
 
 class COCMA(CC):
