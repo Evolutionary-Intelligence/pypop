7f69e226ea018caa4a58106332406bfb1c8bd2db
Evolutionary-Intelligence <duanqq077@163.com>
2024-05-13 13:32:07 +0800

    Update pypop7/optimizers/pso/test_ccpso2.py

--- pypop7/optimizers/pso/test_ccpso2.py
+++ pypop7/optimizers/pso/test_ccpso2.py
@@ -7,9 +7,9 @@ def test_optimize():
                'ndim_problem': ndim_problem,
                'lower_boundary': -5.0 * numpy.ones((ndim_problem,)),
                'upper_boundary': 5.0 * numpy.ones((ndim_problem,))}
-    options = {'max_function_evaluations': 10000000,  # to set optimizer options
+    options = {'max_function_evaluations': 100000,  # to set optimizer options
                'seed_rng': 2022}  # global step-size may need to be tuned for optimality
     ccpso2 = CCPSO2(problem, options)  # to initialize the black-box optimizer class
     results = ccpso2.optimize()  # to run its optimization/evolution process
-    assert results['n_function_evaluations'] == 10000000
-    assert results['best_so_far_y'] < 10.0
+    assert results['n_function_evaluations'] == 100000
+    assert results['best_so_far_y'] < 50.0
