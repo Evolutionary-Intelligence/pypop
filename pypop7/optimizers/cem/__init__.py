24589decb34c643cf3ed67e78cd1fd19691be0cc
Evolutionary-Intelligence <duanqq077@163.com>
2024-05-14 11:41:35 +0800

    Update pypop7/optimizers/cem/__init__.py

--- pypop7/optimizers/cem/__init__.py
+++ pypop7/optimizers/cem/__init__.py
@@ -1,11 +1,11 @@
-from pypop7.optimizers.cem.cem import CEM  # abstract class for all CEM classes
+from pypop7.optimizers.cem.cem import CEM  # abstract class for all cross-entropy method (CEM) classes
 from pypop7.optimizers.cem.scem import SCEM
 from pypop7.optimizers.cem.dscem import DSCEM
 from pypop7.optimizers.cem.mras import MRAS
 # from pypop7.optimizers.cem.dcem import DCEM
 
 
-__all__ = [CEM,  # Cross-Entropy Method (CEM)
+__all__ = [CEM,  # Cross-Entropy Method
            SCEM,  # Standard Cross-Entropy Method (SCEM)
            DSCEM,  # Dynamic Smoothing Cross-Entropy Method (DSCEM)
            MRAS,  # Model Reference Adaptive Search (MRAS)
