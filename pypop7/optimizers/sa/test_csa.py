6bbecef3a534b351c24373448f963bffa0e1b4eb
Evolutionary-Intelligence <duanqq077@163.com>
2024-05-15 13:53:50 +0800

    Update pypop7/optimizers/sa/test_csa.py

--- pypop7/optimizers/sa/test_csa.py
+++ pypop7/optimizers/sa/test_csa.py
@@ -2,16 +2,16 @@ def test_optimize():
     import numpy  # engine for numerical computing
     from pypop7.benchmarks.base_functions import rosenbrock  # function to be minimized
     from pypop7.optimizers.sa.csa import CSA
-    problem = {'fitness_function': rosenbrock,  # define problem arguments
+    problem = {'fitness_function': rosenbrock,  # to define problem arguments
                'ndim_problem': 2,
                'lower_boundary': -5.0 * numpy.ones((2,)),
                'upper_boundary': 5.0 * numpy.ones((2,))}
-    options = {'max_function_evaluations': 5000,  # set optimizer options
+    options = {'max_function_evaluations': 5000,  # to set optimizer options
                'seed_rng': 2022,
-               'x': 3 * numpy.ones((2,)),
-               'sigma': 3.0,
-               'temperature': 100}
-    csa = CSA(problem, options)  # initialize the optimizer class
-    results = csa.optimize()  # run the optimization process
-    # return the number of function evaluations and best-so-far fitness
-    print(f"CSA: {results['n_function_evaluations']}, {results['best_so_far_y']}")
+               'temperature': 100.0,
+               'mean': 3.0 * numpy.ones((2,)),
+               'sigma': 3.0}  # global step-size may need to be tuned for optimality
+    csa = CSA(problem, options)  # to initialize the black-box optimizer class
+    results = csa.optimize()  # to run its optimization/evolution process
+    assert results['n_function_evaluations'] == 5000
+    assert results['best_so_far_y'] < 1.0
