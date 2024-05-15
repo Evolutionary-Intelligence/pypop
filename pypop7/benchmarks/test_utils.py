a3927c86c165dd19511b03d9a61ba5b0595901c1
Evolutionary-Intelligence <duanqq077@163.com>
2024-05-15 23:21:16 +0800

    Update pypop7/benchmarks/test_utils.py

--- pypop7/benchmarks/test_utils.py
+++ pypop7/benchmarks/test_utils.py
@@ -1,11 +1,11 @@
-from unittest import TestCase
+import unittest
 import numpy as np
 
 from pypop7.benchmarks import base_functions
 from pypop7.benchmarks.utils import generate_xyz, plot_contour
 
 
-class Test(TestCase):
+class TestUtils(unittest.TestCase):
     def test_generate_xyz(self):
         x, y, z = generate_xyz(base_functions.sphere, [0, 1], [0, 1], num=2)
         self.assertTrue(np.allclose(x, np.array([[0., 1.], [0., 1.]])))
@@ -15,3 +15,7 @@ class Test(TestCase):
     def test_plot_contour(self):
         plot_contour(base_functions.sphere, [-10, 10], [-10, 10])
         plot_contour(base_functions.ellipsoid, [-10, 10], [-10, 10])
+
+
+if __name__ == '__main__':
+    unittest.main()
