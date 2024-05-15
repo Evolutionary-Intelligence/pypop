13ce205a7f3cd46155797c3e5befb3a6e9d7457d
Evolutionary-Intelligence <duanqq077@163.com>
2024-05-15 13:50:42 +0800

    Update pypop7/optimizers/sa/test_esa.py

--- pypop7/optimizers/sa/test_esa.py
+++ pypop7/optimizers/sa/test_esa.py
@@ -2,13 +2,13 @@ def test_optimize():
     import numpy  # engine for numerical computing
     from pypop7.benchmarks.base_functions import rosenbrock  # function to be minimized
     from pypop7.optimizers.sa.esa import ESA
-    problem = {'fitness_function': rosenbrock,  # define problem arguments
+    problem = {'fitness_function': rosenbrock,  # to define problem arguments
                'ndim_problem': 2,
                'lower_boundary': -5.0 * numpy.ones((2,)),
                'upper_boundary': 5.0 * numpy.ones((2,))}
-    options = {'max_function_evaluations': 5000,  # set optimizer options
+    options = {'max_function_evaluations': 5000,  # to set optimizer options
                'seed_rng': 2022}
-    esa = ESA(problem, options)  # initialize the optimizer class
-    results = esa.optimize()  # run the optimization process
-    # return the number of function evaluations and best-so-far fitness
-    print(f"CSA: {results['n_function_evaluations']}, {results['best_so_far_y']}")
+    esa = ESA(problem, options)  # to initialize the black-box optimizer class
+    results = esa.optimize()  # to run its optimization/evolution process
+    assert results['n_function_evaluations'] == 5000
+    assert results['best_so_far_y'] < 10.0
