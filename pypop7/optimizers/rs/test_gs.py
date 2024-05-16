622a78066e9a0999bfb20ae27becdc4ffe9af6bb
Evolutionary-Intelligence <duanqq077@163.com>
2024-05-16 20:37:53 +0800

    Update pypop7/optimizers/rs/test_gs.py

--- pypop7/optimizers/rs/test_gs.py
+++ pypop7/optimizers/rs/test_gs.py
@@ -2,17 +2,15 @@ def test_optimize():
     import numpy  # engine for numerical computing
     from pypop7.benchmarks.base_functions import rosenbrock  # function to be minimized
     from pypop7.optimizers.rs.gs import GS
-    problem = {'fitness_function': rosenbrock,  # define problem arguments
+    problem = {'fitness_function': rosenbrock,  # to define problem arguments
                'ndim_problem': 2,
                'lower_boundary': -5.0 * numpy.ones((2,)),
                'upper_boundary': 5.0 * numpy.ones((2,))}
-    options = {'max_function_evaluations': 5000,  # set optimizer options
+    options = {'max_function_evaluations': 5000,  # to set optimizer options
                'seed_rng': 2022,
-               'x': 3 * numpy.ones((2,)),
-               'n_individuals': 10,
-               'c': 0.1,
-               'lr': 0.000001}
-    gs = GS(problem, options)  # initialize the optimizer class
-    results = gs.optimize()  # run the optimization process
+               'temperature': 100.0,
+               'mean': 3.0 * numpy.ones((2,))}
+    gs = GS(problem, options)  # to initialize the black-box optimizer class
+    results = gs.optimize()  # to run its optimization/evolution process
     assert results['n_function_evaluations'] == 5000
     assert results['best_so_far_y'] < 1.0
