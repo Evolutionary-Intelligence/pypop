8ec15661c2ab35f913082098bfc58716bdc29be4
Evolutionary-Intelligence <duanqq077@163.com>
2024-05-16 21:11:39 +0800

    Update pypop7/optimizers/rs/test_srs.py

--- pypop7/optimizers/rs/test_srs.py
+++ pypop7/optimizers/rs/test_srs.py
@@ -2,15 +2,15 @@ def test_optimize():
     import numpy  # engine for numerical computing
     from pypop7.benchmarks.base_functions import rosenbrock  # function to be minimized
     from pypop7.optimizers.rs.srs import SRS
-    problem = {'fitness_function': rosenbrock,  # define problem arguments
+    problem = {'fitness_function': rosenbrock,  # to define problem arguments
                'ndim_problem': 2,
                'lower_boundary': -5.0 * numpy.ones((2,)),
                'upper_boundary': 5.0 * numpy.ones((2,))}
-    options = {'max_function_evaluations': 5000,  # set optimizer options
+    options = {'max_function_evaluations': 5000,  # to set optimizer options
                'seed_rng': 2022,
                'x': 3.0 * numpy.ones((2,)),
                'sigma': 3.0}
-    srs = SRS(problem, options)  # initialize the optimizer class
-    results = srs.optimize()  # run the optimization process
+    srs = SRS(problem, options)  # to initialize the black-box optimizer class
+    results = srs.optimize()  # to run its optimization/evolution process
     assert results['n_function_evaluations'] == 5000
     assert results['best_so_far_y'] < 1.0
