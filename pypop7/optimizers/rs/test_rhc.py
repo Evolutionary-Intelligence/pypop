979a0a9866f437a1b1092c43017d7875444abbcf
Evolutionary-Intelligence <duanqq077@163.com>
2024-05-16 20:43:17 +0800

    Update pypop7/optimizers/rs/test_rhc.py

--- pypop7/optimizers/rs/test_rhc.py
+++ pypop7/optimizers/rs/test_rhc.py
@@ -2,15 +2,16 @@ def test_optimize():
     import numpy  # engine for numerical computing
     from pypop7.benchmarks.base_functions import rosenbrock  # function to be minimized
     from pypop7.optimizers.rs.rhc import RHC
-    problem = {'fitness_function': rosenbrock,  # define problem arguments
+    problem = {'fitness_function': rosenbrock,  # to define problem arguments
                'ndim_problem': 2,
                'lower_boundary': -5.0 * numpy.ones((2,)),
                'upper_boundary': 5.0 * numpy.ones((2,))}
-    options = {'max_function_evaluations': 5000,  # set optimizer options
+    options = {'max_function_evaluations': 5000,  # to set optimizer options
                'seed_rng': 2022,
-               'x': 3*numpy.ones((2,)),
-               'sigma': 3.0}
-    rhc = RHC(problem, options)  # initialize the optimizer class
-    results = rhc.optimize()  # run the optimization process
+               'temperature': 100.0,
+               'mean': 3.0 * numpy.ones((2,)),
+               'sigma': 3.0}  # global step-size may need to be tuned for optimality
+    rhc = RHC(problem, options)  # to initialize the black-box optimizer class
+    results = rhc.optimize()  # to run its optimization/evolution process
     assert results['n_function_evaluations'] == 5000
     assert results['best_so_far_y'] < 1.0
