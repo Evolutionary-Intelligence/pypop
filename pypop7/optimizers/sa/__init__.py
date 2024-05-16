c3554245fb08130ac2f1a16f7f237b7cccbe31dd
Evolutionary-Intelligence <duanqq077@163.com>
2024-05-16 21:32:10 +0800

    Update pypop7/optimizers/sa/__init__.py

--- pypop7/optimizers/sa/__init__.py
+++ pypop7/optimizers/sa/__init__.py
@@ -1,7 +1,7 @@
 from pypop7.optimizers.sa.sa import SA  # abstract class for all simulated annealing (SA) subclasses [1983]
-from pypop7.optimizers.sa.csa import CSA
-from pypop7.optimizers.sa.esa import ESA
-from pypop7.optimizers.sa.nsa import NSA
+from pypop7.optimizers.sa.csa import CSA  # Corana et al.' Simulated Annealing
+from pypop7.optimizers.sa.esa import ESA  # Enhanced Simulated Annealing
+from pypop7.optimizers.sa.nsa import NSA  # Noisy Simulated Annealing
 
 __all__ = [SA,  # Simulated Annealing [1983]
            CSA,  # Corana et al.' Simulated Annealing, especially for large-scale black-box optimization
