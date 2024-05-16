3939fedd91ee5b01d580b7ef1b8043768605f97a
Evolutionary-Intelligence <duanqq077@163.com>
2024-05-16 20:40:13 +0800

    Update pypop7/optimizers/rs/test_prs.py

--- pypop7/optimizers/rs/test_prs.py
+++ pypop7/optimizers/rs/test_prs.py
@@ -2,13 +2,15 @@ def test_optimize():
     import numpy  # engine for numerical computing
     from pypop7.benchmarks.base_functions import rosenbrock  # function to be minimized
     from pypop7.optimizers.rs.prs import PRS
-    problem = {'fitness_function': rosenbrock,  # define problem arguments
+    problem = {'fitness_function': rosenbrock,  # to define problem arguments
                'ndim_problem': 2,
                'lower_boundary': -5.0 * numpy.ones((2,)),
                'upper_boundary': 5.0 * numpy.ones((2,))}
-    options = {'max_function_evaluations': 5000,  # set optimizer options
-               'seed_rng': 2022}
-    prs = PRS(problem, options)  # initialize the optimizer class
-    results = prs.optimize()  # run the optimization process
+    options = {'max_function_evaluations': 5000,  # to set optimizer options
+               'seed_rng': 2022,
+               'temperature': 100.0,
+               'mean': 3.0 * numpy.ones((2,))}
+    prs = PRS(problem, options)  # to initialize the black-box optimizer class
+    results = prs.optimize()  # to run its optimization/evolution process
     assert results['n_function_evaluations'] == 5000
     assert results['best_so_far_y'] < 1.0
