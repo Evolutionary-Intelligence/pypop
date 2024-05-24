6b551426a695d2624a2f89d76792580871e7000e
Evolutionary-Intelligence <duanqq077@163.com>
2024-05-24 22:56:34 +0800

    Update pypop7/optimizers/sa/test_nsa.py

--- pypop7/optimizers/sa/test_nsa.py
+++ pypop7/optimizers/sa/test_nsa.py
@@ -1,3 +1,24 @@
+import numpy as np
+
+
+def test_initialize():
+    import numpy  # engine for numerical computing
+    from pypop7.benchmarks.base_functions import rosenbrock  # function to be minimized
+    from pypop7.optimizers.sa.nsa import NSA
+    problem = {'fitness_function': rosenbrock,  # to define problem arguments
+               'ndim_problem': 2,
+               'lower_boundary': -5.0 * numpy.ones((2,)),
+               'upper_boundary': 5.0 * numpy.ones((2,))}
+    options = {'max_function_evaluations': 5000,  # to set optimizer options
+               'seed_rng': 2022,
+               'temperature': 100.0,
+               'x': 3.0 * numpy.ones((2,)),
+               'sigma': 3.0}  # global step-size may need to be tuned for optimality
+    nsa = NSA(problem, options)  # to initialize the black-box optimizer class
+    assert nsa.temperature == 100.0
+    assert np.all(nsa.x == 3.0 * numpy.ones((2,)))
+
+
 def test_optimize():
     import numpy  # engine for numerical computing
     from pypop7.benchmarks.base_functions import rosenbrock  # function to be minimized
