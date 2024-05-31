24a9ce4f0439de22db59a1a6e816553bd3c959d3
Evolutionary-Intelligence <78018333+Evolutionary-Intelligence@users.noreply.github.com>
2024-05-31 22:27:04 +0800

    Add files via upload

---
+++ docs/pypop7_benchmarks_lso/generate_rotation_matrices_for_unimodal_functions.py
@@ -0,0 +1,31 @@
+import time
+import numpy as np
+
+import pypoplib.rotated_functions as cf
+from pypoplib.rotated_functions import generate_rotation_matrix
+
+
+def generate_rotation_matrices(functions=None, ndims=None, seed=None):
+    if functions is None:
+        functions = [cf.sphere, cf.cigar, cf.discus, cf.cigar_discus, cf.ellipsoid,
+                     cf.different_powers, cf.schwefel221, cf.step, cf.rosenbrock, cf.schwefel12]
+    if ndims is None:
+        ndims = [2, 10, 100, 200, 1000, 2000]
+    if seed is None:
+        seed = 20220502
+
+    rng = np.random.default_rng(seed)
+    seeds = rng.integers(np.iinfo(np.int64).max, size=(len(functions), len(ndims)))
+
+    start_run = time.time()
+    for i, f in enumerate(functions):
+        for j, d in enumerate(ndims):
+            start_time = time.time()
+            generate_rotation_matrix(f, d, seeds[i, j])
+            print('* {:d}-d {:s}: runtime {:7.5e}'.format(
+                d, f.__name__, time.time() - start_time))
+    print('*** Total runtime: {:7.5e}.'.format(time.time() - start_run))
+
+
+if __name__ == '__main__':
+    generate_rotation_matrices()
