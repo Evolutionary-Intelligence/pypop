ba4ade8ded2dc1581a42d3a73d9049832c5e9c5d
Evolutionary-Intelligence <78018333+Evolutionary-Intelligence@users.noreply.github.com>
2024-06-05 16:48:40 +0800

    Update pso.py

--- pypop7/optimizers/pso/pso.py
+++ pypop7/optimizers/pso/pso.py
@@ -31,7 +31,8 @@ class PSO(Optimizer):
     <https://doi.org/10.1038/s41467-023-38185-2>`_, `[Guo et al., 2023, ISSTA]
     <https://dl.acm.org/doi/10.1145/3597926.3598058>`_, `[Yang et al., 2023, IEEE-TSP]
     <https://ieeexplore.ieee.org/abstract/document/10048578>`_, `[Weiss et al., 2023, CGF]
-    <https://onlinelibrary.wiley.com/doi/10.1111/cgf.14904>`_, `[Liu et al., 2022, Nature Communications]
+    <https://onlinelibrary.wiley.com/doi/10.1111/cgf.14904>`_, `[Zhang et al., 2023]
+    <https://arxiv.org/abs/2312.08885>`_, `[Liu et al., 2022, Nature Communications]
     <https://doi.org/10.1038/s41467-022-28083-4>`_, `[Benedetti et al., 2019]
     <https://www.nature.com/articles/s41534-019-0157-8>`_, `[Venter&Sobieszczanski-Sobieski, 2003, AIAAJ]
     <https://doi.org/10.2514/2.2111>`_, to name a few.
