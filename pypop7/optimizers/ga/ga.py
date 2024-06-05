a979bec45983d0ee592af539ce981a23e7d69faa
Evolutionary-Intelligence <78018333+Evolutionary-Intelligence@users.noreply.github.com>
2024-06-05 22:24:58 +0800

    Update ga.py

--- pypop7/optimizers/ga/ga.py
+++ pypop7/optimizers/ga/ga.py
@@ -9,9 +9,10 @@ class GA(Optimizer):
     This is the **abstract** class for all `GA` classes. Please use any of its instantiated subclasses to
     optimize the **black-box** problem at hand.
 
-    .. note:: `GA` is one of three earliest versions of evolutionary algorithms along with *evolutionary programming*
-       and *evolution strategies*. Its original history can data back to Holland's JACM paperin 1962 called *outline
-       for a logical theory of adaptive systems*.
+    .. note:: `GA` is one of three *earliest* versions of evolutionary algorithms along with *evolutionary programming*
+       and *evolution strategies*. Its original history can data back to Holland's JACM paper in 1962 called *outline
+       for a logical theory of adaptive systems*. **John H. Holland**, the father of `GA`, was the recipient of `IEEE
+       Evolutionary Computation Pioneer Award 2003 <https://tinyurl.com/456as566>`_.
 
     For some interesting applications of `GA` on diverse areas, please refer to `[Lyu et al., 2024, Science]
     <https://www.science.org/doi/10.1126/science.adn6354>`_, `[Truong-Quoc et al., 2024, Nature Materials]
