cf67cad6a444095ca0ffb8cb2627abd0f558696a
Evolutionary-Intelligence <78018333+Evolutionary-Intelligence@users.noreply.github.com>
2024-06-10 16:23:53 +0800

    Update enes.py

--- pypop7/optimizers/nes/enes.py
+++ pypop7/optimizers/nes/enes.py
@@ -150,7 +150,7 @@ class ENES(ONES):
         grad[:j + 1] = np.dot(v[:j + 1, :], (u - np.dot(base, u)/np.sum(base)))
         grad /= self.n_individuals
         # update the mean of Gaussian search/sampling/mutation distribution
-        mean += self.lr_mean*grad[:self.ndim_problem]
+        mean += self.lr_mean * grad[:self.ndim_problem]
         # update the covariance matrix of Gaussian search/sampling/mutation distribution
         self._d_cv += self.lr_sigma * self._flat2triu(grad[self.ndim_problem:])
         cv = np.dot(self._d_cv.T, self._d_cv)  # to recover covariance matrix
