218aded89c4e6e108f8a3cfe9a024d761a5bcc82
fmyzckj <43314658+fmyzckj@users.noreply.github.com>
2024-06-14 23:33:25 +0800

    Update fcmaes.py

--- pypop7/optimizers/es/fcmaes.py
+++ pypop7/optimizers/es/fcmaes.py
@@ -73,15 +73,15 @@ class FCMAES(ES):
     References
     ----------
     Li, Z., Zhang, Q., Lin, X. and Zhen, H.L., 2020.
-    Fast covariance matrix adaptation for large-scale black-box optimization.
+    `Fast covariance matrix adaptation for large-scale black-box optimization.
+    <https://ieeexplore.ieee.org/abstract/document/8533604>`_
     IEEE Transactions on Cybernetics, 50(5), pp.2073-2083.
-    https://ieeexplore.ieee.org/abstract/document/8533604
 
     Li, Z. and Zhang, Q., 2016.
-    What does the evolution path learn in CMA-ES?.
+    `What does the evolution path learn in CMA-ES?.
+    <hhttps://link.springer.com/chapter/10.1007/978-3-319-45823-6_70>`_
     In Parallel Problem Solving from Nature (pp. 751-760).
     Springer International Publishing.
-    https://link.springer.com/chapter/10.1007/978-3-319-45823-6_70
     """
     def __init__(self, problem, options):
         ES.__init__(self, problem, options)
