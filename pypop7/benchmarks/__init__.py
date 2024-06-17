20d6ccded783d0b2fc9592417411e1a5295009fa
Evolutionary-Intelligence <duanqq077@163.com>
2024-06-17 21:55:19 +0800

    Update pypop7/benchmarks/__init__.py

--- pypop7/benchmarks/__init__.py
+++ pypop7/benchmarks/__init__.py
@@ -1,6 +1,6 @@
-import pypop7.benchmarks.cases
-import pypop7.benchmarks.utils
-import pypop7.benchmarks.base_functions
+import pypop7.benchmarks.cases  # test cases for all benchmarking functions (based on sampling)
+import pypop7.benchmarks.utils  # helper utilities for e.g., plotting
+import pypop7.benchmarks.base_functions  # base forms of benchmarking functions
 import pypop7.benchmarks.shifted_functions
 import pypop7.benchmarks.rotated_functions
 import pypop7.benchmarks.continuous_functions
