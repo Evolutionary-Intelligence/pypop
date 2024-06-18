7fa0330bad309d06da9de534a8248cff9a1f5514
Evolutionary-Intelligence <duanqq077@163.com>
2024-06-18 19:12:14 +0800

    Update pypop7/benchmarks/test_utils.py

--- pypop7/benchmarks/test_utils.py
+++ pypop7/benchmarks/test_utils.py
@@ -1,20 +1,40 @@
+"""Only for the testing purpose. Online documentation:
+"""
 import unittest
-import numpy as np
 
-from pypop7.benchmarks import base_functions
+import numpy as np  # engine for numerical computing
+
+from pypop7.benchmarks import base_functions as bf
+from pypop7.benchmarks import rotated_functions as rf
 from pypop7.benchmarks.utils import generate_xyz, plot_contour
+
+
+# test function for plotting
+def cd(x):  # from https://arxiv.org/pdf/1610.00040v1.pdf
+    return 7.0 * (x[0] ** 2) + 6.0 * x[0] * x[1] + 8.0 * (x[1] ** 2)
 
 
 class TestUtils(unittest.TestCase):
     def test_generate_xyz(self):
-        x, y, z = generate_xyz(base_functions.sphere, [0, 1], [0, 1], num=2)
-        self.assertTrue(np.allclose(x, np.array([[0., 1.], [0., 1.]])))
-        self.assertTrue(np.allclose(y, np.array([[0., 0.], [1., 1.]])))
-        self.assertTrue(np.allclose(z, np.array([[0., 1.], [1., 2.]])))
+        x, y, z = generate_xyz(bf.sphere, [0.0, 1.0], [0.0, 1.0], num=2)
+        self.assertTrue(np.allclose(x, np.array([[0.0, 1.0], [0.0, 1.0]])))
+        self.assertTrue(np.allclose(y, np.array([[0.0, 0.0], [1.0, 1.0]])))
+        self.assertTrue(np.allclose(z, np.array([[0.0, 1.0], [1.0, 2.0]])))
 
     def test_plot_contour(self):
-        plot_contour(base_functions.sphere, [-10, 10], [-10, 10])
-        plot_contour(base_functions.ellipsoid, [-10, 10], [-10, 10])
+        # plot smoothness and convexity
+        plot_contour(bf.sphere, [-10.0, 10.0], [-10.0, 10.0])
+        plot_contour(bf.ellipsoid, [-10.0, 10.0], [-10.0, 10.0])
+        # plot multi-modality
+        plot_contour(bf.rastrigin, [-10.0, 10.0], [-10.0, 10.0])
+        # plot non-convexity
+        plot_contour(bf.ackley, [-10.0, 10.0], [-10.0, 10.0])
+        # plot non-separability
+        plot_contour(cd, [-10.0, 10.0], [-10.0, 10.0])
+        # plot ill-condition and non-separability
+        rf.generate_rotation_matrix(rf.ellipsoid, 2, 72)
+        plot_contour(rf.ellipsoid, [-10.0, 10.0], [-10.0, 10.0],
+                     levels=[1e0, 1e3, 1e6, 5e6, 1e7, 5e7, 1e8, 5e8, 1e9])
 
 
 if __name__ == '__main__':
