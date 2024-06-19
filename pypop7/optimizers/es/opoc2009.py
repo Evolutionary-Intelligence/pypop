008b6b38736a6a622b380e84a105497c62112845
Evolutionary-Intelligence <78018333+Evolutionary-Intelligence@users.noreply.github.com>
2024-06-19 21:58:30 +0800

    Update opoc2009.py

--- pypop7/optimizers/es/opoc2009.py
+++ pypop7/optimizers/es/opoc2009.py
@@ -1,6 +1,6 @@
 import numpy as np  # engine for numerical computing
 
-from pypop7.optimizers.es.es import ES  # abstract class of all evolution strategies (ES)
+from pypop7.optimizers.es.es import ES  # abstract class of all Evolution Strategies (ES) classes
 from pypop7.optimizers.es.opoc2006 import OPOC2006  # (1+1)-Cholesky-CMA-ES 2006 (OPOC2006)
 
 
