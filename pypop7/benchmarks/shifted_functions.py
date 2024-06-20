b559bd77e67328505da85bf94e90aba258f22213
Evolutionary-Intelligence <78018333+Evolutionary-Intelligence@users.noreply.github.com>
2024-06-21 00:13:38 +0800

    Update shifted_functions.py

--- pypop7/benchmarks/shifted_functions.py
+++ pypop7/benchmarks/shifted_functions.py
@@ -83,6 +83,8 @@ def sphere(x, shift_vector=None):
     """**Sphere** test function.
 
        .. note:: It's LaTeX formulation is `$\sum_{i=1}^{n}x_i^2$`.
+          If its parameter `shift_vector` is `None`, please use function `generate_shift_vector()` to
+          generate it (stored in *txt* form) in advance.
 
     Parameters
     ----------
