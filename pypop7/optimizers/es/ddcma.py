9faae3eef90059e20207be59d0aadc5aea701d3a
Evolutionary-Intelligence <78018333+Evolutionary-Intelligence@users.noreply.github.com>
2024-06-20 15:36:11 +0800

    Update ddcma.py

--- pypop7/optimizers/es/ddcma.py
+++ pypop7/optimizers/es/ddcma.py
@@ -6,7 +6,7 @@ from pypop7.optimizers.es.es import ES  # abstract class of all Evolution Strate
 class DDCMA(ES):
     """Diagonal Decoding Covariance Matrix Adaptation (DDCMA).
 
-    .. note:: `DDCMA` is a lateset improvement version of the well-designed `CMA-ES` algorithm, which enjoys
+    .. note:: `DDCMA` is a latest improvement version of the well-designed `CMA-ES` algorithm, which enjoys
        both two worlds of `SEP-CMA-ES` (faster adaptation on nearly separable problems) and `CMA-ES` (more
        robust adaptation on ill-conditioned non-separable problems) via **adaptive diagonal decoding**. It
        is **highly recommended** to first attempt other ES variants (e.g., `LMCMA`, `LMMAES`) for large-scale
