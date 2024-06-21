b67bbb2961821738b18a2412f9a567d9c76dfb50
Evolutionary-Intelligence <78018333+Evolutionary-Intelligence@users.noreply.github.com>
2024-06-21 19:13:08 +0800

    Update cmaes.py

--- pypop7/optimizers/es/cmaes.py
+++ pypop7/optimizers/es/cmaes.py
@@ -70,7 +70,7 @@ class CMAES(ES):
        >>> options = {'max_function_evaluations': 5000,  # to set optimizer options
        ...            'seed_rng': 2022,
        ...            'mean': 3.0*numpy.ones((2,)),
-       ...            'sigma': 3.0}  # global step-size may need to be tuned
+       ...            'sigma': 3.0}  # global step-size may need to be fine-tuned for better performance
        >>> cmaes = CMAES(problem, options)  # to initialize the optimizer class
        >>> results = cmaes.optimize()  # to run the optimization/evolution process
        >>> print(f"CMAES: {results['n_function_evaluations']}, {results['best_so_far_y']}")
