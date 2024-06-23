fd102d836956fe122c1257814e4a97302af1a5d4
Evolutionary-Intelligence <78018333+Evolutionary-Intelligence@users.noreply.github.com>
2024-06-24 00:58:11 +0800

    Update lmcmaes.py

--- pypop7/optimizers/es/lmcmaes.py
+++ pypop7/optimizers/es/lmcmaes.py
@@ -62,7 +62,7 @@ class LMCMAES(ES):
        >>> options = {'max_function_evaluations': 5000,  # to set optimizer options
        ...            'seed_rng': 2022,
        ...            'mean': 3.0*numpy.ones((2,)),
-       ...            'sigma': 3.0}  # global step-size may need to be fine-tuned for better performance
+       ...            'sigma': 3.0}  # global step-size may need to be tuned for optimality
        >>> lmcmaes = LMCMAES(problem, options)  # to initialize the optimizer class
        >>> results = lmcmaes.optimize()  # to run the optimization/evolution process
        >>> print(f"LMCMAES: {results['n_function_evaluations']}, {results['best_so_far_y']}")
