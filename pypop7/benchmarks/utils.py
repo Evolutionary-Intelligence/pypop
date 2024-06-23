997bfdb9625b7dadffdae6376cfc3f020b857c94
ttt-noora <72697265+ttt-noora@users.noreply.github.com>
2024-06-23 19:45:55 +0800

    Update utils.py

--- pypop7/benchmarks/utils.py
+++ pypop7/benchmarks/utils.py
@@ -3,27 +3,21 @@ import seaborn as sns
 from matplotlib import cm
 import matplotlib.pyplot as plt
 
-"""
-Examples
---------
-
-.. code-block:: python
-   :linenos:
-   >>> import numpy as np
-   >>> from pypop7.benchmarks import base_functions as bf
-   >>> from pypop7.benchmarks.utils import generate_xyz
-   >>> x, y, z = generate_xyz(bf.sphere, [0.0, 1.0], [0.0, 1.0], num=2)
-   >>> self.assertTrue(np.allclose(x, np.array([[0.0, 1.0], [0.0, 1.0]])))
-   >>> self.assertTrue(np.allclose(y, np.array([[0.0, 0.0], [1.0, 1.0]])))
-   >>> self.assertTrue(np.allclose(z, np.array([[0.0, 1.0], [1.0, 2.0]])))
-"""
 sns.set_theme(style='darkgrid')
 
 
 # helper function for 2D-plotting
 def generate_xyz(func, x, y, num=200):
     """Generate necessary data before plotting a 2D contour of the fitness landscape.
-
+    Examples
+    --------
+    .. code-block:: python
+       :linenos:
+       >>> import numpy as np
+       >>> from pypop7.benchmarks import base_functions as bf
+       >>> from pypop7.benchmarks.utils import generate_xyz
+       >>> print(x.shape, y.shape, z.shape)
+   
     Parameters
     ----------
     func : func
@@ -57,7 +51,27 @@ def generate_xyz(func, x, y, num=200):
 # helper function for 2D-plotting
 def plot_contour(func, x, y, levels=None, num=200, is_save=False):
     """Plot a 2D contour of the fitness landscape.
+    
+    Examples
+    --------
 
+    .. code-block:: python
+       :linenos:
+       >>> import numpy as np
+       >>> from pypop7.benchmarks import rotated_functions as rf
+       >>> from pypop7.benchmarks.utils import plot_contour
+       >>> plot_contour(bf.sphere, [-10.0, 10.0], [-10.0, 10.0])
+       >>> plot_contour(bf.ellipsoid, [-10.0, 10.0], [-10.0, 10.0])
+       >>> # plot multi-modality
+       >>> plot_contour(bf.rastrigin, [-10.0, 10.0], [-10.0, 10.0])
+       >>> # plot non-convexity
+       >>> plot_contour(bf.ackley, [-10.0, 10.0], [-10.0, 10.0])
+       >>> # plot non-separability
+       >>> plot_contour(cd, [-10.0, 10.0], [-10.0, 10.0])
+       >>> # plot ill-condition and non-separability
+       >>> rf.generate_rotation_matrix(rf.ellipsoid, 2, 72)
+       >>> plot_contour(rf.ellipsoid, [-10.0, 10.0], [-10.0, 10.0], 7)
+    
     Parameters
     ----------
     func    : func
@@ -96,7 +110,29 @@ def plot_contour(func, x, y, levels=None, num=200, is_save=False):
 # helper function for 3D-plotting
 def plot_surface(func, x, y, num=200, is_save=False):
     """Plot a 3D surface of the fitness landscape.
+    
+    Examples
+    --------
 
+    .. code-block:: python
+       :linenos:
+       >>> import numpy as np
+       >>> from pypop7.benchmarks import base_functions as bf
+       >>> from pypop7.benchmarks import rotated_functions as rf
+       >>> from pypop7.benchmarks.utils import plot_surface
+       >>> # plot smoothness and convexity
+       >>> plot_surface(bf.sphere, [-10.0, 10.0], [-10.0, 10.0])
+       >>> plot_surface(bf.ellipsoid, [-10.0, 10.0], [-10.0, 10.0])
+       >>> # plot multi-modality
+       >>> plot_surface(bf.rastrigin, [-10.0, 10.0], [-10.0, 10.0])
+       >>> # plot non-convexity
+       >>> plot_surface(bf.ackley, [-10.0, 10.0], [-10.0, 10.0])
+       >>> # plot non-separability
+       >>> plot_surface(cd, [-10.0, 10.0], [-10.0, 10.0])
+       >>> # plot ill-condition and non-separability
+       >>> rf.generate_rotation_matrix(rf.ellipsoid, 2, 72)
+       >>> plot_surface(rf.ellipsoid, [-10.0, 10.0], [-10.0, 10.0], 7)
+      
     Parameters
     ----------
     func    : func
