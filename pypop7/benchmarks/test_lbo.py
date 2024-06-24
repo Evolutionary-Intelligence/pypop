c5e6e10b1ddedf254941c70a2b22366caa4dbcc0
Evolutionary-Intelligence <duanqq077@163.com>
2024-06-24 16:57:21 +0800

    Update pypop7/benchmarks/test_lbo.py

---
+++ pypop7/benchmarks/test_lbo.py
@@ -0,0 +1,10 @@
+import time
+
+from pypop7.benchmarks.lbo import benchmark_local_search
+from pypop7.optimizers.rs.prs import PRS as Optimizer
+
+
+def test_benchmark_local_search():
+    start_runtime = time.time()
+    benchmark_local_search(Optimizer, 2)
+    print('Total runtime: {:7.5e}.'.format(time.time() - start_runtime))
