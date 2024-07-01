76857e0f7bf867bbcdb1c5b589a51602d6698d0a
Chang-SHAO <101379630+Chang-SHAO@users.noreply.github.com>
2024-07-01 15:22:19 +0800

    Update base_functions.py

--- pypop7/benchmarks/base_functions.py
+++ pypop7/benchmarks/base_functions.py
@@ -707,7 +707,7 @@ class SkewRastrigin(BaseFunction):
 def levy_montalvo(x):
     """**Levy-Montalvo** test function.
 
-       .. note:: It's LaTeX formulation is `$(10 \sin^2(\pi (1 + 0.25(x_1 + 1))) + (0.25(x_n + 1))^2) \sum_{i = 1}^{n - 1} (0.25(x_i + 1))^2 (1 + 10 \sin^2 (\pi (1 + 0.25(x_{i + 1} + 1))))$`.
+       .. note:: It's LaTeX formulation is `$(10 \sin^2(\pi (1.25 + 0.25x_1)) + (0.25(x_n + 1))^2) \sum_{i = 1}^{n - 1} (0.25(x_i + 1))^2 (1 + 10 \sin^2 (\pi (1.25 + 0.25x_{i + 1})))$`.
 
     Parameters
     ----------
