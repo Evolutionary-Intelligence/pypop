0e8ec8169a20d7c51bf951b83bd4ec2fcedd4e85
Evolutionary-Intelligence <duanqq077@163.com>
2024-07-01 16:04:05 +0800

    Update pypop7/benchmarks/__init__.py

--- pypop7/benchmarks/__init__.py
+++ pypop7/benchmarks/__init__.py
@@ -1,15 +1,16 @@
 """This Python module covers a set of benchmark functions.
-    Here we do NOT include the test code for simplicity.
+    Here we do NOT include the *testing* code for simplicity.
 
-    For online documentations, please refer to:
-    https://pypop.readthedocs.io/en/latest/benchmarks.html
+    For online documentations of benchmark functions, please refer to:
+        https://pypop.readthedocs.io/en/latest/benchmarks.html
 """
-import pypop7.benchmarks.cases  # test cases for all benchmarking functions (based on sampling)
-import pypop7.benchmarks.utils  # utilities for saving, reading, plotting, and analyzing optimization results
+import pypop7.benchmarks.cases  # *test* cases for all benchmarking functions (based on sampling)
+# utilities for saving, reading, plotting, and analyzing optimization results
+import pypop7.benchmarks.utils
 import pypop7.benchmarks.base_functions  # base forms of benchmarking functions
 import pypop7.benchmarks.shifted_functions  # shift/transform forms of benchmarking functions
 import pypop7.benchmarks.rotated_functions  # rotation forms of benchmarking functions
 import pypop7.benchmarks.continuous_functions  # rotation-shift forms of benchmarking functions
 import pypop7.benchmarks.data_science  # loss functions from data science
 # NeverGrad: https://github.com/facebookresearch/nevergrad
-import pypop7.benchmarks.never_grad  # photonics model from the NeverGrad platform
+import pypop7.benchmarks.never_grad  # photonics model from *NeverGrad*
