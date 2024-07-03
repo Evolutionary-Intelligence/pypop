80b4e82e4921ba2e522d9ebd9aa2146e938dc46a
Evolutionary-Intelligence <duanqq077@163.com>
2024-07-04 01:16:53 +0800

    Update pypop7/optimizers/ds/test_cs.py

--- pypop7/optimizers/ds/test_cs.py
+++ pypop7/optimizers/ds/test_cs.py
@@ -9,6 +9,6 @@ def test_optimize():
     options = {'max_function_evaluations': 5000,  # to set optimizer options
                'seed_rng': 2022}
     cs = CS(problem, options)  # to initialize the black-box optimizer class
-    results = cs.optimize()  # to run its optimization/evolution process
+    results = cs.optimize()  # to run its optimization process
     assert results['n_function_evaluations'] == 5000
     assert results['best_so_far_y'] < 1.0
