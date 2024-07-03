5e8d520f8860cb34748f2bc3aef4ad23e90d3b3f
Evolutionary-Intelligence <duanqq077@163.com>
2024-07-04 01:15:23 +0800

    Update pypop7/optimizers/ds/test_hj.py

--- pypop7/optimizers/ds/test_hj.py
+++ pypop7/optimizers/ds/test_hj.py
@@ -9,6 +9,6 @@ def test_optimize():
     options = {'max_function_evaluations': 5000,  # to set optimizer options
                'seed_rng': 2022}
     hj = HJ(problem, options)  # to initialize the black-box optimizer class
-    results = hj.optimize()  # to run its optimization/evolution process
+    results = hj.optimize()  # to run its optimization process
     assert results['n_function_evaluations'] == 5000
     assert results['best_so_far_y'] < 1.0
