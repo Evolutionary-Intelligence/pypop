7a7432fd4604e776c4044dae8dbc5609123c05da
Evolutionary-Intelligence <duanqq077@163.com>
2024-07-04 18:15:19 +0800

    pypop7/optimizers/ga/test_gl25.py

--- pypop7/optimizers/ga/test_gl25.py
+++ pypop7/optimizers/ga/test_gl25.py
@@ -8,7 +8,7 @@ def test_optimize():
                'upper_boundary': 5.0 * numpy.ones((2,))}
     options = {'max_function_evaluations': 5000,  # to set optimizer options
                'seed_rng': 2022}
-    gl25 = GL25(problem, options)  # to initialize the optimizer class
-    results = gl25.optimize()  # to run the optimization process
+    gl25 = GL25(problem, options)  # to initialize the black-box optimizer class
+    results = gl25.optimize()  # to run the optimization/evolution process
     assert results['n_function_evaluations'] == 5000
     assert results['best_so_far_y'] < 1.0
