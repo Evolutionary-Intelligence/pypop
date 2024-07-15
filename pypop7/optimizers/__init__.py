d4ebed3ad93bb0c5d21274bf85ce9383eb045fb3
Evolutionary-Intelligence <duanqq077@163.com>
2024-07-15 20:00:43 +0800

    Update pypop7/optimizers/__init__.py

---
+++ pypop7/optimizers/__init__.py
@@ -0,0 +1,2 @@
+from pypop7.optimizers.core.optimizer import Optimizer  # base (abstract) class of all black-box optimizers
+from pypop7.optimizers.rs.prs import PRS  # Pure Random Search (PRS) [1952]
