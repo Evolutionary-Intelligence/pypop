2c534ee19c6b5911490598d3bea1a4cd125331be
Evolutionary-Intelligence <duanqq077@163.com>
2024-08-13 01:01:33 +0800

    Update pypop7/benchmarks/base_functions.py

--- pypop7/benchmarks/base_functions.py
+++ pypop7/benchmarks/base_functions.py
@@ -816,8 +816,6 @@ class Salomon(BaseFunction):
 def shubert(x):
     """**Shubert** test function.
 
-       .. note:: It's LaTeX formulation is `$\prod_{i=1}^{n} \sum_{j=1}^{5} j\cos((j+1) x_i j)$`.
-
     Parameters
     ----------
     x : ndarray
