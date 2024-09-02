5eda8052d656364b35074b72a663e92457ef93bf
Evolutionary-Intelligence <78018333+Evolutionary-Intelligence@users.noreply.github.com>
2024-09-02 23:51:21 +0800

    Update de.py

--- pypop7/optimizers/de/de.py
+++ pypop7/optimizers/de/de.py
@@ -1,6 +1,6 @@
 import numpy as np  # engine for numerical computing
 
-from pypop7.optimizers.core.optimizer import Optimizer  # abstract class of all black-box optimizers (BBO)
+from pypop7.optimizers.core.optimizer import Optimizer  # abstract class of all black-box optimizer (BBO) classes
 
 
 class DE(Optimizer):
@@ -9,12 +9,12 @@ class DE(Optimizer):
     This is the **abstract** class for all Differential Evolution (`DE`) classes. Please use any of its instantiated
     subclasses to optimize the black-box problem at hand.
 
-    .. note:: Originally `DE` was proposed to solve some challenging real-world black-box problems by Kenneth Price and
-       Rainer Storn, `recipients of IEEE Evolutionary Computation Pioneer Award 2017 <https://tinyurl.com/456as566>`_.
+    .. note:: `DE` was proposed to solve some challenging real-world black-box problems by Kenneth Price and Rainer
+       Storn, `two recipients of IEEE Evolutionary Computation Pioneer Award 2017 <https://tinyurl.com/456as566>`_.
        Although there is *few* significant theoretical advance till now (to our knowledge), it is **still widely used
        in practice**, owing to its often attractive search performance on many multimodal black-box functions. `"DE
-       borrows the idea from Nelder&Mead of employing information from within the vector population to alter the search
-       space."---[Storn&Price, 1997, JGO] <https://doi.org/10.1023/A:1008202821328>`_
+       borrows the idea from Nelder&Mead of employing information from within the vector population to alter the
+       search space."---[Storn&Price, 1997, JGO] <https://doi.org/10.1023/A:1008202821328>`_
 
        The popular and powerful `SciPy <https://www.nature.com/articles/s41592-019-0686-2>`_ library has provided an
        open-source Python implementation for `DE` with wide applications.
