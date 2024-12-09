16be515f9b8437a98433c347b923b100203be88e
Evolutionary-Intelligence <78018333+Evolutionary-Intelligence@users.noreply.github.com>
2024-12-09 15:16:04 +0800

    Update es.py

--- pypop7/optimizers/es/es.py
+++ pypop7/optimizers/es/es.py
@@ -52,8 +52,7 @@ class ES(Optimizer):
        <https://www.science.org/doi/10.1126/scirobotics.adg3705>`_,
        `[Milekovic et al., 2023, Nature Medicine (EPFL + ONWARD Medical + Medtronic Inc. + Oxford, etc.)]
        <https://www.nature.com/articles/s41591-023-02584-1>`_,
-       `[Koginov et al., 2023, TMRB]
-       <https://ieeexplore.ieee.org/document/10302449>`_,
+       `[TMRB-2023] <https://ieeexplore.ieee.org/document/10302449>`_,
        `[ACL-2023] <https://aclanthology.org/2023.acl-long.625.pdf>`_,
        `[NeurIPS-Workshop-2023] <https://openreview.net/pdf?id=KiQ8PhDzIU>`_,
        `[TVCG-2014] <https://ieeexplore.ieee.org/abstract/document/6781622>`_,
