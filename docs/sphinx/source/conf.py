1c7b91711f81be961d2451bc381c6a18e721e73e
Evolutionary-Intelligence <78018333+Evolutionary-Intelligence@users.noreply.github.com>
2025-01-02 21:53:33 +0800

    Update conf.py

--- docs/sphinx/source/conf.py
+++ docs/sphinx/source/conf.py
@@ -1,29 +1,12 @@
-# -- Path setup --------------------------------------------------------------
-
-# If extensions (or modules to document with autodoc) are in another directory,
-# add these directories to sys.path here. If the directory is relative to the
-# documentation root, use os.path.abspath to make it absolute, like shown here.
-#
 import os
 import sys
 sys.path.insert(0, os.path.abspath('../../../'))
 
-
-# -- Project information -----------------------------------------------------
-
 project = 'pypop7'
-copyright = '2021 - 2025, Evolutionary-Intelligence @ HIT & SUSTech'
-author = 'Evolutionary-Intelligence @ HIT & SUSTech'
+copyright = '2021-2025, Evolutionary-Intelligence (Qiqi Duan)@HIT&SUSTech'
+author = 'Evolutionary-Intelligence (Qiqi Duan)@HIT&SUSTech'
 
-# The full version, including alpha/beta/rc tags
 release = '0.0.1'
-
-
-# -- General configuration ---------------------------------------------------
-
-# Add any Sphinx extension module names here, as strings. They can be
-# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
-# ones.
 extensions = ['sphinx.ext.autodoc',
               'sphinx.ext.napoleon',
               'sphinx.ext.viewcode',
