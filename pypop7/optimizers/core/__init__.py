bab6ceeb185441f2a713328b02e4caa0e7e862f7
Evolutionary-Intelligence <78018333+Evolutionary-Intelligence@users.noreply.github.com>
2025-01-23 10:46:53 +0800

    Update __init__.py

--- pypop7/optimizers/core/__init__.py
+++ pypop7/optimizers/core/__init__.py
@@ -1,4 +1,7 @@
+# Helper class used by all black-box optimizers
 from pypop7.optimizers.core.optimizer import Terminations
+# Base (abstract) class of all optimizers for continuous
+#   black-box **minimization**
 from pypop7.optimizers.core.optimizer import Optimizer
 
 
