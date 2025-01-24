1ad6596b270a56231c0b9df6e05bc9a023823263
Evolutionary-Intelligence <78018333+Evolutionary-Intelligence@users.noreply.github.com>
2025-01-24 10:50:52 +0800

    Update optimizer.py

--- pypop7/optimizers/core/optimizer.py
+++ pypop7/optimizers/core/optimizer.py
@@ -44,10 +44,11 @@ class Optimizer(object):
         self.fitness_function = problem.get('fitness_function')  # to be *minimized*
         self.ndim_problem = problem.get('ndim_problem')  # number of problem dimensionality
         assert self.ndim_problem > 0
-        # mainly for the initialization process
+        # mainly for both the *initialization* process and the *constrained sampling* process:
+        #   Constrained sampling is covered in its extended library called `PyCoPop7`
         self.upper_boundary = problem.get('upper_boundary')  # upper boundary of search range
         self.lower_boundary = problem.get('lower_boundary')  # lower boundary of search range
-        # only for the initialization process
+        # only for the *initialization* process
         self.initial_upper_boundary = problem.get('initial_upper_boundary', self.upper_boundary)
         self.initial_lower_boundary = problem.get('initial_lower_boundary', self.lower_boundary)
         # mainly for the printing purpose
@@ -55,7 +56,7 @@ class Optimizer(object):
         if (self.problem_name is None) and hasattr(self.fitness_function, '__name__'):
             self.problem_name = self.fitness_function.__name__
 
-        # optimizer-related options
+        # optimizer-related basic options
         self.options = options
         self.max_function_evaluations = options.get('max_function_evaluations', np.inf)
         self.max_runtime = options.get('max_runtime', np.inf)
