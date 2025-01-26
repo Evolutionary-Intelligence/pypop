01648be2328e5dba9a49efbb8c7479d94ea4d610
Evolutionary-Intelligence <78018333+Evolutionary-Intelligence@users.noreply.github.com>
2025-01-27 01:13:56 +0800

    Update optimizer.py

--- pypop7/optimizers/core/optimizer.py
+++ pypop7/optimizers/core/optimizer.py
@@ -62,6 +62,7 @@ class Optimizer(object):
         self.max_function_evaluations = options.get('max_function_evaluations', np.inf)
         # for `MAX_RUNTIME` in `MAX_RUNTIME`
         self.max_runtime = options.get('max_runtime', np.inf)
+        # for `FITNESS_THRESHOLD` in `Terminations`
         self.fitness_threshold = options.get('fitness_threshold', -np.inf)
         self.n_individuals = options.get('n_individuals')  # offspring population size
         self.n_parents = options.get('n_parents')  # parent population size
