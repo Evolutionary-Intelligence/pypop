fb66c0418a613fe617f80d7f9f5aac3658c53377
Evolutionary-Intelligence (Qiqi Duan - 段琦琦) <78018333+Evolutionary-Intelligence@users.noreply.github.com>
2025-04-08 21:28:13 +0800

    Update compare_deap-es_vs_pypop7-ses.py

--- docs/efficiency/sphere/compare_deap-es_vs_pypop7-ses.py
+++ docs/efficiency/sphere/compare_deap-es_vs_pypop7-ses.py
@@ -1,5 +1,7 @@
 """Compare computational efficiency between *DEAP (SES)* and
     *PYPOP7 (SES)* on the well-known Sphere function.
+
+    Chinese: 该绘图脚本被段琦琦的博士论文（哈工大与南科大联合培养）所使用。
 """
 import os
 import sys
