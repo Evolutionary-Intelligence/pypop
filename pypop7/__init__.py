739f9e09fd630593cdfdaa52a4db28c24a2ace78
Evolutionary-Intelligence (Qiqi Duan - 段琦琦) <78018333+Evolutionary-Intelligence@users.noreply.github.com>
2025-04-15 17:50:16 +0800

    Update __init__.py

--- pypop7/__init__.py
+++ pypop7/__init__.py
@@ -1,14 +1,17 @@
-"""PyPop7 is a Pure-PYthon library of POPulation-based (e.g., evolutionary/swarm) OPtimization for single-objective,
-    real-parameter, black-box problems. Its main goal is to provide a **unified** interface and **elegant**
-    implementations for Black-Box Optimization (BBO), particularly population-based optimizers, in order to
-    facilitate research repeatability, BBO benchmarking, and also (possible) real-world applications.
+"""PyPop7 is a Pure-PYthon library of POPulation-based (e.g., evolutionary/swarm) OPtimization
+    for single-objective, real-parameter, black-box problems. Its main goal is to provide a
+    *unified* interface and *elegant* implementations for Black-Box Optimization (BBO),
+    particularly population-based optimizers, in order to facilitate research repeatability,
+    BBO benchmarking, and also (possible) real-world applications.
 
-    More specifically, for alleviating the notorious **curse of dimensionality** of BBO (based on iterative sampling),
-    the primary focus of PyPop7 is to cover their State-Of-The-Art (SOTA) implementations for Large-Scale Optimization
-    (LSO), though many of their other versions and variants are also included here (for benchmarking/mixing purpose,
-    and also for practical purpose).
+    More specifically, for alleviating the notorious **curse of dimensionality** of BBO (based
+    on iterative sampling), one primary focus of PyPop7 is to cover their State-Of-The-Art
+    (SOTA) implementations for Large-Scale Optimization (LSO), though many of their other
+    versions and variants are also included here (for benchmarking/mixing purpose, and also
+    for practical purpose).
 """
-from pypop7.optimizers.core import Optimizer, Terminations  # base (abstract) class for all optimizers
+# *base* (*abstract*) class for all global optimizers
+from pypop7.optimizers.core import Optimizer, Terminations
 from pypop7.optimizers.rs import RS, PRS, RHC, ARHC, SRS, GS, BES
 from pypop7.optimizers.ep import EP, CEP, FEP, LEP
 from pypop7.optimizers.ga import GA, GENITOR, G3PCX, GL25  # ASGA
