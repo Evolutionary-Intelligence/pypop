42f9845b56ed5afc87bc85d8ea15a5b635861844
Evolutionary-Intelligence (Qiqi Duan - 段琦琦) <78018333+Evolutionary-Intelligence@users.noreply.github.com>
2025-04-15 22:01:15 +0800

    Update __init__.py

--- pypop7/optimizers/__init__.py
+++ pypop7/optimizers/__init__.py
@@ -1,2 +1,2 @@
-from pypop7.optimizers.core.optimizer import Optimizer  # base (abstract) class of all black-box optimizers
-from pypop7.optimizers.rs.prs import PRS  # Pure Random Search (PRS) [1952]
+# *base* (*abstract*) class of all derivative-free optimizers
+from pypop7.optimizers.core.optimizer import Optimizer
