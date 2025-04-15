fd48208ac438ea36820e8d7ac6e62458e55dcc0f
Evolutionary-Intelligence (Qiqi Duan - 段琦琦) <78018333+Evolutionary-Intelligence@users.noreply.github.com>
2025-04-15 18:25:19 +0800

    Update __init__.py

--- pypop7/__init__.py
+++ pypop7/__init__.py
@@ -1,14 +1,15 @@
-"""PyPop7 is a Pure-PYthon library of POPulation-based (e.g., evolutionary/swarm) OPtimization
-    for single-objective, real-parameter, black-box problems. Its main goal is to provide a
-    *unified* interface and *elegant* implementations for Black-Box Optimization (BBO),
-    particularly population-based optimizers, in order to facilitate research repeatability,
-    BBO benchmarking, and also (possible) real-world applications.
+"""PyPop7 is a Pure-PYthon library of POPulation-based (e.g., evolutionary/swarm)
+    OPtimization for single-objective, real-parameter, global problems. Its main
+    goal is to provide a *unified* interface and *elegant* implementations for
+    Derivative-Free Optimization (DFO), particularly population-based optimizers,
+    in order to facilitate research repeatability, DFO benchmarking, and also
+    (possible) real-world applications.
 
-    More specifically, for alleviating the notorious **curse of dimensionality** of BBO (based
-    on iterative sampling), one primary focus of PyPop7 is to cover their State-Of-The-Art
-    (SOTA) implementations for Large-Scale Optimization (LSO), though many of their other
-    versions and variants are also included here (for benchmarking/mixing purpose, and also
-    for practical purpose).
+    More specifically, for alleviating the notorious **curse of dimensionality**
+    of DFO (based on iterative sampling), one primary focus of PyPop7 is to cover
+    their State-Of-The-Art (SOTA) implementations for Large-Scale Optimization
+    (LSO), though many of their other versions and variants are also included here
+    (for benchmarking/mixing purpose, and also for practical purpose).
 """
 # *base* (*abstract*) class for all global optimizers
 from pypop7.optimizers.core import Optimizer, Terminations
