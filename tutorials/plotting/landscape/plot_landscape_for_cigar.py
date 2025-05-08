0bf8b35d94b15218688e3b0d689f7136f837c71b
Evolutionary-Intelligence (Qiqi Duan - 段琦琦) <78018333+Evolutionary-Intelligence@users.noreply.github.com>
2025-05-08 19:50:38 +0800

    Add files via upload

---
+++ tutorials/plotting/landscape/plot_landscape_for_cigar.py
@@ -0,0 +1,36 @@
+import numpy as np
+import matplotlib
+import matplotlib.pyplot as plt
+
+from pypop7.benchmarks.base_functions import cigar as bf
+from pypop7.benchmarks.utils import generate_xyz
+
+
+font_size = 10
+font_family = 'Times New Roman'
+matplotlib.rcParams['font.family'] = font_family
+matplotlib.rcParams['font.size'] = font_size
+
+
+# helper function for 2D-plotting
+def plot_contour(func, x, y):
+    levels = [bf(i * np.ones((2,))) for i in np.arange(0, 12, 2)]
+    x, y, z = generate_xyz(func, x, y, 500)
+    plt.contourf(x, y, z, cmap='Blues', levels=levels)
+    plt.contour(x, y, z, colors='white', levels=levels)
+
+
+if __name__ == '__main__':
+    bound=[-10.0, 10.0]
+    plt.figure(figsize=(2.5, 2.5))
+    plt.title(bf.__name__, fontsize=font_size)
+    plt.xlim(bound)
+    plt.ylim(bound)
+    plt.xticks(fontsize=font_size, fontfamily=font_family)
+    plt.yticks(fontsize=font_size, fontfamily=font_family)
+    plot_contour(bf, bound, bound)
+    plt.xlabel('X1', fontsize=font_size)
+    plt.ylabel('X2', fontsize=font_size, labelpad=-1)
+    plt.savefig(str(bf.__name__) + '.png',
+                dpi=700, bbox_inches='tight')
+    plt.show()
