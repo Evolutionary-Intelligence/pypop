e1658d993c813ccf45754995ef39d84c5be07b2b
Evolutionary-Intelligence (Qiqi Duan - 段琦琦) <78018333+Evolutionary-Intelligence@users.noreply.github.com>
2025-06-14 22:01:46 +0800

    Update cmaes.py

--- pypop7/optimizers/es/cmaes.py
+++ pypop7/optimizers/es/cmaes.py
@@ -59,7 +59,6 @@ class CMAES(ES):
        <https://www.sciencedirect.com/science/article/pii/S0094576514002318>`_, `[Khaira et al., 2014, ACS Macro Lett.]
        <https://pubs.acs.org/doi/full/10.1021/mz5002349>`_, `[Otake et al., 2013, Phys. Med. Biol.]
        <https://iopscience.iop.org/article/10.1088/0031-9155/58/23/8535>`_,
-       `RoboCup <https://doi.org/10.1007/s10458-024-09642-z>`_, 2014 3D Simulation League Competition Champions,
        to name a few.
 
     Parameters
