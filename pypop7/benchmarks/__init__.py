686ca38ef40663d3dfaf1a9c7703bd2ddddbd2f1
Evolutionary-Intelligence (Qiqi Duan - 段琦琦) <78018333+Evolutionary-Intelligence@users.noreply.github.com>
2025-09-12 11:19:12 +0800

    Update __init__.py

--- pypop7/benchmarks/__init__.py
+++ pypop7/benchmarks/__init__.py
@@ -1,6 +1,8 @@
 """This Python module covers a set of benchmark functions. For online documentations of all
     benchmark functions, please refer to: https://pypop.readthedocs.io/en/latest/benchmarks.html
 """
+# for all functions with only two dimensions, mainly for visualization
+import pypop7.benchmarks.ndim_2
 # *test* cases for all artificially-constructed benchmarking functions
 import pypop7.benchmarks.cases
 # utilities for saving, reading, plotting, and analyzing optimization results
