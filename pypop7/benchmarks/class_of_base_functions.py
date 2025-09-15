4dacf41b4f728a6b2e4792c76f06029264449b5c
Evolutionary-Intelligence (Qiqi Duan - 段琦琦) <78018333+Evolutionary-Intelligence@users.noreply.github.com>
2025-09-15 11:00:31 +0800

    Update class_of_base_functions.py

--- pypop7/benchmarks/class_of_base_functions.py
+++ pypop7/benchmarks/class_of_base_functions.py
@@ -124,6 +124,23 @@ class Rastrigin(BaseFunction):
         return bf.rastrigin(x)
 
 
+class Rosenbrock(BaseFunction):
+    def __call__(self, x):
+        """Class of **Rosenbrock** test function.
+
+        Parameters
+        ----------
+        x : ndarray
+            An input vector.
+
+        Returns
+        -------
+        y : float
+            A scalar fitness.
+        """
+        return bf.rosenbrock(x)
+
+
 class Schwefel221(BaseFunction):
     def __call__(self, x):
         """Class of **Schwefel221** test function.
