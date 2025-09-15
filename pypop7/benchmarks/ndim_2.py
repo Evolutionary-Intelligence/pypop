2b1d15fd7f8e81f9864e3d38228f55a0897b42e8
Evolutionary-Intelligence (Qiqi Duan - 段琦琦) <78018333+Evolutionary-Intelligence@users.noreply.github.com>
2025-09-15 11:57:29 +0800

    Update ndim_2.py

--- pypop7/benchmarks/ndim_2.py
+++ pypop7/benchmarks/ndim_2.py
@@ -1,1 +1,20 @@
+import math
 
+
+def cosine(x):
+    """**Cosine** test function.
+
+    Parameters
+    ----------
+    x: ndarray
+       input vector.
+
+    Returns
+    -------
+    y: float
+       scalar fitness.
+    """
+    y = 10.0 * x[0] ** 2 * (1.0 + 0.75 * math.cos(70.0 * x[0]) / 12.0) + math.cos(100.0 * x[0]) ** 2 / 24.0 + \
+        2.0 * x[1] ** 2 * (1.0 + 0.75 * math.cos(70.0 * x[1]) / 12.0) + math.cos(100.0 * x[1]) ** 2 / 24.0 + \
+        4.0 * x[0] * x[1]
+    return y
