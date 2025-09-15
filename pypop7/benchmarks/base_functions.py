43e6d3cfe8d022b1c1ea4b3e376e398d9bfc77c1
Evolutionary-Intelligence (Qiqi Duan - 段琦琦) <78018333+Evolutionary-Intelligence@users.noreply.github.com>
2025-09-15 11:56:34 +0800

    Update base_functions.py

--- pypop7/benchmarks/base_functions.py
+++ pypop7/benchmarks/base_functions.py
@@ -656,25 +656,6 @@ class Schaffer(BaseFunction):
 
 
 # all of the following functions are only for visualization purpose
-def cosine(x):
-    """**Cosine** test function.
-
-    Parameters
-    ----------
-    x: ndarray
-       input vector.
-
-    Returns
-    -------
-    y: float
-       scalar fitness.
-    """
-    y = 10.0 * x[0] ** 2 * (1.0 + 0.75 * math.cos(70.0 * x[0]) / 12.0) + math.cos(100.0 * x[0]) ** 2 / 24.0 + \
-        2.0 * x[1] ** 2 * (1.0 + 0.75 * math.cos(70.0 * x[1]) / 12.0) + math.cos(100.0 * x[1]) ** 2 / 24.0 + \
-        4.0 * x[0] * x[1]
-    return y
-
-
 def dennis_woods(x):
     """**Dennis-Woods** test function.
 
