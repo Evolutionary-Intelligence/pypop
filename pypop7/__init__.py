19e43d7f5fb66369457ddf4f005ff6c8266d2a35
Evolutionary-Intelligence (Qiqi Duan - 段琦琦) <78018333+Evolutionary-Intelligence@users.noreply.github.com>
2025-09-16 16:05:57 +0800

    Update __init__.py

--- pypop7/__init__.py
+++ pypop7/__init__.py
@@ -1,18 +1,19 @@
-"""PyPop7 is a Pure-PYthon library of POPulation-based (e.g., evolutionary/swarm)
-    OPtimization for single-objective, real-parameter, global problems. Its main
-    goal is to provide a *unified* interface and *elegant* implementations for
-    Derivative-Free Optimization (DFO), particularly population-based optimizers,
-    in order to facilitate research repeatability, DFO benchmarking, and also
-    (possible) real-world applications.
+"""PyPop7 is a Pure-PYthon library of POPulation-based randomized OPtimization
+    algorithm for single-objective, real-parameter problems. Its main goal is
+    to provide a *unified* interface as well as *elegant* implementations for
+    Derivative-Free Optimization, particularly population-based optimizers,
+    in order to facilitate research repeatability, algorithmic benchmarking,
+    and also real-world applications.
 
     More specifically, for alleviating the notorious **curse of dimensionality**
-    of DFO (based on iterative sampling), one primary focus of PyPop7 is to cover
-    their State-Of-The-Art (SOTA) implementations for Large-Scale Optimization
-    (LSO), though many (rather all) of their small-and-medium-scaled versions and
-    variants are also included for *benchmarking*, *hybrid*, *educational*, and
-    more importantly **practical** purposes.
+    issue (owing to a limited population of randomized samples), one *primary*
+    focus of PyPop7 is to cover their State-Of-The-Art (SOTA) implementations
+    for Large-Scale Optimization (LSO), though many (rather all) of their
+    small-and-medium-scaled versions and variants are also included for
+    *benchmarking*, *hybrid*, *educational*, and more importantly **practical**
+    purposes.
 """
-# termination conditions shared for all global optimizers
+# termination conditions shared by all optimizers
 from pypop7.optimizers.core import Terminations
-# *base* (*abstract*) class for all global optimizers
+# *base* (*abstract*) class for all optimizers
 from pypop7.optimizers.core import Optimizer
