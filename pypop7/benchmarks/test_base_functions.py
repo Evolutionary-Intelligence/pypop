0aa58203ef663eade62ce0c9fdf565a8693ddbea
Evolutionary-Intelligence (Qiqi Duan - 段琦琦) <78018333+Evolutionary-Intelligence@users.noreply.github.com>
2025-09-16 14:33:39 +0800

    Update test_base_functions.py

--- pypop7/benchmarks/test_base_functions.py
+++ pypop7/benchmarks/test_base_functions.py
@@ -138,13 +138,6 @@ class TestBaseFunctions(unittest.TestCase):
                 self.assertTrue(sample.compare(func, ndim, get_y_scaled_rastrigin(ndim - 1), atol=0.01))
             self.assertTrue(sample.check_origin(func))
 
-    def test_skew_rastrigin(self):
-        sample = Cases()
-        for func in [skew_rastrigin, SkewRastrigin()]:
-            for ndim in range(1, 5):
-                self.assertTrue(sample.compare(func, ndim, get_y_skew_rastrigin(ndim - 1), atol=0.1))
-            self.assertTrue(sample.check_origin(func))
-
     def test_levy_montalvo(self):
         for func in [levy_montalvo, LevyMontalvo()]:
             for ndim in range(1, 8):
@@ -171,6 +164,12 @@ class TestBaseFunctions(unittest.TestCase):
             for ndim in range(1, 4):
                 self.assertTrue(sample.compare(func, ndim, get_y_schaffer(ndim - 1), atol=0.01))
             self.assertTrue(sample.check_origin(func))
+
+    def test_skew_rastrigin(self):
+        sample = Cases()
+        for ndim in range(1, 5):
+            self.assertTrue(sample.compare(skew_rastrigin, ndim, get_y_skew_rastrigin(ndim - 1), atol=0.1))
+        self.assertTrue(sample.check_origin(skew_rastrigin))
 
     def test_sphere(self):
         sample = Cases()
