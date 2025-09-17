a6e3ce13df0f2732fd31c50417174f16ec49259d
Evolutionary-Intelligence (Qiqi Duan - 段琦琦) <78018333+Evolutionary-Intelligence@users.noreply.github.com>
2025-09-17 10:47:05 +0800

    Update base_functions.py

--- pypop7/benchmarks/base_functions.py
+++ pypop7/benchmarks/base_functions.py
@@ -58,7 +58,7 @@ def ackley(x):
         A scalar fitness.
     """
     x = squeeze_and_check(x)
-    y = 20.0 + np.exp(1)
+    y = 20.0 + np.exp(1.0)
     y -= 20.0 * np.exp(-0.2 * np.sqrt(np.sum(np.square(x)) / len(x)))
     y -= np.exp(np.sum(np.cos(2.0 * np.pi * x)) / len(x))
     return y
