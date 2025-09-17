89338e90e44a986a4bde237607127d176960e7ea
Evolutionary-Intelligence (Qiqi Duan - 段琦琦) <78018333+Evolutionary-Intelligence@users.noreply.github.com>
2025-09-17 16:14:24 +0800

    Update class_of_base_functions.py

--- pypop7/benchmarks/class_of_base_functions.py
+++ pypop7/benchmarks/class_of_base_functions.py
@@ -22,7 +22,7 @@ class BaseFunction(object):
         pass
 
 
-# a list of 23 *scalable* base functions:
+# a list of 23 classes of *scalable* base functions:
 class Ackley(BaseFunction):
     def __call__(self, x):
         """Class of **Ackley** test function.
