f62784b6987e4bdee733458f20b36b56b4dec815
Evolutionary-Intelligence (Qiqi Duan - 段琦琦) <78018333+Evolutionary-Intelligence@users.noreply.github.com>
2025-09-18 14:08:33 +0800

    Update ndim_2.py

--- pypop7/benchmarks/ndim_2.py
+++ pypop7/benchmarks/ndim_2.py
@@ -3,6 +3,30 @@ import math
 import numpy as np
 
 
+def bohachevsky(x):
+    """**Bohachevsky** test function.
+
+        .. note:: Its dimensionality should `> 1`.
+
+    Parameters
+    ----------
+    x : ndarray
+        An input vector.
+
+    Returns
+    -------
+    y : float
+        A scalar fitness.
+    """
+    x = squeeze_and_check(x, True)
+    y = 0.7 * len(x)
+    for i in range(len(x) - 1):
+        y += np.square(x[i]) + 2.0 * np.square(x[i + 1])
+        y -= 0.3 * np.cos(3.0 * np.pi * x[i])
+        y -= 0.4 * np.cos(4.0 * np.pi * x[i + 1])
+    return y
+
+
 def cosine(x):
     """**Cosine** test function.
 
