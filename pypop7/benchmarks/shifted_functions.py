7428748f6b4a1c95d129291ae371a0d834c09bb8
Evolutionary-Intelligence (Qiqi Duan - 段琦琦) <78018333+Evolutionary-Intelligence@users.noreply.github.com>
2025-09-18 19:11:06 +0800

    Update shifted_functions.py

--- pypop7/benchmarks/shifted_functions.py
+++ pypop7/benchmarks/shifted_functions.py
@@ -1,4 +1,4 @@
-"""For the online documentation of these base functions, please refer to:
+"""For the online documentation of shifted forms of these base functions, please refer to:
     https://pypop.readthedocs.io/en/latest/benchmarks.html#shifted-transformed-forms
 
     Here all of below base functions are ordered only in *name*. However,
