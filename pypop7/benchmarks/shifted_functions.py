777aa0073ec2600e5c59a61b64cbc47af847b2a0
Evolutionary-Intelligence (Qiqi Duan - 段琦琦) <78018333+Evolutionary-Intelligence@users.noreply.github.com>
2025-09-18 12:41:20 +0800

    Update shifted_functions.py

--- pypop7/benchmarks/shifted_functions.py
+++ pypop7/benchmarks/shifted_functions.py
@@ -108,15 +108,6 @@ def sphere(x, shift_vector=None):
     return y
 
 
-class Sphere(BaseFunction):
-    def __init__(self):
-        BaseFunction.__init__(self)
-        self.__name__ = 'sphere'
-
-    def __call__(self, x, shift_vector=None):
-        return sphere(x, shift_vector)
-
-
 def cigar(x, shift_vector=None):
     """**Cigar** test function.
 
@@ -139,26 +130,6 @@ def cigar(x, shift_vector=None):
     shift_vector = load_shift_vector(cigar, x, shift_vector)
     y = base_functions.cigar(x - shift_vector)
     return y
-
-
-class Cigar(BaseFunction):
-    def __init__(self):
-        BaseFunction.__init__(self)
-        self.__name__ = 'cigar'
-
-    def __call__(self, x, shift_vector=None):
-        """
-        Parameters
-        ----------
-        x : ndarray
-            input vector.
-    
-        Returns
-        -------
-        y : float
-            scalar fitness.
-        """
-        return cigar(x, shift_vector)
 
 
 def discus(x, shift_vector=None):  # also called tablet
@@ -185,26 +156,6 @@ def discus(x, shift_vector=None):  # also called tablet
     return y
 
 
-class Discus(BaseFunction):  # also called Tablet
-    def __init__(self):
-        BaseFunction.__init__(self)
-        self.__name__ = 'discus'
-
-    def __call__(self, x, shift_vector=None):
-        """
-        Parameters
-        ----------
-        x : ndarray
-            input vector.
-    
-        Returns
-        -------
-        y : float
-            scalar fitness.
-        """
-        return discus(x, shift_vector)
-
-
 def cigar_discus(x, shift_vector=None):
     """**Cigar-Discus** test function.
 
@@ -227,26 +178,6 @@ def cigar_discus(x, shift_vector=None):
     shift_vector = load_shift_vector(cigar_discus, x, shift_vector)
     y = base_functions.cigar_discus(x - shift_vector)
     return y
-
-
-class CigarDiscus(BaseFunction):
-    def __init__(self):
-        BaseFunction.__init__(self)
-        self.__name__ = 'cigar_discus'
-
-    def __call__(self, x, shift_vector=None):
-        """
-        Parameters
-        ----------
-        x : ndarray
-            input vector.
-        
-        Returns
-        -------
-        y : float
-            scalar fitness.
-        """
-        return cigar_discus(x, shift_vector)
 
 
 def ellipsoid(x, shift_vector=None):
@@ -273,26 +204,6 @@ def ellipsoid(x, shift_vector=None):
     return y
 
 
-class Ellipsoid(BaseFunction):
-    def __init__(self):
-        BaseFunction.__init__(self)
-        self.__name__ = 'ellipsoid'
-
-    def __call__(self, x, shift_vector=None):
-        """
-        Parameters
-        ----------
-        x : ndarray
-            input vector.
-        
-        Returns
-        -------
-        y : float
-            scalar fitness.
-        """
-        return ellipsoid(x, shift_vector)
-
-
 def different_powers(x, shift_vector=None):
     """**Different-Powers** test function.
 
@@ -315,26 +226,6 @@ def different_powers(x, shift_vector=None):
     shift_vector = load_shift_vector(different_powers, x, shift_vector)
     y = base_functions.different_powers(x - shift_vector)
     return y
-
-
-class DifferentPowers(BaseFunction):
-    def __init__(self):
-        BaseFunction.__init__(self)
-        self.__name__ = 'different_powers'
-
-    def __call__(self, x, shift_vector=None):
-        """
-        Parameters
-        ----------
-        x : ndarray
-            input vector.
-        
-        Returns
-        -------
-        y : float
-            scalar fitness.
-        """
-        return different_powers(x, shift_vector)
 
 
 def schwefel221(x, shift_vector=None):
@@ -361,26 +252,6 @@ def schwefel221(x, shift_vector=None):
     return y
 
 
-class Schwefel221(BaseFunction):
-    def __init__(self):
-        BaseFunction.__init__(self)
-        self.__name__ = 'schwefel221'
-
-    def __call__(self, x, shift_vector=None):
-        """
-        Parameters
-        ----------
-        x : ndarray
-            input vector.
-        
-        Returns
-        -------
-        y : float
-            scalar fitness.
-        """
-        return schwefel221(x, shift_vector)
-
-
 def step(x, shift_vector=None):
     """**Step** test function.
 
@@ -403,26 +274,6 @@ def step(x, shift_vector=None):
     shift_vector = load_shift_vector(step, x, shift_vector)
     y = base_functions.step(x - shift_vector)
     return y
-
-
-class Step(BaseFunction):
-    def __init__(self):
-        BaseFunction.__init__(self)
-        self.__name__ = 'step'
-
-    def __call__(self, x, shift_vector=None):
-        """
-        Parameters
-        ----------
-        x : ndarray
-            input vector.
-        
-        Returns
-        -------
-        y : float
-            scalar fitness.
-        """
-        return step(x, shift_vector)
 
 
 def schwefel222(x, shift_vector=None):
@@ -449,26 +300,6 @@ def schwefel222(x, shift_vector=None):
     return y
 
 
-class Schwefel222(BaseFunction):
-    def __init__(self):
-        BaseFunction.__init__(self)
-        self.__name__ = 'schwefel222'
-
-    def __call__(self, x, shift_vector=None):
-        """
-        Parameters
-        ----------
-        x : ndarray
-            input vector.
-        
-        Returns
-        -------
-        y : float
-            scalar fitness.
-        """
-        return schwefel222(x, shift_vector)
-
-
 def rosenbrock(x, shift_vector=None):
     """**Rosenbrock** test function.
 
@@ -491,26 +322,6 @@ def rosenbrock(x, shift_vector=None):
     shift_vector = load_shift_vector(rosenbrock, x, shift_vector)
     y = base_functions.rosenbrock(x - shift_vector)
     return y
-
-
-class Rosenbrock(BaseFunction):
-    def __init__(self):
-        BaseFunction.__init__(self)
-        self.__name__ = 'rosenbrock'
-
-    def __call__(self, x, shift_vector=None):
-        """
-        Parameters
-        ----------
-        x : ndarray
-            input vector.
-        
-        Returns
-        -------
-        y : float
-            scalar fitness.
-        """
-        return rosenbrock(x, shift_vector)
 
 
 def schwefel12(x, shift_vector=None):
@@ -537,26 +348,6 @@ def schwefel12(x, shift_vector=None):
     return y
 
 
-class Schwefel12(BaseFunction):
-    def __init__(self):
-        BaseFunction.__init__(self)
-        self.__name__ = 'schwefel12'
-
-    def __call__(self, x, shift_vector=None):
-        """
-        Parameters
-        ----------
-        x : ndarray
-            input vector.
-        
-        Returns
-        -------
-        y : float
-            scalar fitness.
-        """
-        return schwefel12(x, shift_vector)
-
-
 def exponential(x, shift_vector=None):
     """**Exponential** test function.
 
@@ -579,26 +370,6 @@ def exponential(x, shift_vector=None):
     shift_vector = load_shift_vector(exponential, x, shift_vector)
     y = base_functions.exponential(x - shift_vector)
     return y
-
-
-class Exponential(BaseFunction):
-    def __init__(self):
-        BaseFunction.__init__(self)
-        self.__name__ = 'exponential'
-
-    def __call__(self, x, shift_vector=None):
-        """
-        Parameters
-        ----------
-        x : ndarray
-            input vector.
-        
-        Returns
-        -------
-        y : float
-            scalar fitness.
-        """
-        return exponential(x, shift_vector)
 
 
 def griewank(x, shift_vector=None):
@@ -625,26 +396,6 @@ def griewank(x, shift_vector=None):
     return y
 
 
-class Griewank(BaseFunction):
-    def __init__(self):
-        BaseFunction.__init__(self)
-        self.__name__ = 'griewank'
-
-    def __call__(self, x, shift_vector=None):
-        """
-        Parameters
-        ----------
-        x : ndarray
-            input vector.
-        
-        Returns
-        -------
-        y : float
-            scalar fitness.
-        """
-        return griewank(x, shift_vector)
-
-
 def bohachevsky(x, shift_vector=None):
     """**Bohachevsky** test function.
 
@@ -667,26 +418,6 @@ def bohachevsky(x, shift_vector=None):
     shift_vector = load_shift_vector(bohachevsky, x, shift_vector)
     y = base_functions.bohachevsky(x - shift_vector)
     return y
-
-
-class Bohachevsky(BaseFunction):
-    def __init__(self):
-        BaseFunction.__init__(self)
-        self.__name__ = 'bohachevsky'
-
-    def __call__(self, x, shift_vector=None):
-        """
-        Parameters
-        ----------
-        x : ndarray
-            input vector.
-        
-        Returns
-        -------
-        y : float
-            scalar fitness.
-        """
-        return bohachevsky(x, shift_vector)
 
 
 def ackley(x, shift_vector=None):
@@ -713,26 +444,6 @@ def ackley(x, shift_vector=None):
     return y
 
 
-class Ackley(BaseFunction):
-    def __init__(self):
-        BaseFunction.__init__(self)
-        self.__name__ = 'ackley'
-
-    def __call__(self, x, shift_vector=None):
-        """
-        Parameters
-        ----------
-        x : ndarray
-            input vector.
-        
-        Returns
-        -------
-        y : float
-            scalar fitness.
-        """
-        return ackley(x, shift_vector)
-
-
 def rastrigin(x, shift_vector=None):
     """**Rastrigin** test function.
 
@@ -755,26 +466,6 @@ def rastrigin(x, shift_vector=None):
     shift_vector = load_shift_vector(rastrigin, x, shift_vector)
     y = base_functions.rastrigin(x - shift_vector)
     return y
-
-
-class Rastrigin(BaseFunction):
-    def __init__(self):
-        BaseFunction.__init__(self)
-        self.__name__ = 'rastrigin'
-
-    def __call__(self, x, shift_vector=None):
-        """
-        Parameters
-        ----------
-        x : ndarray
-            input vector.
-        
-        Returns
-        -------
-        y : float
-            scalar fitness.
-        """
-        return rastrigin(x, shift_vector)
 
 
 def scaled_rastrigin(x, shift_vector=None):
@@ -801,26 +492,6 @@ def scaled_rastrigin(x, shift_vector=None):
     return y
 
 
-class ScaledRastrigin(BaseFunction):
-    def __init__(self):
-        BaseFunction.__init__(self)
-        self.__name__ = 'scaled_rastrigin'
-
-    def __call__(self, x, shift_vector=None):
-        """
-        Parameters
-        ----------
-        x : ndarray
-            input vector.
-        
-        Returns
-        -------
-        y : float
-            scalar fitness.
-        """
-        return scaled_rastrigin(x, shift_vector)
-
-
 def skew_rastrigin(x, shift_vector=None):
     """**Skew-Rastrigin** test function.
 
@@ -843,26 +514,6 @@ def skew_rastrigin(x, shift_vector=None):
     shift_vector = load_shift_vector(skew_rastrigin, x, shift_vector)
     y = base_functions.skew_rastrigin(x - shift_vector)
     return y
-
-
-class SkewRastrigin(BaseFunction):
-    def __init__(self):
-        BaseFunction.__init__(self)
-        self.__name__ = 'skew_rastrigin'
-
-    def __call__(self, x, shift_vector=None):
-        """
-        Parameters
-        ----------
-        x : ndarray
-            input vector.
-        
-        Returns
-        -------
-        y : float
-            scalar fitness.
-        """
-        return skew_rastrigin(x, shift_vector)
 
 
 def levy_montalvo(x, shift_vector=None):
@@ -889,26 +540,6 @@ def levy_montalvo(x, shift_vector=None):
     return y
 
 
-class LevyMontalvo(BaseFunction):
-    def __init__(self):
-        BaseFunction.__init__(self)
-        self.__name__ = 'levy_montalvo'
-
-    def __call__(self, x, shift_vector=None):
-        """
-        Parameters
-        ----------
-        x : ndarray
-            input vector.
-        
-        Returns
-        -------
-        y : float
-            scalar fitness.
-        """
-        return levy_montalvo(x, shift_vector)
-
-
 def michalewicz(x, shift_vector=None):
     """**Michalewicz** test function.
 
@@ -931,26 +562,6 @@ def michalewicz(x, shift_vector=None):
     shift_vector = load_shift_vector(michalewicz, x, shift_vector)
     y = base_functions.michalewicz(x - shift_vector)
     return y
-
-
-class Michalewicz(BaseFunction):
-    def __init__(self):
-        BaseFunction.__init__(self)
-        self.__name__ = 'michalewicz'
-
-    def __call__(self, x, shift_vector=None):
-        """
-        Parameters
-        ----------
-        x : ndarray
-            input vector.
-        
-        Returns
-        -------
-        y : float
-            scalar fitness.
-        """
-        return michalewicz(x, shift_vector)
 
 
 def salomon(x, shift_vector=None):
@@ -977,26 +588,6 @@ def salomon(x, shift_vector=None):
     return y
 
 
-class Salomon(BaseFunction):
-    def __init__(self):
-        BaseFunction.__init__(self)
-        self.__name__ = 'salomon'
-
-    def __call__(self, x, shift_vector=None):
-        """
-        Parameters
-        ----------
-        x : ndarray
-            input vector.
-        
-        Returns
-        -------
-        y : float
-            scalar fitness.
-        """
-        return salomon(x, shift_vector)
-
-
 def shubert(x, shift_vector=None):
     """**Shubert** test function.
 
@@ -1021,26 +612,6 @@ def shubert(x, shift_vector=None):
     return y
 
 
-class Shubert(BaseFunction):
-    def __init__(self):
-        BaseFunction.__init__(self)
-        self.__name__ = 'shubert'
-
-    def __call__(self, x, shift_vector=None):
-        """
-        Parameters
-        ----------
-        x : ndarray
-            input vector.
-        
-        Returns
-        -------
-        y : float
-            scalar fitness.
-        """
-        return shubert(x, shift_vector)
-
-
 def schaffer(x, shift_vector=None):
     """**Schaffert** test function.
 
@@ -1063,23 +634,3 @@ def schaffer(x, shift_vector=None):
     shift_vector = load_shift_vector(schaffer, x, shift_vector)
     y = base_functions.schaffer(x - shift_vector)
     return y
-
-
-class Schaffer(BaseFunction):
-    def __init__(self):
-        BaseFunction.__init__(self)
-        self.__name__ = 'schaffer'
-
-    def __call__(self, x, shift_vector=None):
-        """
-        Parameters
-        ----------
-        x : ndarray
-            input vector.
-        
-        Returns
-        -------
-        y : float
-            scalar fitness.
-        """
-        return schaffer(x, shift_vector)
