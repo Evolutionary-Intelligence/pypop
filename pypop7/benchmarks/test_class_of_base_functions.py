397247c8eb0f8ec2b558be2430a2427d169400e4
Evolutionary-Intelligence (Qiqi Duan - 段琦琦) <78018333+Evolutionary-Intelligence@users.noreply.github.com>
2025-09-18 16:59:09 +0800

    Update test_class_of_base_functions.py

--- pypop7/benchmarks/test_class_of_base_functions.py
+++ pypop7/benchmarks/test_class_of_base_functions.py
@@ -29,20 +29,12 @@ from pypop7.benchmarks.cases import *
 
 
 class TestBaseFunctions(unittest.TestCase):
-    def test_squeeze_and_check(self):
-        self.assertEqual(squeeze_and_check(0), np.array([0]))
-        self.assertEqual(squeeze_and_check(np.array(0)), np.array([0]))
-        x1 = np.array([0.7])
-        self.assertEqual(squeeze_and_check(x1), x1)
-        x2 = np.array([0.0, 1.0])
-        self.assertTrue(np.allclose(squeeze_and_check(x2), x2))
-        x3 = np.arange(6).reshape(2, 3)
-        with self.assertRaisesRegex(TypeError, 'The number+'):
-            squeeze_and_check(x3)
-        with self.assertRaisesRegex(TypeError, 'The size should > 1+'):
-            squeeze_and_check(x1, True)
-        with self.assertRaisesRegex(TypeError, 'the size should != 0.'):
-            squeeze_and_check([])
+    def test_ackley(self):
+        sample = Cases()
+        ackley = Ackley()
+        for ndim in range(2, 8):
+            self.assertTrue(sample.compare(ackley, ndim, get_y_ackley(ndim - 2), atol=0.001))
+        self.assertTrue(sample.check_origin(ackley))
 
     def test_cigar(self):
         sample = Cases()
@@ -107,13 +99,6 @@ class TestBaseFunctions(unittest.TestCase):
         for func in [griewank, Griewank()]:
             for ndim in range(2, 8):
                 self.assertTrue(sample.compare(func, ndim, get_y_griewank(ndim - 2), atol=0.001))
-            self.assertTrue(sample.check_origin(func))
-
-    def test_ackley(self):
-        sample = Cases()
-        for func in [ackley, Ackley()]:
-            for ndim in range(2, 8):
-                self.assertTrue(sample.compare(func, ndim, get_y_ackley(ndim - 2), atol=0.001))
             self.assertTrue(sample.check_origin(func))
 
     def test_rastrigin(self):
