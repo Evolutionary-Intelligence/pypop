354be6234726a98cb60ed49d376a2885f567e600
Evolutionary-Intelligence (Qiqi Duan - 段琦琦) <78018333+Evolutionary-Intelligence@users.noreply.github.com>
2025-09-19 12:40:24 +0800

    Update shifted_functions.py

--- pypop7/benchmarks/shifted_functions.py
+++ pypop7/benchmarks/shifted_functions.py
@@ -21,8 +21,8 @@ def generate_shift_vector(func, ndim, low, high, seed=None):
     """Generate a *random* shift vector of dimension `ndim`, sampled uniformly
        between `low` (inclusive) and `high` (exclusive).
 
-       .. note:: The generated shift vector will be automatically stored in *txt* form
-          **for further use**.
+       .. note:: The generated shift vector will be automatically stored in
+          the *txt* form **for further use**.
 
     Parameters
     ----------
