988fcc4552c9acc343c40b075e7ce5836dfa0e5a
Evolutionary-Intelligence (Qiqi Duan - 段琦琦) <78018333+Evolutionary-Intelligence@users.noreply.github.com>
2025-11-12 17:39:26 +0800

    Update optimizer.py

--- pypop7/optimizers/core/optimizer.py
+++ pypop7/optimizers/core/optimizer.py
@@ -56,21 +56,25 @@ class Optimizer(object):
                 * 'seed_rng'                 - seed for random number generation needed to be *explicitly* set (`int`).
     """
     def __init__(self, problem, options):
-        # problem-related basic settings
-        self.fitness_function = problem.get('fitness_function')  # to be *minimized*
+        # **problem-related** basic settings
+        #   to be *minimized* approximately globally
+        self.fitness_function = problem.get('fitness_function')
         self.ndim_problem = problem.get('ndim_problem')  # number of problem dimensionality
         assert self.ndim_problem > 0
-        # mainly for both the *initialization* process and the *constrained sampling* process:
-        #   Constrained sampling is mainly covered in its extended library called `PyCoPop7`
-        self.upper_boundary = problem.get('upper_boundary')  # upper boundary of search range
-        self.lower_boundary = problem.get('lower_boundary')  # lower boundary of search range
-        # only for the *initialization* process
+        #   mainly for both the *initialization* process and the *constrained sampling* process:
+        #     constrained sampling is mainly covered in its extended library called `PyCoPop7`
+        #   upper boundary of search range
+        self.upper_boundary = problem.get('upper_boundary')
+        #   lower boundary of search range
+        self.lower_boundary = problem.get('lower_boundary')
+        #   only for the *initialization* process
         self.initial_upper_boundary = problem.get('initial_upper_boundary',
                                                   self.upper_boundary)
         self.initial_lower_boundary = problem.get('initial_lower_boundary',
                                                   self.lower_boundary)
-        # mainly for the printing purpose
-        self.problem_name = problem.get('problem_name')  # should be of data type `str`
+        #   mainly for the *printing* purpose
+        #   should be of data type `str`
+        self.problem_name = problem.get('problem_name')
         if (self.problem_name is None) and hasattr(self.fitness_function, '__name__'):
             self.problem_name = self.fitness_function.__name__
 
